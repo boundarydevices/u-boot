f247fab36e4d174e15e3a9af75fe7e0614304576
Eric Nelson <eric.nelson@boundarydevices.com>
2013-03-05 16:44:07 -0700

    nitrogen6x: check temperature late so environment is available
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- board/boundary/nitrogen6x/nitrogen6x.c
+++ board/boundary/nitrogen6x/nitrogen6x.c
@@ -794,7 +794,6 @@ int checkboard(void)
 		puts("Board: Nitrogen6X\n");
 	else
 		puts("Board: SABRE Lite\n");
-	check_cpu_temperature();
 
 	return 0;
 }
@@ -893,6 +892,7 @@ static const struct boot_mode board_boot_modes[] = {
 
 int misc_init_r(void)
 {
+	check_cpu_temperature();
 #ifdef CONFIG_PREBOOT
 	preboot_keys();
 #endif
