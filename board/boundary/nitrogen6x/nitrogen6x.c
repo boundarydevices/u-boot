2c9a7d85e08e570286827b70e0ec687cb94dc74f
Robert Winkler <robert.winkler@boundarydevices.com>
2013-05-29 13:55:35 -0700

    i.MX6: nitrogen6x: Add splash screen support via loading from flash
    Signed-off-by: Robert Winkler <robert.winkler@boundarydevices.com>
    
    Conflicts:
    	board/boundary/nitrogen6x/nitrogen6x.c
    	include/configs/nitrogen6x.h

--- board/boundary/nitrogen6x/nitrogen6x.c
+++ board/boundary/nitrogen6x/nitrogen6x.c
@@ -406,6 +406,28 @@ int board_eth_init(bd_t *bis)
 	return 0;
 }
 
+
+void splash_screen_prepare(void)
+{
+	char *env_loadsplash;
+
+	if (!getenv("splashimage") || !getenv("splashsize")) {
+		return;
+	}
+
+	env_loadsplash = getenv("loadsplash");
+	if (env_loadsplash == NULL) {
+		printf("Environment variable loadsplash not found!\n");
+		return;
+	}
+
+	if (run_command_list(env_loadsplash, -1, 0)) {
+		printf("failed to run loadsplash %s\n\n", env_loadsplash);
+	}
+
+	return;
+}
+
 static void setup_buttons(void)
 {
 	imx_iomux_v3_setup_multiple_pads(button_pads,
@@ -623,6 +645,10 @@ int board_video_skip(void)
 		printf("unsupported panel %s\n", panel);
 		ret = -EINVAL;
 	}
+
+	if (!ret)
+		splash_screen_prepare();
+
 	return (0 != ret);
 }
 
