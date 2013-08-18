1fdceb77280dcdbc766bbd4959e1c45ab0f8db40
Troy Kisky <troy.kisky@boundarydevices.com>
2013-08-18 10:01:00 -0700

    nitrogen6x: Boundary changes for v2016.03
    nitrogen6x: delay before enabling backlight
    spi_display: set mosi as write in enable_spi_rgb
    nitrogen6x: add IMX_VD_A030JN01_YUV720
    nitrogen6x: add hdmi vga mode
    nitrogen6x: add hdmi 480p
    6x_bootscript-ubuntu-3.14.txt: add imx7d support
    6x_bootscript-yocto-3.14.txt: add imx7d support
    nitrogen6x: prefer hannstar7 over lg1280x800
    nitrogen6x: initialize GP_USB_HUB_RESET as low output
    nitrogen6x: add IMX_VD38_AUO_B101EW05
    nitrogen6x: add IMX_VD38_DT070BTFT
    nitrogen6x: support AR8035 phy as well
    Needed for new carrier with old SOM cpu card.
    nitrogen6x: add CONFIG_PHY_ATHEROS
    nitrogen6x: EIM_D29 high for new carrier with old som
    6x_bootscript-ubuntu-3.14.txt: add rw if not using initrd
    nitrogen6x: add IMX_VD38_TM070JDHG30
    nitrogen6x.h: CONFIG_IPUV3_CLK 264000000
    nitrogen6x: add imx6qp support for ipu priorities
    nitrogen6x: ddr-setup.cfg: disable mirroring
    No boundary board needs mirroring, and the 4G
    boards, cannot have mirroring. So disable by
    default, to prevent copy/paste errors.
    
    ddr-setup.cfg: add IMX6QP support
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>
    
    nitrogen6_lum_dl: rename board and config to nitrogen6_lum_dl1
    
    Signed-off-by: Laci Tele <laci@boundarydevices.com>
    
    6x_bootscript-yocto-3.14.txt: Yocto boots from zImage
    6x_bootscript-*-3.14.txt: Changed address range logic for SX when dtbname set
    6x_bootscript-yocto-3.14: Add check for ${board} being undefined
    6x_bootscript-ubuntu-3.14: Add check for ${board} being undefined
    6x_bootscript-*-3.14.txt: Add dtb support for 6QP
    
    Signed-off-by: Ian Coolidge <ian@boundarydevices.com>
    
    6x_bootscript-mainline: switch to zImage booting
    Also consider the different RAM base address for MX6SX or MX7 devices.
    
    nitrogen6x: add basic fastboot support
    Only works on MMC 1 with GPT already present.
    
    6x_bootscript-yocto-3.10.53: add Quad Plus dtb option
    For the imx6qp-nitrogen6_max.dtb device tree.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- include/configs/nitrogen6x.h
+++ include/configs/nitrogen6x.h
@@ -15,7 +15,7 @@
 #define CONFIG_MACH_TYPE	3769
 
 /* Size of malloc() pool */
-#define CONFIG_SYS_MALLOC_LEN		(10 * 1024 * 1024)
+#define CONFIG_SYS_MALLOC_LEN		(12 * 1024 * 1024)
 
 #define CONFIG_BOARD_EARLY_INIT_F
 #define CONFIG_MISC_INIT_R
@@ -32,6 +32,7 @@
 #define CONFIG_CMD_SF
 #ifdef CONFIG_CMD_SF
 #define CONFIG_MXC_SPI
+#define CONFIG_MXC_SPI_DISPLAY
 #define CONFIG_SF_DEFAULT_BUS  0
 #define CONFIG_SF_DEFAULT_CS   0
 #define CONFIG_SF_DEFAULT_SPEED 25000000
@@ -42,15 +43,16 @@
 #define CONFIG_CMD_I2C
 #define CONFIG_SYS_I2C
 #define CONFIG_SYS_I2C_MXC
-#define CONFIG_SYS_I2C_MXC_I2C1		/* enable I2C bus 1 */
-#define CONFIG_SYS_I2C_MXC_I2C2		/* enable I2C bus 2 */
-#define CONFIG_SYS_I2C_MXC_I2C3		/* enable I2C bus 3 */
+#define CONFIG_SYS_I2C_MXC_I2C1
+#define CONFIG_SYS_I2C_MXC_I2C2
+#define CONFIG_SYS_I2C_MXC_I2C3
 #define CONFIG_SYS_I2C_SPEED		100000
 #define CONFIG_I2C_EDID
 
 /* MMC Configs */
 #define CONFIG_SYS_FSL_ESDHC_ADDR      0
 #define CONFIG_SYS_FSL_USDHC_NUM       2
+#define CONFIG_EFI_PARTITION
 
 #ifdef CONFIG_MX6Q
 #define CONFIG_CMD_SATA
@@ -80,6 +82,7 @@
 #define CONFIG_PHYLIB
 #define CONFIG_PHY_MICREL
 #define CONFIG_PHY_MICREL_KSZ9021
+#define CONFIG_PHY_ATHEROS
 
 /* USB Configs */
 #define CONFIG_CMD_USB
@@ -113,11 +116,11 @@
 #define CONFIG_VIDEO_BMP_GZIP
 #define CONFIG_SYS_VIDEO_LOGO_MAX_SIZE (6 * 1024 * 1024)
 #define CONFIG_BMP_16BPP
-#define CONFIG_IPUV3_CLK 260000000
+#define CONFIG_IPUV3_CLK 264000000
 #define CONFIG_CMD_HDMIDETECT
 #define CONFIG_CONSOLE_MUX
 #define CONFIG_IMX_HDMI
-#define CONFIG_IMX_VIDEO_SKIP
+#define CONFIG_CMD_FBPANEL
 
 #define CONFIG_PREBOOT                 ""
 
@@ -263,7 +266,13 @@
 		"setenv stdout serial,vga; " \
 		"echo no block devices found;" \
 		"\0" \
+	"dfu_alt_info=u-boot raw 0x0 0xc0000\0" \
+	"fdt_addr=0x13000000\0" \
+	"fdt_high=0xffffffff\0" \
 	"initrd_high=0xffffffff\0" \
+	"loadsplash=if sf probe ; then sf read ${splashimage} c2000 ${splashsize} ; fi\0" \
+	"rundfu=dfu 0 sf 0:0:25000000:0\0" \
+	"uboot_defconfig=" CONFIG_DEFCONFIG "\0" \
 	"upgradeu=for dtype in ${bootdevs}" \
 		"; do " \
 		"for disk in 0 1 ; do ${dtype} dev ${disk} ;" \
@@ -272,6 +281,16 @@
 				"&& source 10008000 ; " \
 		"done ; " \
 	"done\0" \
+	"usbnet_devaddr=00:19:b8:00:00:02\0" \
+	"usbnet_hostaddr=00:19:b8:00:00:01\0" \
+	"usbrecover=setenv ethact usb_ether; " \
+		"setenv ipaddr 10.0.0.2; " \
+		"setenv netmask 255.255.255.0; " \
+		"setenv serverip 10.0.0.1; " \
+		"setenv bootargs console=ttymxc1,115200; " \
+		"tftpboot 10800000 10.0.0.1:uImage-${board}-recovery" \
+		"&& tftpboot 12800000 10.0.0.1:uramdisk-${board}-recovery.img " \
+		"&& bootm 10800000 12800000\0" \
 
 #endif
 /* Miscellaneous configurable options */
@@ -282,6 +301,7 @@
 /* Physical Memory Map */
 #define CONFIG_NR_DRAM_BANKS	       1
 #define PHYS_SDRAM		       MMDC0_ARB_BASE_ADDR
+#define CONFIG_RESET_CAUSE_ADDR	       (PHYS_SDRAM + 0x80)
 
 #define CONFIG_SYS_SDRAM_BASE	       PHYS_SDRAM
 #define CONFIG_SYS_INIT_RAM_ADDR       IRAM_BASE_ADDR
@@ -318,6 +338,7 @@
 #define CONFIG_CMD_TIME
 #define CONFIG_CMD_MEMTEST
 #define CONFIG_SYS_ALT_MEMTEST
+#define CONFIG_BOARD_LATE_INIT
 
 /*
  * PCI express
@@ -329,6 +350,7 @@
 #define CONFIG_PCIE_IMX
 #endif
 
+#define CONFIG_CMD_UNZIP
 
 #define CONFIG_USB_GADGET
 #define CONFIG_CMD_USB_MASS_STORAGE
@@ -345,6 +367,17 @@
 #define CONFIG_CMD_FASTBOOT
 #define CONFIG_ANDROID_BOOT_IMAGE
 #define CONFIG_FASTBOOT_BUF_ADDR   CONFIG_SYS_LOAD_ADDR
-#define CONFIG_FASTBOOT_BUF_SIZE   0x07000000
+#define CONFIG_FASTBOOT_BUF_SIZE   0x17000000
+#define CONFIG_FASTBOOT_FLASH
+#define CONFIG_FASTBOOT_FLASH_MMC_DEV   1
+#define CONFIG_CMD_GPT
+#define CONFIG_PARTITION_UUIDS
+
+/* USB Device Firmware Update support */
+#define CONFIG_USB_FUNCTION_DFU
+#define CONFIG_DFU_SF
+#define CONFIG_CMD_DFU
+#define CONFIG_SYS_DFU_DATA_BUF_SIZE	0xc0000
+#define DFU_MANIFEST_POLL_TIMEOUT	25000
 
 #endif	       /* __CONFIG_H */
