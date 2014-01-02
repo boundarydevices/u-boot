5111d3f8d567b82ee6c0ed79c3f583bcfdf074f6
Eric Nelson <eric.nelson@boundarydevices.com>
2014-01-02 12:06:07 -0700

    sp: no SGTL5000 on this board
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- board/boundary/sp/sp.c
+++ board/boundary/sp/sp.c
@@ -78,7 +78,7 @@ iomux_v3_cfg_t const uart2_pads[] = {
 
 #define PC MUX_PAD_CTRL(I2C_PAD_CTRL)
 
-/* I2C1, SGTL5000 */
+/* I2C1 */
 struct i2c_pads_info i2c_pad_info0 = {
 	.scl = {
 		.i2c_mode = MX6_PAD_EIM_D21__I2C1_SCL | PC,
