6075d37f29bd5af317f79c6bfc5342eac095f105
Eric Nelson <eric.nelson@boundarydevices.com>
2014-02-05 17:01:33 -0700

    boundary boards: i2c: update config using mxc driver to new subsystem
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- include/configs/sp.h
+++ include/configs/sp.h
@@ -68,8 +68,8 @@
 
 /* I2C Configs */
 #define CONFIG_CMD_I2C
-#define CONFIG_I2C_MULTI_BUS
-#define CONFIG_I2C_MXC
+#define CONFIG_SYS_I2C
+#define CONFIG_SYS_I2C_MXC
 #define CONFIG_SYS_I2C_SPEED		100000
 #define CONFIG_I2C_EDID
 
