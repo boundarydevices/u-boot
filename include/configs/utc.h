18395073009d82f1eb68cd5726113765122c71fb
Eric Nelson <eric.nelson@boundarydevices.com>
2014-02-25 09:06:11 -0700

    utc: include serverip in usbboot
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- include/configs/utc.h
+++ include/configs/utc.h
@@ -231,7 +231,6 @@
 		"setenv ipaddr 10.0.0.2; " \
 		"setenv netmask 255.255.255.0; " \
 		"setenv serverip 10.0.0.1; " \
-		"setenv ncip 10.0.0.1; " \
 		"setenv console=ttymxc1,115200; " \
 		"tftpboot 10800000 10.0.0.1:uImage-recovery" \
 		"&& tftpboot 12800000 10.0.0.1:uramdisk-recovery.img " \
