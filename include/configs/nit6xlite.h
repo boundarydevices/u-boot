cb2fdcbf844346f3f7bb22d74b2882f3e31efcee
Eric Nelson <eric.nelson@boundarydevices.com>
2014-02-25 09:05:28 -0700

    nit6xlite: include serverip in usbboot
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- include/configs/nit6xlite.h
+++ include/configs/nit6xlite.h
@@ -203,6 +203,7 @@
 	"usbboot=setenv ethact usb_ether; " \
 		"setenv ipaddr 10.0.0.2; " \
 		"setenv netmask 255.255.255.0; " \
+		"setenv serverip 10.0.0.1; " \
 		"setenv bootargs console=ttymxc1,115200; " \
 		"tftpboot 10800000 10.0.0.1:uImage-nit6xlite-recovery" \
 		"&& tftpboot 12800000 10.0.0.1:uramdisk-nit6xlite-recovery.img " \
