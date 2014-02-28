a981de875c424d90c00ca60c99fd6d7c9114f4c1
Troy Kisky <troy.kisky@boundarydevices.com>
2014-02-28 10:25:03 -0700

    vp: initial addition, Boundary Devices board
    vp.h: CONFIG_IPUV3_CLK 264000000
    vp: fix MAINTAINERS file
    vp: use nitrogen6x clocks.cfg/ddr-setup.cfg
    vp: add CONFIG_CMD_GPIO
    vp: explicit fbp_detect_i2c
    vp: verify port in board_ehci_hcd_init
    vp: use boundary.h
    vp: setup rgb_gpio_pads in board_early_init_f
    vp: add CONFIG_SPI_FLASH_SPANSION
    vp: vp_defconfig add CONFIG_BLOCK_CACHE
    vp: use common code for eth init
    vp: eth.c now in common directory
    vp: move misc_init_r/do_kbd to common
    vp: move max77823_init to common
    vp: move mmc_init/ dram_init/ overwrite_console/ common_board_init/ splash_screen_prepare/ board_cfb_skip to common
    vp: use common 1066mhz_4x256mx16.cfg
    vp: add  CONFIG_SPI_FLASH_GIGADEVICE: to defconfigs
    vp: use common ddr script
    vp: port to v2018.07
    vp: add CONFIG_CMD_BMP
    vp: use boundary vendorid for GADGET_VENDOR_NUM
    vp: add  CONFIG_NR_DRAM_BANKS=1: to defconfigs
    vp: move MX6Q to Kconfig
    vp: add  CONFIG_FSL_ESDHC_IMX: to defconfigs
    vp: enable touch driver
    vp: add  CONFIG_BOUNDARY_BOARD_IMX6: to defconfigs
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>
    
    vp: update to v2017.01
    vp: update to v2017.03
    vp: add  CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG: to defconfigs
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

---
+++ include/configs/vp.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2010-2011 Freescale Semiconductor, Inc.
+ *
+ * Configuration settings for the Boundary Devices VP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include "mx6_common.h"
+
+#define CONFIG_MACH_TYPE	3769
+#define CONFIG_ETHPRIME			"usb_ether"
+
+#define CONFIG_IMX_HDMI
+#define CONFIG_SYS_FSL_USDHC_NUM	2
+#define CONFIG_USB_MAX_CONTROLLER_COUNT 2
+#define BD_I2C_MASK	7
+
+#include "boundary.h"
+#define CONFIG_EXTRA_ENV_SETTINGS BD_BOUNDARY_ENV_SETTINGS \
+
+#endif	       /* __CONFIG_H */
