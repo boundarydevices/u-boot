60442976e5ebaac56109ff7047ccf7e00cb6e458
Troy Kisky <troy.kisky@boundarydevices.com>
2014-03-12 16:06:49 -0700

    mcs: initial addition, Boundary Devices board
    mcs: wait for panel to sync up before enabling backlight
    mcs: add display detection based on 4/5 wire touchscreen
    mcs: mcssolo1g_64bit.cfg: new calibration values
    mcs.h: CONFIG_IPUV3_CLK 264000000
    mcs: setup_dispay is done in fbpanel
    mcs: documentation fix, GP_LVDS_DE_15_4 is unused
    mcs: add CONFIG_CMD_GPIO
    mcs: remove redundant CONFIG_CMD_GPIO
    mcs: explicit fbp_detect_i2c
    mcs: pass fbp_detect_gpio
    mcs: use VD_LD101WX1
    mcs: verify port in board_ehci_hcd_init
    mcs: use boundary.h
    mcs: add CONFIG_SPI_FLASH_SPANSION
    mcs: mcsquad_defconfig add CONFIG_BLOCK_CACHE
    mcssolo1g_64bit: mcssolo1g_64bit_defconfig add CONFIG_BLOCK_CACHE
    mcssolo: mcssolo_defconfig add CONFIG_BLOCK_CACHE
    mcs: use common code for eth init
    mcs: eth.c now in common directory
    mcs: move misc_init_r/do_kbd to common
    mcs: move mmc_init/ dram_init/ overwrite_console/ common_board_init/ splash_screen_prepare/ board_cfb_skip to common
    mcs: use common 1066mhz_4x256mx16.cfg
    mcs: add  CONFIG_SPI_FLASH_GIGADEVICE: to defconfigs
    mcs: use common 800mhz_4x128mx16.cfg
    mcs: use common ddr scripts
    mcs: port to v2018.07
    mcs: add CONFIG_CMD_BMP
    mcs: use boundary vendorid for GADGET_VENDOR_NUM
    mcs: add mcs_dl2g_defconfig
    mcs: remove pull down from 4-wire/5-wire select
    mcs: add  CONFIG_NR_DRAM_BANKS=1: to defconfigs
    mcs: move MX6QDL to Kconfig
    mcs: add  CONFIG_FSL_ESDHC_IMX: to defconfigs
    mcs: enable touch driver
    mcs: add  CONFIG_BOUNDARY_BOARD_IMX6: to defconfigs
    mcs: move CONFIG_PREBOOT to defconfig and add CONFIG_USE_PREBOOT
    mcs: split CONFIG_SYS_EXTRA_OPTIONS
    mcs: add CONFIG_SYS_DFU_DATA_BUF_SIZE/CONFIG_SYS_DFU_MAX_FILE_SIZE
    mcs: move CONFIG_SYS_I2C_MXC into defconfig file
    mcs: add CONFIG_I2C_EDID=y
    mcs: delete define __ASSEMBLY__ line
    mcs: add CONFIG_SYS_MALLOC_LEN=0xc00000
    mcs: s/CONFIG_SECURE_BOOT/CONFIG_IMX_HAB/
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>
    
    mcs: add 6x_bootscript-ubuntu-3.14.txt
    
    Signed-off-by: Laci Tele <laci@boundarydevices.com>
    
    mcs: update to v2017.01
    mcs: update to v2017.03
    mcs: add  CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG: to defconfigs
    mcs: add CONFIG_FSL_USDHC to defconfigs
    mcs: add CONFIG_CMD_ERASEENV to defconfigs
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

---
+++ include/configs/mcs.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2010-2011 Freescale Semiconductor, Inc.
+ *
+ * Configuration settings for the Boundary Devices MCS board.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include "mx6_common.h"
+
+#define CONFIG_MACH_TYPE	3769
+#define CONFIG_SYS_FSL_USDHC_NUM	3
+#define CONFIG_USB_MAX_CONTROLLER_COUNT 2
+#define BD_SPLASH_FLASH "f0000"
+
+#include "boundary.h"
+#define CONFIG_EXTRA_ENV_SETTINGS BD_BOUNDARY_ENV_SETTINGS \
+	"disable_giga=1\0" \
+	"savesplash=script=/savesplash; run runscript\0" \
+
+#endif	       /* __CONFIG_H */
