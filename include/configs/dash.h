d80107fccb5e93a53c9d1ff2f1b6c905da2e455d
Eric Nelson <eric.nelson@boundarydevices.com>
2014-04-28 11:52:05 -0700

    dash: don't use board variable for recovery images
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- include/configs/dash.h
+++ include/configs/dash.h
@@ -298,8 +298,8 @@
 		"setenv netmask 255.255.255.0; " \
 		"setenv serverip 10.0.0.1; " \
 		"setenv bootargs console=ttymxc1,115200; " \
-		"tftpboot 10800000 10.0.0.1:uImage-${board}-recovery" \
-		"&& tftpboot 12800000 10.0.0.1:uramdisk-${board}-recovery.img " \
+		"tftpboot 10800000 10.0.0.1:uImage-dash-recovery" \
+		"&& tftpboot 12800000 10.0.0.1:uramdisk-dash-recovery.img " \
 		"&& bootm 10800000 12800000\0" \
 
 #endif
