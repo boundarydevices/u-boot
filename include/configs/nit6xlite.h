e2cecbd03828173e140d2bdc5dac0e96ec97ffee
Eric Nelson <eric.nelson@boundarydevices.com>
2014-06-10 09:38:43 -0700

    nit6xlite: force 10/100 ethernet
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- include/configs/nit6xlite.h
+++ include/configs/nit6xlite.h
@@ -188,6 +188,7 @@
 			"done ; " \
 		"done ; " \
 	"done\0" \
+	"disable_giga=1\0" \
 	"usbnet_devaddr=00:19:b8:00:00:02\0" \
 	"usbnet_hostaddr=00:19:b8:00:00:01\0" \
 	"usbrecover=setenv ethact usb_ether; " \
