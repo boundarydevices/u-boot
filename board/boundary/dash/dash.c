5ffef88d53a5ecb4574a2a87fcc2d26c6f8347b1
Eric Nelson <eric.nelson@boundarydevices.com>
2014-09-29 13:12:53 -0700

    dash: Update SPI NOR to match updates in main-line
    Default chip-select is done in a call-back, doesn't need
    SPI pad control.
    
    See commits 155fa9af9 and 3b31605ae.
    
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- board/boundary/dash/dash.c
+++ board/boundary/dash/dash.c
@@ -244,9 +244,14 @@ int board_mmc_init(bd_t *bis)
 }
 
 #ifdef CONFIG_MXC_SPI
+int board_spi_cs_gpio(unsigned bus, unsigned cs)
+{
+	return (bus == 0 && cs == 0) ? (IMX_GPIO_NR(3, 19)) : -1;
+}
+
 static iomux_v3_cfg_t const ecspi1_pads[] = {
 	/* SS1 */
-	MX6_PAD_EIM_D19__GPIO3_IO19   | MUX_PAD_CTRL(SPI_PAD_CTRL),
+	MX6_PAD_EIM_D19__GPIO3_IO19  | MUX_PAD_CTRL(NO_PAD_CTRL),
 	MX6_PAD_EIM_D17__ECSPI1_MISO | MUX_PAD_CTRL(SPI_PAD_CTRL),
 	MX6_PAD_EIM_D18__ECSPI1_MOSI | MUX_PAD_CTRL(SPI_PAD_CTRL),
 	MX6_PAD_EIM_D16__ECSPI1_SCLK | MUX_PAD_CTRL(SPI_PAD_CTRL),
