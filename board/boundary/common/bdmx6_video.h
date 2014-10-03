cdcdc736f0b62e2f3f267f101ade2588a8ee5779
Eric Nelson <eric.nelson@boundarydevices.com>
2014-10-02 17:09:30 -0700

    nitrogen6x: move displays and initialization to boundary/common/bdmxc_video.c
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

---
+++ board/boundary/common/bdmx6_video.h
@@ -0,0 +1,11 @@
+/*
+ * (C) Copyright 2014 Boundary Devices
+ *
+ * Eric Nelson <eric.nelson@boundarydevices.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#ifndef __BDMX6_VIDEO_H_
+#define __BDMX6_VIDEO_H_
+
+#endif
