e3a0fba3ccac761cec297faa1c094ff79ac155d4
Troy Kisky <troy.kisky@boundarydevices.com>
2014-12-18 12:16:02 -0700

    mxc_i2c: add I2C_PADS_INFO_ENTRY macro definition

--- arch/arm/include/asm/imx-common/mxc_i2c.h
+++ arch/arm/include/asm/imx-common/mxc_i2c.h
@@ -19,6 +19,39 @@ struct i2c_pads_info {
 	struct i2c_pin_ctrl sda;
 };
 
+#define I2C_PADS_INFO_CPU(cpu, i2cnum, scl_pad, scl_bank, scl_gp,	       \
+		sda_pad, sda_bank, sda_gp, ctrl) {			       \
+    .scl = {								       \
+	.i2c_mode = NEW_PAD_CTRL(cpu##_PAD_##scl_pad##__##i2cnum##_SCL, ctrl), \
+	.gpio_mode = NEW_PAD_CTRL(					       \
+		cpu##_PAD_##scl_pad##__GPIO##scl_bank##_IO##scl_gp, ctrl),     \
+	.gp = IMX_GPIO_NR(scl_bank, scl_gp)				       \
+    },									       \
+    .sda = {								       \
+	.i2c_mode = NEW_PAD_CTRL(cpu##_PAD_##sda_pad##__##i2cnum##_SDA, ctrl), \
+	.gpio_mode = NEW_PAD_CTRL(					       \
+		cpu##_PAD_##sda_pad##__GPIO##sda_bank##_IO##sda_gp, ctrl),     \
+	.gp = IMX_GPIO_NR(sda_bank, sda_gp)				       \
+    }									       \
+}
+
+#if defined(CONFIG_MX6QDL)
+#define I2C_PADS_INFO_ENTRY(i2cnum, scl_pad, scl_bank, scl_gp,		\
+		sda_pad, sda_bank, sda_gp, ctrl)			\
+	I2C_PADS_INFO_CPU(MX6Q, i2cnum, scl_pad, scl_bank, scl_gp,	\
+		sda_pad, sda_bank, sda_gp, ctrl),			\
+	I2C_PADS_INFO_CPU(MX6DL, i2cnum, scl_pad, scl_bank, scl_gp,	\
+		sda_pad, sda_bank, sda_gp, ctrl)
+#define I2C_PADS_INFO_ENTRY_SPACING 2
+#else
+#define I2C_PADS_INFO_ENTRY(i2cnum, scl_pad, scl_bank, scl_gp,		\
+		sda_pad, sda_bank, sda_gp, ctrl)			\
+	I2C_PADS_INFO_CPU(MX6, i2cnum, scl_pad, scl_bank, scl_gp,	\
+		sda_pad, sda_bank, sda_gp, ctrl)
+#define I2C_PADS_INFO_ENTRY_SPACING 1
+#endif
+
+
 #if defined(CONFIG_MX6QDL)
 #define I2C_PADS(name, scl_i2c, scl_gpio, scl_gp, sda_i2c, sda_gpio, sda_gp) \
 	struct i2c_pads_info mx6q_##name = {		\
@@ -60,4 +93,15 @@ int bus_i2c_read(void *base, uchar chip, uint addr, int alen, uchar *buf,
 		int len);
 int bus_i2c_write(void *base, uchar chip, uint addr, int alen,
 		const uchar *buf, int len);
+
+static inline int i2c_get_info_entry_offset(void) {
+#if defined(CONFIG_MX6QDL)
+	int cpu = get_cpu_type();
+
+	if ((cpu != MXC_CPU_MX6Q) && (cpu != MXC_CPU_MX6D))
+		return 1;
+#endif
+	return 0;
+}
+
 #endif
