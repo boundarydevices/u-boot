5971ec2be700dafc5282466f270f6e9c8cd4d454
Troy Kisky <troy.kisky@boundarydevices.com>
2015-01-02 16:31:57 -0700

    nitrogen6_max: fix bmod mmc1

--- board/boundary/nitrogen6_max/nitrogen6_max.c
+++ board/boundary/nitrogen6_max/nitrogen6_max.c
@@ -1010,7 +1010,7 @@ static void preboot_keys(void)
 static const struct boot_mode board_boot_modes[] = {
 	/* 4 bit bus width */
 	{"mmc0",	MAKE_CFGVAL(0x40, 0x30, 0x00, 0x00)},
-	{"mmc1",	MAKE_CFGVAL(0x40, 0x38, 0x00, 0x00)},
+	{"mmc1",	MAKE_CFGVAL(0x60, 0x58, 0x00, 0x00)},	/* 8-bit eMMC */
 	{NULL,		0},
 };
 #endif
