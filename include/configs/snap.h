390c2e3e8ffcf7a2b338e69072905fc2c4acfb4c
Troy Kisky <troy.kisky@boundarydevices.com>
2015-02-11 16:14:16 -0700

    snap: initial addition, Boundary Devices board
    snap: use nitrogen6x/ddr-setup.cfg
    snap: add CONFIG_CMD_GPIO
    snap: use boundary.h
    snap: add CONFIG_SPI_FLASH_SPANSION
    snap: snap_defconfig add CONFIG_BLOCK_CACHE
    snap: use common code for eth init
    snap: eth.c now in common directory
    snap: move misc_init_r/do_kbd to common
    snap: move mmc_init/ dram_init/ overwrite_console/ common_board_init/ splash_screen_prepare/ board_cfb_skip to common
    snap: use common 1066mhz_4x256mx16.cfg
    snap: add  CONFIG_SPI_FLASH_GIGADEVICE: to defconfigs
    snap: use common ddr script
    snap: port to v2018.07
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>
    
    snap: update to v2017.01
    snap: update to v2017.03
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

---
+++ include/configs/snap.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2010-2011 Freescale Semiconductor, Inc.
+ *
+ * Configuration settings for the Boundary Devices Snap board
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include "mx6_common.h"
+
+#define CONFIG_MACH_TYPE	3778
+
+#define CONFIG_CONSOLE_MUX
+
+#define CONFIG_FEC_MXC_PHYADDR		7
+#define CONFIG_SYS_FSL_USDHC_NUM	2
+#define CONFIG_USB_MAX_CONTROLLER_COUNT 2
+#define BD_I2C_MASK	7
+#define BD_MMC_DISKS "1"
+#define BD_MMC_UMS_DISKS "1 0"
+
+#include "boundary.h"
+#define CONFIG_EXTRA_ENV_SETTINGS BD_BOUNDARY_ENV_SETTINGS \
+	"disable_giga=1\0" \
+
+#endif	       /* __CONFIG_H */
