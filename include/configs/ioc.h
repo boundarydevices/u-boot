9cece979b45041e2fedc129ae5f8a409919571a0
Troy Kisky <troy.kisky@boundarydevices.com>
2015-07-20 15:00:46 -0700

    ioc: initial addition, Boundary Devices board
    ioc: only pwm1 for all backlights
    ioc: prefer hannstar7 over lg1280x800
    ioc: add IMX_VD38_AUO_B101EW05
    ioc.h: CONFIG_IPUV3_CLK 264000000
    ioc: add CONFIG_CMD_GPIO
    ioc: explicit fbp_detect_i2c
    ioc: add wlmac
    ioc: verify port in board_ehci_hcd_init
    ioc: use boundary.h
    ioc: setup rgb_gpio_pads in board_early_init_f
    ioc: add CONFIG_SPI_FLASH_SPANSION
    ioc: ioc_defconfig add CONFIG_BLOCK_CACHE
    ioc: use common code for eth init
    ioc: eth.c now in common directory
    ioc: move misc_init_r/do_kbd to common
    ioc: move mmc_init/ dram_init/ overwrite_console/ common_board_init/ splash_screen_prepare/ board_cfb_skip to common
    ioc: use common 1066mhz_4x128mx16.cfg
    ioc: add  CONFIG_SPI_FLASH_GIGADEVICE: to defconfigs
    ioc: use common ddr script
    ioc: port to v2018.07
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>
    
    ioc: update to v2017.01
    ioc: update to v2017.03
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

---
+++ include/configs/ioc.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2010-2011 Freescale Semiconductor, Inc.
+ *
+ * Configuration settings for the Boundary Devices ioc
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include "mx6_common.h"
+
+#define CONFIG_MACH_TYPE	3769
+
+#define CONFIG_MXC_SPI_DISPLAY
+
+#ifdef CONFIG_MX6Q
+#endif
+#define CONFIG_IMX_HDMI
+#define CONFIG_SYS_FSL_USDHC_NUM	2
+#define CONFIG_USB_MAX_CONTROLLER_COUNT 2
+#define BD_I2C_MASK	7
+#define BD_MMC_DISKS "1 0"
+
+#include "boundary.h"
+#define CONFIG_EXTRA_ENV_SETTINGS BD_BOUNDARY_ENV_SETTINGS \
+	"fb_lvds=AA065VE11\0" \
+	"panel=AA065VE11\0" \
+
+#endif	       /* __CONFIG_H */
