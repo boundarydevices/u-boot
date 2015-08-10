55075fdf417230dd7d1eec909312e5f6d1adca61
Troy Kisky <troy.kisky@boundarydevices.com>
2015-08-10 12:50:52 -0700

    imx-common: video.h: add fields fbtype/fbflags

--- arch/arm/include/asm/imx-common/video.h
+++ arch/arm/include/asm/imx-common/video.h
@@ -14,6 +14,18 @@ struct display_info_t {
 	int	pixfmt;
 	int	(*detect)(struct display_info_t const *dev);
 	void	(*enable)(struct display_info_t const *dev);
+
+#define FB_HDMI		0
+#define FB_LCD		1
+#define FB_LVDS		2
+#define FB_LVDS2	3
+#define FB_COUNT	4
+	int	fbtype;
+
+#define FBF_MODESTR	1
+#define FBF_JEIDA	2
+#define FBF_SPLITMODE	4
+	int	fbflags;
 	struct	fb_videomode mode;
 };
 
@@ -26,4 +38,28 @@ extern struct display_info_t const displays[];
 extern size_t display_count;
 #endif
 
+static void __board_enable_hdmi(struct display_info_t const *di)
+{
+}
+
+static void __board_enable_lcd(struct display_info_t const *di)
+{
+}
+
+static void __board_enable_lvds(struct display_info_t const *di)
+{
+}
+
+static void __board_enable_lvds2(struct display_info_t const *di)
+{
+}
+
+void board_enable_hdmi(struct display_info_t const *di)
+	__attribute__((weak, alias("__board_enable_hdmi")));
+void board_enable_lcd(struct display_info_t const *di)
+	__attribute__((weak, alias("__board_enable_lcd")));
+void board_enable_lvds(struct display_info_t const *di)
+	__attribute__((weak, alias("__board_enable_lvds")));
+void board_enable_lvds2(struct display_info_t const *di)
+	__attribute__((weak, alias("__board_enable_lvds2")));
 #endif
