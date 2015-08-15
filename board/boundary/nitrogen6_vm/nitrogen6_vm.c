4828f636bf2b26730a11f74edaca9640d12c4df0
Troy Kisky <troy.kisky@boundarydevices.com>
2015-08-14 18:42:52 -0700

    nitrogen6_vm: setenv uboot_defconfig

--- board/boundary/nitrogen6_vm/nitrogen6_vm.c
+++ board/boundary/nitrogen6_vm/nitrogen6_vm.c
@@ -945,8 +945,11 @@ int misc_init_r(void)
 int board_late_init(void)
 {
 	int cpurev = get_cpu_rev();
-	setenv("cpu",get_imx_type((cpurev & 0xFF000) >> 12));
-	if (0 == getenv("board"))
-		setenv("board","nitrogen6_vm");
+
+	setenv("cpu", get_imx_type((cpurev & 0xFF000) >> 12));
+	if (!getenv("board"))
+		setenv("board", "nitrogen6_vm");
+	if (!getenv("uboot_defconfig"))
+		setenv("uboot_defconfig", CONFIG_DEFCONFIG);
 	return 0;
 }
