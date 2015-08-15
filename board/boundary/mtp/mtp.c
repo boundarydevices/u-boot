67079ec938ee7a11987602e2e905c94cd516268a
Troy Kisky <troy.kisky@boundarydevices.com>
2015-08-14 18:38:50 -0700

    mtp: setenv uboot_defconfig

--- board/boundary/mtp/mtp.c
+++ board/boundary/mtp/mtp.c
@@ -246,7 +246,11 @@ int misc_init_r(void)
 int board_late_init(void)
 {
 	int cpurev = get_cpu_rev();
-	setenv("cpu",get_imx_type((cpurev & 0xFF000) >> 12));
-	setenv("board","mtp");
+
+	setenv("cpu", get_imx_type((cpurev & 0xFF000) >> 12));
+	if (!getenv("board"))
+		setenv("board","mtp");
+	if (!getenv("uboot_defconfig"))
+		setenv("uboot_defconfig", CONFIG_DEFCONFIG);
 	return 0;
 }
