619f92b79efae705f9d4f3b60b6dd80080823aa3
Troy Kisky <troy.kisky@boundarydevices.com>
2015-08-14 18:31:13 -0700

    dash: setenv uboot_defconfig

--- board/boundary/dash/dash.c
+++ board/boundary/dash/dash.c
@@ -751,7 +751,11 @@ int misc_init_r(void)
 int board_late_init(void)
 {
 	int cpurev = get_cpu_rev();
-	setenv("cpu",get_imx_type((cpurev & 0xFF000) >> 12));
-	setenv("board","DASH");
+
+	setenv("cpu", get_imx_type((cpurev & 0xFF000) >> 12));
+	if (!getenv("board"))
+		setenv("board", "dash");
+	if (!getenv("uboot_defconfig"))
+		setenv("uboot_defconfig", CONFIG_DEFCONFIG);
 	return 0;
 }
