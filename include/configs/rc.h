7042a0f4af05ab4b6ae6bfdc3773c19477f47a74
Troy Kisky <troy.kisky@boundarydevices.com>
2015-11-25 17:04:57 -0700

    rc: initial addition, Boundary Devices board
    rc: correct AR8035 comments
    rc.h: CONFIG_IPUV3_CLK 264000000
    rc: use nitrogen6x clocks.cfg/ddr-setup.cfg
    rc: add CONFIG_CMD_GPIO
    rc: explicit fbp_detect_i2c
    rc: verify port in board_ehci_hcd_init
    rc: use boundary.h
    rc: add CONFIG_SPI_FLASH_SPANSION
    rc: rc_defconfig add CONFIG_BLOCK_CACHE
    rc: use common code for eth init
    rc: eth.c now in common directory
    rc: move misc_init_r/do_kbd to common
    rc: move mmc_init/ dram_init/ overwrite_console/ common_board_init/ splash_screen_prepare/ board_cfb_skip to common
    rc: use common 800mhz_4x128mx16.cfg
    rc: add  CONFIG_SPI_FLASH_GIGADEVICE: to defconfigs
    rc: use common ddr script
    rc: port to v2018.07
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>
    
    rc: clocks.cfg: set IPU AXI bus priorities
    
    Signed-off-by: Eric Nelson <eric@nelint.com>
    
    rc: update to v2017.01
    rc: update to v2017.03
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

---
+++ include/configs/rc.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2010-2011 Freescale Semiconductor, Inc.
+ *
+ * Configuration settings for the Boundary Devices rc
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include "mx6_common.h"
+
+#define CONFIG_MACH_TYPE	3778
+
+#define CONFIG_FEC_MXC_PHYADDR		4
+#define CONFIG_IMX_HDMI
+#define CONFIG_SYS_FSL_USDHC_NUM	2
+#define CONFIG_USB_MAX_CONTROLLER_COUNT 2
+#define BD_I2C_MASK	7
+#define BD_MMC_UMS_DISKS "1 0"
+
+#include "boundary.h"
+#define CONFIG_EXTRA_ENV_SETTINGS BD_BOUNDARY_ENV_SETTINGS \
+
+#endif	       /* __CONFIG_H */
