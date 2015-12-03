b750271ac9301ae61d3d6ea82f341b80c3c3513d
Gary Bisson <gary.bisson@boundarydevices.com>
2015-12-03 19:03:11 +0100

    cnt: initial addition, Boundary Devices board
    cnt: add defconfig for 1GB of RAM version
    cnt: fix boot devs configuration
    cnt: update to v2017.01
    cnt: update to v2017.03
    cnt.h: add custom BOOT_TARGET_DEVICES priority
    cnt: add  CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG: to defconfigs
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>
    
    cnt: print ethernet phy name (AR8035)
    cnt.h: CONFIG_IPUV3_CLK 264000000
    cnt: use nitrogen6x clock.cfg/ddr-setup.cfg
    cnt: add CONFIG_CMD_GPIO
    cnt: explicit fbp_detect_i2c
    cnt: verify port in board_ehci_hcd_init
    cnt: preboot_keys is not defined, remove call
    cnt: use boundary.h
    cnt: add CONFIG_SPI_FLASH_SPANSION
    cnt: 1066mhz_4x256mx16.cfg: update calibration for new spin of board
    cnt: cnt1g_defconfig add CONFIG_BLOCK_CACHE
    cnt2g: cnt2g_defconfig add CONFIG_BLOCK_CACHE
    cnt: 1066mhz_4x256mx16.cfg: increase cycles for tFAW/tRRD
    cnt2g: change ddr to 500.21 MHz
    cnt: use common code for eth init
    cnt: eth.c now in common directory
    cnt: move misc_init_r/do_kbd to common
    cnt: move mmc_init/ dram_init/ overwrite_console/ common_board_init/ splash_screen_prepare/ board_cfb_skip to common
    cnt: use common 1066mhz_4x256mx16.cfg
    cnt: use common 1066mhz_4x128mx16.cfg
    cnt: add  CONFIG_SPI_FLASH_GIGADEVICE: to defconfigs
    cnt: use common ddr scripts
    cnt: port to v2018.07
    cnt: add CONFIG_CMD_BMP
    cnt: use boundary vendorid for GADGET_VENDOR_NUM
    cnt: use CONFIG_USB_BOOT_FIRST
    cnt: add  CONFIG_NR_DRAM_BANKS=1: to defconfigs
    cnt: move MX6Q to Kconfig
    cnt: add  CONFIG_FSL_ESDHC_IMX: to defconfigs
    cnt: enable touch driver
    cnt: add  CONFIG_BOUNDARY_BOARD_IMX6: to defconfigs
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

---
+++ include/configs/cnt.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2015 Boundary Devices, Inc.
+ *
+ * Configuration settings for the Boundary Devices CNT board.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#define CONFIG_BOOTDELAY	1
+
+#include "mx6_common.h"
+
+#define CONFIG_MACH_TYPE	3769
+
+#define CONFIG_MXC_SPI_DISPLAY
+
+#define CONFIG_FEC_MXC_PHYADDR		4
+#define CONFIG_SYS_FSL_USDHC_NUM	2
+#define CONFIG_USB_MAX_CONTROLLER_COUNT 2
+#define BD_I2C_MASK	7
+
+#include "boundary.h"
+#define CONFIG_EXTRA_ENV_SETTINGS BD_BOUNDARY_ENV_SETTINGS \
+
+#endif	       /* __CONFIG_H */
