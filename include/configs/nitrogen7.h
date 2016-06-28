75299b883292dfc93290ac9ad69dce54834a3236
Gary Bisson <gary.bisson@boundarydevices.com>
2016-06-28 17:48:28 +0200

    nitrogen7: add initial m4 support for FreeRTOS
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- include/configs/nitrogen7.h
+++ include/configs/nitrogen7.h
@@ -62,6 +62,34 @@
 #define BD_CONSOLE	"ttymxc0"
 #define BD_I2C_MASK	0xf
 
+/* M4 specific */
+#define CONFIG_SYS_AUXCORE_BOOTDATA_DDR		0x9ff00000
+#define CONFIG_SYS_AUXCORE_BOOTDATA_OCRAM	0x00910000
+#define CONFIG_SYS_AUXCORE_BOOTDATA_QSPI	0x601e0000
+#define CONFIG_SYS_AUXCORE_BOOTDATA_TCM		0x007F8000
+#define CONFIG_EXTRA_ENV_M4 \
+	"m4image=m4_fw.bin\0" \
+	"m4offset=0x1e0000\0" \
+	"m4size=0x8000\0" \
+	"loadm4image=load ${dtype} ${disk}:1 ${loadaddr} ${m4image}\0" \
+	"m4update=for dtype in ${bootdevs}; do " \
+		"for disk in 0 1 ; do ${dtype} dev ${disk} ;" \
+			"if run loadm4image; then " \
+				"sf probe; " \
+				"sf erase ${m4offset} ${m4size}; " \
+				"sf write ${loadaddr} ${m4offset} ${filesize}; " \
+				"exit; " \
+			"fi; " \
+		"done; " \
+		"done\0" \
+	"m4loadaddr="__stringify(CONFIG_SYS_AUXCORE_BOOTDATA_TCM)"\0" \
+	"m4boot=run m4boot_nor\0" \
+	"m4boot_ext=load ${dtype} ${disk}:1 ${m4loadaddr} ${m4image}; " \
+		"dcache flush; bootaux ${m4loadaddr}\0" \
+	"m4boot_nor=sf probe; sf read ${m4loadaddr} ${m4offset} ${m4size}; " \
+		"dcache flush; bootaux ${m4loadaddr}\0" \
+	"m4boot_qspi=bootaux "__stringify(CONFIG_SYS_AUXCORE_BOOTDATA_QSPI)"\0"
+
 #include "boundary.h"
 #define CONFIG_EXTRA_ENV_SETTINGS BD_BOUNDARY_ENV_SETTINGS \
 	"mfgtool_args=setenv bootargs console=${console},${baudrate} " \
@@ -74,5 +102,6 @@
 	"initrd_addr=0x83800000\0" \
 	"bootcmd_mfg=run mfgtool_args;bootz ${loadaddr} ${initrd_addr} ${fdt_addr};\0" \
 	"cmd_custom=echo\0" \
+	CONFIG_EXTRA_ENV_M4
 
 #endif	/* __CONFIG_H */
