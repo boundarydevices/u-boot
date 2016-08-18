3082b043b0c3b22877bab472a20b8e72e4a7f2b5
Gary Bisson <gary.bisson@boundarydevices.com>
2016-08-18 15:56:06 +0200

    boundary.h: add secure boot options
    Disabled by default, can be enabled through menuconfig so your
    U-Boot image IVT header is aware of the CSF section.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- include/configs/boundary.h
+++ include/configs/boundary.h
@@ -21,6 +21,15 @@
 #define CONFIG_MXC_UART_BASE		UART2_BASE
 #endif
 
+/* Secure boot (HAB) support */
+#ifdef CONFIG_SECURE_BOOT
+#define CONFIG_CSF_SIZE			0x2000
+#define CONFIG_SYS_FSL_SEC_COMPAT	4
+#define CONFIG_FSL_CAAM
+#define CONFIG_CMD_DEKBLOB
+#define CONFIG_SYS_FSL_SEC_LE
+#endif
+
 /* SPL magic */
 #ifdef CONFIG_SPL
 #define CONFIG_SPL_FRAMEWORK
