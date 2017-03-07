f8dcc506609fe25ac30cc02bd514cb9d0cff4664
Gary Bisson <gary.bisson@boundarydevices.com>
2017-03-07 18:01:12 +0100

    image-sparse: disable data cache to avoid misaligned warnings
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- lib/image-sparse.c
+++ lib/image-sparse.c
@@ -36,6 +36,7 @@
 
 #include <config.h>
 #include <common.h>
+#include <cpu_func.h>
 #include <blk.h>
 #include <image-sparse.h>
 #include <div64.h>
@@ -65,6 +66,7 @@ int write_sparse_image(struct sparse_storage *info,
 	chunk_header_t *chunk_header;
 	uint32_t total_blocks = 0;
 	int fill_buf_num_blks;
+	int dcache_enabled = dcache_status();
 	int i;
 	int j;
 
@@ -108,6 +110,10 @@ int write_sparse_image(struct sparse_storage *info,
 	}
 
 	puts("Flashing Sparse Image\n");
+
+	/* Disable data cache since chunks might not be aligned */
+	if (dcache_enabled)
+		dcache_disable();
 
 	/* Start processing chunks */
 	blk = info->start;
@@ -250,6 +256,10 @@ int write_sparse_image(struct sparse_storage *info,
 			return -1;
 		}
 	}
+
+	/* Re-enable data cache it in case it was in the first place */
+	if (dcache_enabled)
+		dcache_enable();
 
 	debug("Wrote %d blocks, expected to write %d blocks\n",
 	      total_blocks, sparse_header->total_blks);
