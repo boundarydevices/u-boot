31610df4941f2bd3d36a6d2c08a4df50fec7c95a
Troy Kisky <troy.kisky@boundarydevices.com>
2017-05-03 12:02:00 -0700

    hl: initial addition, Boundary Devices board
    hl: set wlmac for TiWi
    hl: use common code for eth init
    hl: eth.c now in common directory
    hl: move misc_init_r/do_kbd to common
    hl: move mmc_init/ dram_init/ overwrite_console/ common_board_init/ splash_screen_prepare/ board_cfb_skip to common
    hl: default to tm070jdhg30 instead of hannstar7
    hl: add  CONFIG_SPI_FLASH_GIGADEVICE: to defconfigs
    hl: 800mhz_2x256mx16.cfg: remove _P1 accesses
    hl: 800mhz_2x256mx16.cfg: use common values
    
    MDCFG2: from 0x01FF0093 to 0x01FF00db
    tRTP - from 3 to 4 clocks (4 is minimum allowed)
    tWTR - from 3 to 4 clocks (4 is minimum allowed)
    
    MR0: from 0x13208030 to 0x15208030
    tWR - from 5 to 6 clocks
    
    hl: use common ddr script
    hl: port to v2018.07
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

---
+++ include/configs/hl.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2010-2011 Freescale Semiconductor, Inc.
+ *
+ * Configuration settings for the Boundary Devices Nitrogen6X-Lite
+ * board.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include "mx6_common.h"
+
+#define CONFIG_MACH_TYPE	3771
+
+/* Size of malloc() pool */
+#define CONFIG_SYS_MALLOC_LEN		(40 * 1024 * 1024)
+
+#define CONFIG_VIDEO_LOGO
+
+#define CONFIG_SYS_FSL_USDHC_NUM	1
+#define CONFIG_USB_MAX_CONTROLLER_COUNT 2
+#define BD_I2C_MASK	7
+
+#include "boundary.h"
+#define CONFIG_EXTRA_ENV_SETTINGS BD_BOUNDARY_ENV_SETTINGS \
+
+#endif	       /* __CONFIG_H */
