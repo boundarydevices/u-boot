0003aee09f476f8f0e2ae3c0b522e2d88fc901bc
Troy Kisky <troy.kisky@boundarydevices.com>
2017-07-18 20:50:32 -0700

    neol: initial addition, Boundary Devices board
    neol: move misc_init_r/do_kbd to common
    
    neol: eth.c now in common directory
    neol: initialize GP_5V_DLP_EN low
    neol: move mmc_init/ dram_init/ overwrite_console/ common_board_init/ splash_screen_prepare/ board_cfb_skip to common
    Revert "neol: initialize GP_5V_DLP_EN low"
    neol: use EIM_DA7 as GP_5V_DLP_EN
    neol: use common 1066mhz_4x256mx16.cfg
    neol: leave  GP_5V_DLP_EN high for early boards
    neol: add  CONFIG_SPI_FLASH_GIGADEVICE: to defconfigs
    neol: drive GP_5V_DLP_EN_REV0 high, don't rely on pull-up
    neol: use common ddr script
    neol: port to v2018.07
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>
    
    neol: fix GP_5V_DLP_EN_REV0 definition
    
    Signed-off-by: Stefan Becker <sbecker@neolitics.com>

---
+++ include/configs/neol.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2015 Boundary Devices, Inc.
+ *
+ * Configuration settings for the Boundary Devices Neol
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include "mx6_common.h"
+
+#define CONFIG_MACH_TYPE	3778
+
+#define CONFIG_IMX_HDMI
+#define CONFIG_SYS_FSL_USDHC_NUM	2
+#define CONFIG_USB_MAX_CONTROLLER_COUNT 2
+#define BD_I2C_MASK	7
+
+#include "boundary.h"
+#define CONFIG_EXTRA_ENV_SETTINGS BD_BOUNDARY_ENV_SETTINGS \
+
+#endif	       /* __CONFIG_H */
