115eee39abbd49c5f05cbfff14f36aa5f649df22
Troy Kisky <troy.kisky@boundarydevices.com>
2017-07-18 19:18:07 -0700

    boundary: add eth.c/padctrl.h
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

---
+++ board/boundary/padctrl.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2017, Boundary Devices <info@boundarydevices.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#ifdef CONFIG_MX6SX
+#include "padctrl-mx6sx.h"
+#elif defined(CONFIG_MX7D)
+#include "padctrl-mx7d.h"
+#elif defined(CONFIG_MX51)
+#include "padctrl-mx51.h"
+#else
+#include "padctrl-mx6.h"
+#endif
