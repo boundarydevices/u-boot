5b5b28ec8f6a173d02235e5afea07f2705f60a95
Troy Kisky <troy.kisky@boundarydevices.com>
2017-09-11 17:02:58 -0700

    ash2: initial addition, Boundary Devices Board
    ash2: KEY_COL0 must be UART4_TX_DATA
    ash2: add power_init_board because mdelay cannot be used in board_early_init_f
    ash2: use common 800mhz_4x128mx16.cfg, and calibrate ddr
    ash2: rgb24 vs rgb66 panel have different backlight active levels
    ash2: fixup dtb backlight levels
    ash2: add  CONFIG_SPI_FLASH_GIGADEVICE: to defconfigs
    ash2: use common ddr script
    ash2: port to v2018.07
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

---
+++ include/configs/ash2.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2010-2011 Freescale Semiconductor, Inc.
+ *
+ * Configuration settings for the Boundary Devices Ash
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include "mx6_common.h"
+
+#define CONFIG_MACH_TYPE	3769
+
+#define CONFIG_MXC_SPI_DISPLAY
+#define CONFIG_VIDEO_SKIP_VERSION
+
+#define CONFIG_IMX_HDMI
+#define CONFIG_SYS_FSL_USDHC_NUM	1
+#define CONFIG_USB_MAX_CONTROLLER_COUNT 2
+#define BD_I2C_MASK	7
+
+#include "boundary.h"
+#define CONFIG_EXTRA_ENV_SETTINGS BD_BOUNDARY_ENV_SETTINGS \
+
+#endif	       /* __CONFIG_H */
