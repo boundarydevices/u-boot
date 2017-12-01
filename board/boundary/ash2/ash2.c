62492f81b41009384bbca66cf18ab26417b5c066
Troy Kisky <troy.kisky@boundarydevices.com>
2017-12-01 10:57:27 -0700

    ash2: add power_init_board because mdelay cannot be used in board_early_init_f
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- board/boundary/ash2/ash2.c
+++ board/boundary/ash2/ash2.c
@@ -326,6 +326,13 @@ static const struct i2c_pads_info i2c_pads[] = {
 };
 #define I2C_BUS_CNT	3
 
+int power_init_board(void)
+{
+	mdelay(3);
+	SETUP_IOMUX_PADS(sd3_usdhc3_pads);
+	return 0;
+}
+
 #ifdef CONFIG_USB_EHCI_MX6
 int board_ehci_hcd_init(int port)
 {
@@ -465,8 +472,6 @@ int board_early_init_f(void)
 	set_gpios(gpios_out_low, ARRAY_SIZE(gpios_out_low), 0);
 	SETUP_IOMUX_PADS(sd3_gpio_pads);
 	SETUP_IOMUX_PADS(init_pads);
-	mdelay(3);
-	SETUP_IOMUX_PADS(sd3_usdhc3_pads);
 	return 0;
 }
 
