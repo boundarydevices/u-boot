a6cf3039b8c61b39ddc49a12051614f5d9966ff3
Ye Li <ye.li@nxp.com>
2017-12-14 19:56:40 -0600

    MLK-17210 usb: imx8m: Add PU power on/off to USB
    ATF will power off all PUs at default, so for USB, we enable
    its PU power for both host and device modes in board_usb_init and
    disable the power when usb is stop in board_usb_cleanup.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Li Jun <jun.li@nxp.com>

--- arch/arm/mach-imx/imx8m/soc.c
+++ arch/arm/mach-imx/imx8m/soc.c
@@ -1371,3 +1371,22 @@ enum env_location env_get_location(enum env_operation op, int prio)
 }
 
 #endif
+
+#ifdef CONFIG_USB_XHCI_IMX8M
+#define FSL_SIP_GPC                    0xC2000000
+#define FSL_SIP_CONFIG_GPC_PM_DOMAIN   0x03
+int imx8m_usb_power(int usb_id, bool on)
+{
+	unsigned long ret;
+
+	if (usb_id > 1)
+		return -EINVAL;
+
+	ret = call_imx_sip(FSL_SIP_GPC,
+			FSL_SIP_CONFIG_GPC_PM_DOMAIN, 2 + usb_id, on, 0);
+	if (ret)
+		return -EPERM;
+
+	return 0;
+}
+#endif
