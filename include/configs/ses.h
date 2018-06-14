fc5af2acab8f69a832f13542687f5d1d8d2a3a77
Troy Kisky <troy.kisky@boundarydevices.com>
2018-06-14 15:03:30 -0700

    ses: initial addition, Boundary Devices board
    ses: increase CONFIG_SYS_MALLOC_LEN to 40MB for 1080p display
    ses: port to v2018.07
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

---
+++ include/configs/ses.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2018, Boundary Devices <info@boundarydevices.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include "mx6_common.h"
+
+/* Size of malloc() pool */
+#define CONFIG_SYS_MALLOC_LEN		(40 * 1024 * 1024)
+
+#define CONFIG_SYS_FSL_USDHC_NUM	2
+#define CONFIG_USB_MAX_CONTROLLER_COUNT 2
+#define BD_I2C_MASK	7
+
+#include "boundary.h"
+#define CONFIG_EXTRA_ENV_SETTINGS BD_BOUNDARY_ENV_SETTINGS \
+
+#endif	       /* __CONFIG_H */
