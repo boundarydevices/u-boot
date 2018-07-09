a963771ec4f5f0a76eada2041a5769855bb85f3c
Troy Kisky <troy.kisky@boundarydevices.com>
2018-07-09 12:05:06 -0700

    mach-imx: video: call imx8m_fb_init if CONFIG_VIDEO_IMXDCSS
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- arch/arm/mach-imx/video.c
+++ arch/arm/mach-imx/video.c
@@ -4,6 +4,9 @@
 #include <env.h>
 #include <linux/errno.h>
 #include <asm/mach-imx/video.h>
+#ifdef CONFIG_VIDEO_IMXDCSS
+#include <asm/arch/video_common.h>
+#endif
 
 #ifdef CONFIG_IMX_HDMI
 #include <asm/arch/mxc_hdmi.h>
@@ -44,8 +47,13 @@ int board_video_skip(void)
 	}
 
 	if (i < display_count) {
+#if defined(CONFIG_VIDEO_IPUV3)
 		ret = ipuv3_fb_init(&displays[i].mode, displays[i].di ? 1 : 0,
 				    displays[i].pixfmt);
+#elif defined(CONFIG_VIDEO_IMXDCSS)
+		ret = imx8m_fb_init(&displays[i].mode, displays[i].bus,
+					displays[i].pixfmt);
+#endif
 		if (!ret) {
 			if (displays[i].enable)
 				displays[i].enable(displays + i);
