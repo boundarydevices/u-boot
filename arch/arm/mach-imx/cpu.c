72ff8eb1b492db0564a539ba7c564de5bd8bd629
Troy Kisky <troy.kisky@boundarydevices.com>
2018-07-11 17:04:14 -0700

    mach-imx: cpu: call imx8m_fb_disable in arch_preboot_os
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- arch/arm/mach-imx/cpu.c
+++ arch/arm/mach-imx/cpu.c
@@ -15,6 +15,9 @@
 #include <asm/arch/clock.h>
 #include <asm/arch/sys_proto.h>
 #include <asm/arch/crm_regs.h>
+#if defined(CONFIG_VIDEO_IMXDCSS)
+#include <asm/arch/video_common.h>
+#endif
 #include <asm/mach-imx/boot_mode.h>
 #include <imx_thermal.h>
 #include <ipu_pixfmt.h>
@@ -305,6 +308,9 @@ void arch_preboot_os(void)
 #endif
 #if defined(CONFIG_VIDEO_MXS)
 	lcdif_power_down();
+#endif
+#if defined(CONFIG_VIDEO_IMXDCSS)
+	imx8m_fb_disable();
 #endif
 }
 
