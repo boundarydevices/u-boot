ba077398576b75ad6011aa4d4ddf7eb1761ca0f4
Troy Kisky <troy.kisky@boundarydevices.com>
2018-07-27 13:25:05 -0700

    env: add newline to prevent run on messages
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- env/env.c
+++ env/env.c
@@ -190,7 +190,7 @@ int env_load(void)
 		if (!env_has_inited(drv->location))
 			continue;
 
-		printf("Loading Environment from %s... ", drv->name);
+		printf("Loading Environment from %s...\n", drv->name);
 		/*
 		 * In error case, the error message must be printed during
 		 * drv->load() in some underlying API, and it must be exactly
@@ -240,7 +240,7 @@ int env_reload(void)
 	if (drv) {
 		int ret;
 
-		printf("Loading Environment from %s... ", drv->name);
+		printf("Loading Environment from %s...\n", drv->name);
 
 		if (!env_has_inited(drv->location)) {
 			printf("not initialized\n");
