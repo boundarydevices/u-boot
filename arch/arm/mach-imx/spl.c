84639f6a18f3cdb1f8b04b8fec545eee7c3dcf4d
Troy Kisky <troy.kisky@boundarydevices.com>
2018-12-11 18:15:29 -0800

    imx-common: spl: fix spl_boot_device for imx8mm
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- arch/arm/mach-imx/spl.c
+++ arch/arm/mach-imx/spl.c
@@ -167,6 +167,11 @@ u32 spl_boot_device(void)
 	case SD2_BOOT:
 	case MMC2_BOOT:
 		return BOOT_DEVICE_MMC2;
+#if defined(CONFIG_IMX8MM)
+	case SD3_BOOT:
+	case MMC3_BOOT:
+		return BOOT_DEVICE_MMC3;
+#endif
 #endif
 	case NAND_BOOT:
 		return BOOT_DEVICE_NAND;
