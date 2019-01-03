300c2cc2139d1b6b7cb8b52c1c52336a9eaf1e1c
Patrick Bruenn <p.bruenn@beckhoff.com>
2019-01-03 07:54:32 +0100

    mmc: fsl_esdhc: add compatible for fsl, imx53-esdhc
    Add compatible "fsl,imx53-esdhc" to keep mmc working on i.MX53 platforms
    with CONFIG_DM_MMC=y
    
    Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

--- drivers/mmc/fsl_esdhc.c
+++ drivers/mmc/fsl_esdhc.c
@@ -1767,6 +1767,7 @@ static struct esdhc_soc_data usdhc_imx8qm_data = {
 };
 
 static const struct udevice_id fsl_esdhc_ids[] = {
+	{ .compatible = "fsl,imx53-esdhc", },
 	{ .compatible = "fsl,imx6ul-usdhc", },
 	{ .compatible = "fsl,imx6sx-usdhc", },
 	{ .compatible = "fsl,imx6sl-usdhc", },
