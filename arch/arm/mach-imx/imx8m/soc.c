46c6ed63c0ff44ed5388f671cc9f7d239e61eec1
Troy Kisky <troy.kisky@boundarydevices.com>
2019-01-23 12:12:37 -0800

    mach-imx: imx8m: soc: imx8m_usb_power is needed for imx8mm too
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- arch/arm/mach-imx/imx8m/soc.c
+++ arch/arm/mach-imx/imx8m/soc.c
@@ -419,7 +419,7 @@ int arch_misc_init(void)
 }
 #endif
 
-#ifdef CONFIG_USB_XHCI_IMX8M
+#if defined(CONFIG_USB_XHCI_IMX8M) || defined(CONFIG_USB_EHCI_MX7)
 #define FSL_SIP_GPC			0xC2000000
 #define FSL_SIP_CONFIG_GPC_PM_DOMAIN	0x03
 int imx8m_usb_power(int usb_id, bool on)
