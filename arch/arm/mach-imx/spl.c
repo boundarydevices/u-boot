nxp-imx/uboot-imx

MLK-16758-4 SPL: Add HAB image authentication to FIT
Introduce two board level callback functions to FIT image loading process, and
a SPL_FIT_FOUND flag to differentiate FIT image or RAW image.

Implement functions in imx common SPL codes to call HAB funtion
to authenticate the FIT image. Generally, we have to sign multiple regions
in FIT image:
1. Sign FIT FDT data (configuration)
2. Sign FIT external data (Sub-images)

Because the CSF supports to sign multiple memory blocks, so that we can use one
signature to cover all regions in FIT image and only authenticate once.
The authentication should be done after the entire FIT image is loaded into
memory including all sub-images.
We use "-p" option to generate FIT image to reserve a space for FIT IVT
and FIT CSF, also this help to fix the offset of the external data (u-boot-nodtb.bin,
ATF, u-boot DTB).

The signed FIT image layout is as below:
--------------------------------------------------
|     |     |     |   |           |     |        |
| FIT | FIT | FIT |   | U-BOOT    | ATF | U-BOOT |
| FDT | IVT | CSF |   | nodtb.bin |     |   DTB  |
|     |     |     |   |           |     |        |
--------------------------------------------------

Signed-off-by: Ye Li <ye.li@nxp.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>

arch/arm/imx-common/spl.c
@@ -161,15 +161,37 @@ __weak void __noreturn jump_to_image_no_args(struct spl_image_info *spl_image)
 
 	debug("image entry point: 0x%lX\n", spl_image->entry_point);
 
-	/* HAB looks for the CSF at the end of the authenticated data therefore,
-	 * we need to subtract the size of the CSF from the actual filesize */
-	if (authenticate_image(spl_image->load_addr,
-			       spl_image->size - CONFIG_CSF_SIZE)) {
+	if (spl_image->flags & SPL_FIT_FOUND) {
 		image_entry();
 	} else {
+		/* HAB looks for the CSF at the end of the authenticated data therefore,
+		 * we need to subtract the size of the CSF from the actual filesize */
+		if (authenticate_image(spl_image->load_addr,
+				       spl_image->size - CONFIG_CSF_SIZE)) {
+			image_entry();
+		} else {
+			puts("spl: ERROR:  image authentication unsuccessful\n");
+			hang();
+		}
+	}
+}
+
+ulong board_spl_fit_size_align(ulong size)
+{
+	/* HAB authenticate_image requests the IVT offset is aligned to 0x1000 */
+#define ALIGN_SIZE		0x1000
+
+	size = ALIGN(size, ALIGN_SIZE);
+	size += CONFIG_CSF_SIZE;
+
+	return size;
+}
+
+void board_spl_fit_post_load(ulong load_addr, size_t length)
+{
+	if (!authenticate_image(load_addr, length - CONFIG_CSF_SIZE)) {
 		puts("spl: ERROR:  image authentication unsuccessful\n");
 		hang();
 	}
 }
-
 #endif

boundarydevices/u-boot

arch/arm/mach-imx/spl.c
@@ -255,19 +255,41 @@ __weak void __noreturn jump_to_image_no_args(struct spl_image_info *spl_image)
 
 	debug("image entry point: 0x%lX\n", spl_image->entry_point);
 
-	/* HAB looks for the CSF at the end of the authenticated data therefore,
-	 * we need to subtract the size of the CSF from the actual filesize */
-	offset = spl_image->size - CONFIG_CSF_SIZE;
-	if (!imx_hab_authenticate_image(spl_image->load_addr,
-					offset + IVT_SIZE + CSF_PAD_SIZE,
-					offset)) {
+	if (spl_image->flags & SPL_FIT_FOUND) {
 		image_entry();
 	} else {
+		/* HAB looks for the CSF at the end of the authenticated data therefore,
+		 * we need to subtract the size of the CSF from the actual filesize */
+		offset = spl_image->size - CONFIG_CSF_SIZE;
+		if (!imx_hab_authenticate_image(spl_image->load_addr,
+					offset + IVT_SIZE + CSF_PAD_SIZE,
+					offset)) {
+			image_entry();
+		} else {
+			puts("spl: ERROR:  image authentication unsuccessful\n");
+			hang();
+		}
+	}
+}
+
+ulong board_spl_fit_size_align(ulong size)
+{
+	/* HAB authenticate_image requests the IVT offset is aligned to 0x1000 */
+#define ALIGN_SIZE		0x1000
+
+	size = ALIGN(size, ALIGN_SIZE);
+	size += CONFIG_CSF_SIZE;
+
+	return size;
+}
+
+void board_spl_fit_post_load(ulong load_addr, size_t length)
+{
+	if (!imx_hab_authenticate_image(load_addr, length - CONFIG_CSF_SIZE)) {
 		puts("spl: ERROR:  image authentication unsuccessful\n");
 		hang();
 	}
 }
-
 #endif
 
 #if defined(CONFIG_MX6) && defined(CONFIG_SPL_OS_BOOT)