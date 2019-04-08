6634fd5b83081376723a44bdd8a11429769b8bde
Troy Kisky <troy.kisky@boundarydevices.com>
2019-04-08 16:31:38 -0700

    nitrogen8mm: keep camera in reset
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- board/boundary/nitrogen8mm/nitrogen8mm.c
+++ board/boundary/nitrogen8mm/nitrogen8mm.c
@@ -51,8 +51,13 @@ static iomux_v3_cfg_t const init_pads[] = {
 #define GP_TC358762_EN		IMX_GPIO_NR(1, 9)
 #define GP_I2C2_SN65DSI83_EN	IMX_GPIO_NR(1, 9)
 #define GP_MIPI_RESET		IMX_GPIO_NR(1, 9)
-	IMX8MM_PAD_GPIO1_IO09_GPIO1_IO9 | MUX_PAD_CTRL(0x26),
+	IMX8MM_PAD_GPIO1_IO09_GPIO1_IO9 | MUX_PAD_CTRL(0x06),
 	IMX8MM_PAD_SAI2_RXC_GPIO4_IO22 | MUX_PAD_CTRL(PAD_CTL_DSE1 | PAD_CTL_ODE),
+
+#define GP_CSI1_MIPI_PWDN	IMX_GPIO_NR(1, 3)
+	IMX8MM_PAD_GPIO1_IO03_GPIO1_IO3 | MUX_PAD_CTRL(0x141),
+#define GP_CSI1_MIPI_RESET	IMX_GPIO_NR(1, 4)
+	IMX8MM_PAD_GPIO1_IO04_GPIO1_IO4 | MUX_PAD_CTRL(0x101),
 
 	/* pcie */
 #define GP_PCIE0_RESET		IMX_GPIO_NR(4, 31)
@@ -182,6 +187,8 @@ int board_init(void)
 	gpio_request(GPIRQ_GT911, "gt911_irq");
 	gpio_request(GP_LTK08_MIPI_EN, "lkt08_mipi_en");
 	gpio_direction_output(GP_GT911_RESET, 0);
+	gpio_direction_output(GP_CSI1_MIPI_PWDN, 1);
+	gpio_direction_output(GP_CSI1_MIPI_RESET, 0);
 #ifdef CONFIG_MXC_SPI
 	setup_spi();
 #endif
