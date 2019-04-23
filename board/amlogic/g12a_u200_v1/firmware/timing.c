a16ed70ec67faa8e9ae9873e4a9be7001370c710
cheng.chen <cheng.chen@amlogic.com>
2019-04-23 15:35:13 +0800

    boottime: reduce dram init time [1/1]
    PD#SWPL-7321
    
    Problem:
    1. openlinux g12a timing.c should be update.
    2. all timing groups in timing.c should be update.
    
    Solution:
    add bitTimeControl_2d in all timing groups
    
    Verify:
    test pass on sm1_ac200
    
    Change-Id: I5efccb7048606c7d1be88933fc5150c8662fde4e
    Signed-off-by: cheng.chen <cheng.chen@amlogic.com>

--- board/amlogic/g12a_u200_v1/firmware/timing.c
+++ board/amlogic/g12a_u200_v1/firmware/timing.c
@@ -128,6 +128,7 @@ ddr_set_t __ddr_setting[] = {
 	.pll_ssc_mode			= (1<<20) | (1<<8) | (2<<4) | 0,//center_ssc_1000ppm
 	.ddr_func				= DDR_FUNC,
 	.magic					= DRAM_CFG_MAGIC,
+	.bitTimeControl_2d		= 1,
 },
 {
 	/* g12a skt (u209) ddr3 */
@@ -209,6 +210,7 @@ ddr_set_t __ddr_setting[] = {
 	.pll_ssc_mode			= (1<<20) | (1<<8) | (2<<4) | 0,//center_ssc_1000ppm
 	.ddr_func				= DDR_FUNC,
 	.magic					= DRAM_CFG_MAGIC,
+	.bitTimeControl_2d		= 1,
 },
 {
 	/* g12a skt (u209) lpddr4 */
@@ -283,6 +285,7 @@ ddr_set_t __ddr_setting[] = {
 	.ddr_func				= DDR_FUNC,
 	.magic					= DRAM_CFG_MAGIC,
 	.diagnose				= CONFIG_DIAGNOSE_DISABLE,
+	.bitTimeControl_2d		= 1,
 },
 {
 	/* g12a Y2 dongle */
@@ -354,6 +357,7 @@ ddr_set_t __ddr_setting[] = {
 	.pll_ssc_mode			= (1<<20) | (1<<8) | (2<<4) | 0,//center_ssc_1000ppm
 	.ddr_func				= DDR_FUNC,
 	.magic					= DRAM_CFG_MAGIC,
+	.bitTimeControl_2d		= 1,
 },
 {
 	/* lpddr3 */
@@ -414,6 +418,7 @@ ddr_set_t __ddr_setting[] = {
 	.ddr_func				= DDR_FUNC,
 	.magic					= DRAM_CFG_MAGIC,
 	.diagnose				= CONFIG_DIAGNOSE_DISABLE,
+	.bitTimeControl_2d		= 1,
 },
 };
 
