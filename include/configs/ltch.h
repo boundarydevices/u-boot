4bdbc1bac832ce86824f1adf4cdbe443ea87aaa2
Troy Kisky <troy.kisky@boundarydevices.com>
2019-07-30 09:53:35 -0700

    ltch: boot order: boot from USB 1st
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- include/configs/ltch.h
+++ include/configs/ltch.h
@@ -16,6 +16,11 @@
 #define CONFIG_USB_MAX_CONTROLLER_COUNT 2
 #define BD_I2C_MASK	7
 
+#define BOOT_TARGET_DEVICES(func) \
+	DISTRO_BOOT_DEV_USB(func) \
+	DISTRO_BOOT_DEV_MMC(func) \
+	DISTRO_BOOT_DEV_SATA(func)
+
 #include "boundary.h"
 #define CONFIG_EXTRA_ENV_SETTINGS BD_BOUNDARY_ENV_SETTINGS \
 	"usb_connect_wait=3000\0" \
