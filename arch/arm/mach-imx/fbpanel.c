3e9c69748b7d241eda7f2c3bb8ec6aa4d68dd64a
Troy Kisky <troy.kisky@boundarydevices.com>
2019-11-13 09:39:47 -0800

    fbpanel: add CONFIG_IMX8MN checks
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- arch/arm/mach-imx/fbpanel.c
+++ arch/arm/mach-imx/fbpanel.c
@@ -1640,7 +1640,7 @@ static int init_display(const struct display_info_t *di)
 	ret = 0;
 	if (di->fbtype == FB_HDMI)
 		imx8m_fb_init(&di->mode, 0, di->pixfmt);
-#elif defined(CONFIG_IMX8MM)
+#elif defined(CONFIG_IMX8MM) || defined(CONFIG_IMX8MN)
 	ret = 0;
 #else
 	ret = 0;
@@ -1718,7 +1718,7 @@ static int do_fbpanel(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	ret = init_display(di);
 	if (ret)
 		return ret;
-#elif defined(CONFIG_IMX8MM)
+#elif defined(CONFIG_IMX8MM) || defined(CONFIG_IMX8MN)
 	return 0;
 #elif !defined(CONFIG_MX6SX) && !defined(CONFIG_MX7D)
 	ipuv3_fb_shutdown();
