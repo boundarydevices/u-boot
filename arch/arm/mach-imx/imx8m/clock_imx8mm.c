nxp-imx/uboot-imx

MLK-21889-1 imx8mn: Add new SoC iMX8M Nano
Introduce a new SoC kconfig CONFIG_IMX8MN.
Since the chip is derived from iMX8MM, we reuse the clocks and registers
mapping files of iMX8MM. The iomux header file is the latest v0.03
generated by tool and add SION for i2c and usdhc3.

Signed-off-by: Ye Li <ye.li@nxp.com>
Signed-off-by: Jacky Bai <ping.bai@nxp.com>

arch/arm/mach-imx/imx8m/clock_imx8mm.c
@@ -253,6 +253,7 @@ u32 decode_fracpll(enum clk_root_src frac_pll)
 }
 
 enum intpll_out_freq {
+	INTPLL_OUT_600M,
 	INTPLL_OUT_750M,
 	INTPLL_OUT_800M,
 	INTPLL_OUT_1200M,
@@ -504,6 +505,11 @@ int intpll_configure(enum pll_clocks pll, enum intpll_out_freq freq)
 	};
 
 	switch (freq) {
+	case INTPLL_OUT_600M:
+		/* 24 * 0x12c / 3 / 2 ^ 2 */
+		pll_div_ctl_val = INTPLL_MAIN_DIV_VAL(0x12c) |
+			INTPLL_PRE_DIV_VAL(3) | INTPLL_POST_DIV_VAL(2);
+		break;
 	case INTPLL_OUT_750M:
 		/* 24 * 0xfa / 2 / 2 ^ 2 */
 		pll_div_ctl_val = INTPLL_MAIN_DIV_VAL(0xfa) |
@@ -579,7 +585,12 @@ void mxs_set_lcdclk(uint32_t base_addr, uint32_t freq)
 find:
 	/* Select to video PLL */
 	debug("mxs_set_lcdclk, pre = %d, post = %d\n", pre, post);
+
+#ifdef CONFIG_IMX8MN
+	clock_set_target_val(DISPLAY_PIXEL_CLK_ROOT, CLK_ROOT_ON | CLK_ROOT_SOURCE_SEL(1) | CLK_ROOT_PRE_DIV(pre - 1) | CLK_ROOT_POST_DIV(post - 1));
+#else
 	clock_set_target_val(LCDIF_PIXEL_CLK_ROOT, CLK_ROOT_ON | CLK_ROOT_SOURCE_SEL(1) | CLK_ROOT_PRE_DIV(pre - 1) | CLK_ROOT_POST_DIV(post - 1));
+#endif
 
 }
 
@@ -600,8 +611,11 @@ void enable_display_clk(unsigned char enable)
 		clock_set_target_val(MIPI_DSI_CORE_CLK_ROOT, CLK_ROOT_ON | CLK_ROOT_SOURCE_SEL(1));
 
 		/* 27Mhz MIPI DPHY PLL ref from video PLL */
+#ifdef CONFIG_IMX8MN
+		clock_set_target_val(DISPLAY_DSI_PHY_REF_CLK_ROOT, CLK_ROOT_ON | CLK_ROOT_SOURCE_SEL(7) |CLK_ROOT_POST_DIV(CLK_ROOT_POST_DIV22));
+#else
 		clock_set_target_val(MIPI_DSI_PHY_REF_CLK_ROOT, CLK_ROOT_ON | CLK_ROOT_SOURCE_SEL(7) |CLK_ROOT_POST_DIV(CLK_ROOT_POST_DIV22));
-
+#endif
 		clock_enable(CCGR_DISPMIX, true);
 	} else {
 		clock_enable(CCGR_DISPMIX, false);
@@ -644,7 +658,10 @@ int clock_init()
 			     CLK_ROOT_SOURCE_SEL(1) | \
 			     CLK_ROOT_POST_DIV(CLK_ROOT_POST_DIV1));
 
-	intpll_configure(ANATOP_SYSTEM_PLL3, INTPLL_OUT_750M);
+	if (is_imx8mn())
+		intpll_configure(ANATOP_SYSTEM_PLL3, INTPLL_OUT_600M);
+	else
+		intpll_configure(ANATOP_SYSTEM_PLL3, INTPLL_OUT_750M);
 	clock_set_target_val(NOC_CLK_ROOT, CLK_ROOT_ON | CLK_ROOT_SOURCE_SEL(2));
 
 	/* config GIC to sys_pll2_100m */

boundarydevices/u-boot

Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

arch/arm/mach-imx/imx8m/clock_imx8mm.c
@@ -251,6 +251,7 @@ static u32 decode_fracpll(enum clk_root_src frac_pll)
 }
 
 enum intpll_out_freq {
+	INTPLL_OUT_600M,
 	INTPLL_OUT_750M,
 	INTPLL_OUT_800M,
 	INTPLL_OUT_1200M,
@@ -471,6 +472,11 @@ int intpll_configure(enum pll_clocks clock, enum intpll_out_freq freq)
 	}
 
 	switch (freq) {
+	case INTPLL_OUT_600M:
+		/* 24 * 0x12c / 3 / 2 ^ 2 */
+		div_ctl_val = INTPLL_MAIN_DIV_VAL(0x12c) |
+			INTPLL_PRE_DIV_VAL(3) | INTPLL_POST_DIV_VAL(2);
+		break;
 	case INTPLL_OUT_750M:
 		/* 24 * 0xfa / 2 / 2 ^ 2 */
 		div_ctl_val = INTPLL_MAIN_DIV_VAL(0xfa) |
@@ -538,8 +544,10 @@ void enable_display_clk(unsigned char enable)
 
 		clock_set_target_val(MIPI_DSI_CORE_CLK_ROOT, CLK_ROOT_ON | CLK_ROOT_SOURCE_SEL(1));
 
-#ifdef CONFIG_IMX8MM
+#ifdef CONFIG_IMX8MN
 		/* 27Mhz MIPI DPHY PLL ref from video PLL */
+		clock_set_target_val(DISPLAY_DSI_PHY_REF_CLK_ROOT, CLK_ROOT_ON | CLK_ROOT_SOURCE_SEL(7) |CLK_ROOT_POST_DIV(CLK_ROOT_POST_DIV22));
+#else
 		clock_set_target_val(MIPI_DSI_PHY_REF_CLK_ROOT, CLK_ROOT_ON | CLK_ROOT_SOURCE_SEL(7) |CLK_ROOT_POST_DIV(CLK_ROOT_POST_DIV22));
 #endif
 
@@ -621,7 +629,10 @@ int clock_init(void)
 		INTPLL_DIV20_CLKE_MASK;
 	writel(val_cfg0, &ana_pll->sys_pll2.gnrl_ctl);
 
-	intpll_configure(ANATOP_SYSTEM_PLL3, INTPLL_OUT_750M);
+	if (is_imx8mn())
+		intpll_configure(ANATOP_SYSTEM_PLL3, INTPLL_OUT_600M);
+	else
+		intpll_configure(ANATOP_SYSTEM_PLL3, INTPLL_OUT_750M);
 	clock_set_target_val(NOC_CLK_ROOT, CLK_ROOT_ON | CLK_ROOT_SOURCE_SEL(2));
 
 	/* config GIC to sys_pll2_100m */
@@ -733,7 +744,9 @@ void mxs_set_lcdclk(uint32_t base_addr, uint32_t freq)
 find:
 	/* Select to video PLL */
 	debug("mxs_set_lcdclk, pre = %d, post = %d\n", pre, post);
-#ifdef CONFIG_IMX8MM
+#ifdef CONFIG_IMX8MN
+	clock_set_target_val(DISPLAY_PIXEL_CLK_ROOT, CLK_ROOT_ON | CLK_ROOT_SOURCE_SEL(1) | CLK_ROOT_PRE_DIV(pre - 1) | CLK_ROOT_POST_DIV(post - 1));
+#else
 	clock_set_target_val(LCDIF_PIXEL_CLK_ROOT, CLK_ROOT_ON | CLK_ROOT_SOURCE_SEL(1) | CLK_ROOT_PRE_DIV(pre - 1) | CLK_ROOT_POST_DIV(post - 1));
 #endif
 