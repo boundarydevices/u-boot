ee6e43d80cdb6a0a090d3c1e3f143847e5c92ef8
Ye Li <ye.li@nxp.com>
2020-05-09 01:48:57 -0700

    MLK-23964-5 arm: bootm: disable video link before booting kernel
    For ARM platforms which enabled video link, shut down it before entering
    into kernel.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>

--- arch/arm/lib/bootm.c
+++ arch/arm/lib/bootm.c
@@ -33,6 +33,7 @@
 #include <bootm.h>
 #include <vxworks.h>
 #include <asm/cache.h>
+#include <video_link.h>
 
 #ifdef CONFIG_ARMV7_NONSEC
 #include <asm/armv7.h>
@@ -108,6 +109,10 @@ static void announce_and_cleanup(int fake)
 
 #ifdef CONFIG_USB_DEVICE
 	udc_disconnect();
+#endif
+
+#if defined(CONFIG_VIDEO_LINK)
+	video_link_shut_down();
 #endif
 
 	board_quiesce_devices();
