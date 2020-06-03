214f185066acc112d410e57a361340b2ca349195
Troy Kisky <troy.kisky@boundarydevices.com>
2020-06-03 14:40:15 -0700

    nitrogen8mm_som: add mipi display DMT050WVNXCMI
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- board/boundary/nitrogen8mm_som/nitrogen8mm_som.c
+++ board/boundary/nitrogen8mm_som/nitrogen8mm_som.c
@@ -53,6 +53,7 @@ static iomux_v3_cfg_t const init_pads[] = {
 	IOMUX_PAD_CTRL(GPIO1_IO07__GPIO1_IO7, 0x49),
 
 #define GP_TC358762_EN		IMX_GPIO_NR(5, 0)
+#define GP_TC358867_EN		IMX_GPIO_NR(5, 0)
 #define GP_I2C2_SN65DSI83_EN	IMX_GPIO_NR(5, 0)
 #define GP_MIPI_RESET		IMX_GPIO_NR(5, 0)
 	IOMUX_PAD_CTRL(SAI3_TXC__GPIO5_IO0, 0x06),
@@ -143,6 +144,7 @@ int board_detect_gt911(struct display_info_t const *di)
 
 static const struct display_info_t displays[] = {
 	VD_MIPI_M101NWWB(MIPI, fbp_detect_i2c, fbp_bus_gp(3, GP_I2C2_SN65DSI83_EN, 0, 0), 0x2c, FBP_MIPI_TO_LVDS, FBTS_FT5X06),
+	VD_DMT050WVNXCMI(MIPI, fbp_detect_i2c, fbp_bus_gp(3, GP_TC358867_EN, GP_TC358867_EN, 30), 0x2f, FBP_SPI_LCD, FBTS_GOODIX),
 	VD_LTK080A60A004T(MIPI, board_detect_gt911, fbp_bus_gp(3, GP_LTK08_MIPI_EN, GP_LTK08_MIPI_EN, 0), 0x5d, FBTS_GOODIX),	/* Goodix touchscreen */
 	VD_LCM_JM430_MINI(MIPI, fbp_detect_i2c, fbp_bus_gp(3, GP_ST1633_RESET, GP_TC358762_EN, 30), fbp_addr_gp(0x55, GP_LCM_JM430_BKL_EN, 0, 0), FBTS_ST1633I),		/* Sitronix touch */
 	VD_LTK0680YTMDB(MIPI, NULL, fbp_bus_gp(3, GP_MIPI_RESET, GP_MIPI_RESET, 0), 0x5d, FBTS_GOODIX),
