c8af39a2da72c0a5447793b2ccf83c434c3a04b6
Troy Kisky <troy.kisky@boundarydevices.com>
2020-06-15 17:28:31 -0700

    common: spl: spl_mmc: add BOOT_DEVICE_MMC3 support
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- common/spl/spl_mmc.c
+++ common/spl/spl_mmc.c
@@ -124,6 +124,8 @@ static int spl_mmc_get_device_index(u32 boot_device)
 	case BOOT_DEVICE_MMC2:
 	case BOOT_DEVICE_MMC2_2:
 		return 1;
+	case BOOT_DEVICE_MMC3:
+		return 2;
 	}
 
 #ifdef CONFIG_SPL_LIBCOMMON_SUPPORT
@@ -445,4 +447,7 @@ int spl_mmc_load_image(struct spl_image_info *spl_image,
 
 SPL_LOAD_IMAGE_METHOD("MMC1", 0, BOOT_DEVICE_MMC1, spl_mmc_load_image);
 SPL_LOAD_IMAGE_METHOD("MMC2", 0, BOOT_DEVICE_MMC2, spl_mmc_load_image);
+#if defined(CONFIG_IMX8MM)||defined(CONFIG_IMX8MN)||defined(CONFIG_IMX8MP)
+SPL_LOAD_IMAGE_METHOD("MMC3", 0, BOOT_DEVICE_MMC3, spl_mmc_load_image);
+#endif
 SPL_LOAD_IMAGE_METHOD("MMC2_2", 0, BOOT_DEVICE_MMC2_2, spl_mmc_load_image);
