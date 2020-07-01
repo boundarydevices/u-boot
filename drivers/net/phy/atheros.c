a50e95a3e864c6fdcadeb5d15e0094fbe5ee2971
Troy Kisky <troy.kisky@boundarydevices.com>
2020-07-01 14:07:10 -0700

    net: phy: atheros: reduce drive strength if 0 is requested
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/net/phy/atheros.c
+++ drivers/net/phy/atheros.c
@@ -64,7 +64,8 @@ static int ar8035_config(struct phy_device *phydev)
 	phy_write(phydev, MDIO_DEVAD_NONE, 0xe, 0x8016);
 	phy_write(phydev, MDIO_DEVAD_NONE, 0xd, 0x4007);
 	regval = phy_read(phydev, MDIO_DEVAD_NONE, 0xe);
-	regval &= ~0x1c;
+	regval &= ~0x11c;
+	regval |= 0x80;	/* 1/2 drive strength */
 	freq = env_get_ulong("phy_clock_out", 10, 125000000);
 	if (freq >= 125000000) {
 		regval |= 0x18;
@@ -72,6 +73,9 @@ static int ar8035_config(struct phy_device *phydev)
 		regval |= 0x10;
 	} else if (freq >= 50000000) {
 		regval |= 0x08;
+	} else if (freq == 0) {
+		/* 1/4 drive strength since off was requested */
+		regval |= 0x180;
 	}
 	phy_write(phydev, MDIO_DEVAD_NONE, 0xe, regval);
 
