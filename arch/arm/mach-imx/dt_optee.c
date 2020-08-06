28518a70247b211b6364821aa447ea10031f832c
Troy Kisky <troy.kisky@boundarydevices.com>
2020-08-06 13:45:55 -0700

    arch: arm: mach-imx: dt_optee: test valid overlay before using
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- arch/arm/mach-imx/dt_optee.c
+++ arch/arm/mach-imx/dt_optee.c
@@ -29,12 +29,17 @@ int ft_add_optee_node(void *fdt, bd_t *bd)
 
 #ifdef CONFIG_OF_LIBFDT_OVERLAY
 	if (rom_pointer[2]) {
-		debug("OP-TEE: applying overlay on 0x%lx\n",rom_pointer[2]);
-		ret = fdt_overlay_apply_verbose(fdt, (void*)rom_pointer[2]);
-		if (ret == 0) {
-			debug("Overlay applied with success");
-			fdt_pack(fdt);
-			return 0;
+		void *fdto = (void *)rom_pointer[2];
+		if (fdt_magic(fdto) == FDT_MAGIC) {
+			debug("OP-TEE: applying overlay on 0x%p\n", fdto);
+			ret = fdt_overlay_apply_verbose(fdt, fdto);
+			if (ret == 0) {
+				debug("Overlay applied with success");
+				fdt_pack(fdt);
+				return 0;
+			}
+		} else {
+			printf("%s: optee dtb overlay(0x%p) is invalid\n", __func__, fdto);
 		}
 	}
 	/* Fallback to previous implementation */
