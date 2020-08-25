eae0188ff8e835fcb2351324af88998f0e4213db
Troy Kisky <troy.kisky@boundarydevices.com>
2020-08-25 14:32:02 -0700

    nitrogen8mm: add display MTD0900DCP27KF
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- board/boundary/nitrogen8mm/nitrogen8mm.c
+++ board/boundary/nitrogen8mm/nitrogen8mm.c
@@ -156,6 +156,7 @@ int board_detect_gt911(struct display_info_t const *di)
 
 static const struct display_info_t displays[] = {
 	VD_MIPI_M101NWWB(MIPI, fbp_detect_i2c, fbp_bus_gp(1, GP_I2C2_SN65DSI83_EN, 0, 0), 0x2c, FBP_MIPI_TO_LVDS, FBTS_FT5X06),
+	VD_MIPI_MTD0900DCP27KF(MIPI, fbp_detect_i2c, fbp_bus_gp(1, 0, 0, 0), 0x41, FBP_MIPI_TO_LVDS, FBTS_ILI251X),
 	VD_DMT050WVNXCMI(MIPI, fbp_detect_i2c, fbp_bus_gp(1, GP_SC18IS602B_RESET, 0, 30), fbp_addr_gp(0x2f, 0, 6, 0), FBP_SPI_LCD, FBTS_GOODIX),
 	VD_LTK080A60A004T(MIPI, board_detect_gt911, fbp_bus_gp(1, GP_LTK08_MIPI_EN, GP_LTK08_MIPI_EN, 0), 0x5d, FBTS_GOODIX),	/* Goodix touchscreen */
 	VD_LCM_JM430_MINI(MIPI, fbp_detect_i2c, fbp_bus_gp(1, GP_ST1633_RESET, GP_TC358762_EN, 30), fbp_addr_gp(0x55, GP_LCM_JM430_BKL_EN, 0, 0), FBTS_ST1633I),	/* Sitronix touch */
