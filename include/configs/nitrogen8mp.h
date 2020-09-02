7555a8fc7f108c1d818558937313afa46458be0e
Gary Bisson <gary.bisson@boundarydevices.com>
2020-09-02 13:57:04 +0200

    nitrogen8mp: fix emmc index
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- include/configs/nitrogen8mp.h
+++ include/configs/nitrogen8mp.h
@@ -185,7 +185,7 @@
 #endif
 
 #ifdef CONFIG_CMD_MMC
-#define DISTRO_BOOT_DEV_MMC(func) func(MMC, mmc, 1) func(MMC, mmc, 0)
+#define DISTRO_BOOT_DEV_MMC(func) func(MMC, mmc, 2) func(MMC, mmc, 0)
 #else
 #define DISTRO_BOOT_DEV_MMC(func)
 #endif
