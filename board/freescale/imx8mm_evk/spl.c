d3067e864b4fb57d50a9ef4638f348e3548bc0ee
Troy Kisky <troy.kisky@boundarydevices.com>
2020-10-26 14:53:52 -0700

    imx8mm_evk: s/IMX8MM_PAD_UART2_RXD_UART2_RX/IMX8MM_PAD_UART2_RXD__UART2_DCE_RX/
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- board/freescale/imx8mm_evk/spl.c
+++ board/freescale/imx8mm_evk/spl.c
@@ -67,12 +67,12 @@ int board_fit_config_name_match(const char *name)
 #define WDOG_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_ODE | PAD_CTL_PUE | PAD_CTL_PE)
 
 static iomux_v3_cfg_t const uart_pads[] = {
-	IMX8MM_PAD_UART2_RXD_UART2_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
-	IMX8MM_PAD_UART2_TXD_UART2_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
+	IMX8MM_PAD_UART2_RXD__UART2_DCE_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
+	IMX8MM_PAD_UART2_TXD__UART2_DCE_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
 };
 
 static iomux_v3_cfg_t const wdog_pads[] = {
-	IMX8MM_PAD_GPIO1_IO02_WDOG1_WDOG_B  | MUX_PAD_CTRL(WDOG_PAD_CTRL),
+	IMX8MM_PAD_GPIO1_IO02__WDOG1_WDOG_B  | MUX_PAD_CTRL(WDOG_PAD_CTRL),
 };
 
 int board_early_init_f(void)
