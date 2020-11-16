0ab430aa30fba02c6d8381c549f876daf68a6a02
Gary Bisson <gary.bisson@boundarydevices.com>
2020-11-16 14:14:44 +0100

    nitrogen8mp: fix malloc allocatable size for android boot
    Otherwise can't allocate enough memory to load boot image and check it.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- include/configs/nitrogen8mp.h
+++ include/configs/nitrogen8mp.h
@@ -85,9 +85,10 @@
 #define CONFIG_MMCROOT			"/dev/mmcblk2p2"  /* USDHC3 */
 
 /* Size of malloc() pool */
-#define CONFIG_SYS_MALLOC_LEN		SZ_32M
+#define CONFIG_SYS_MALLOC_LEN		(128 * SZ_1M)
+
 /* bootm image length (Android) */
-#define CONFIG_SYS_BOOTM_LEN		(96 * 1024 * 1024)
+#define CONFIG_SYS_BOOTM_LEN		(96 * SZ_1M)
 
 /* Totally 6GB DDR */
 #define CONFIG_SYS_SDRAM_BASE		0x40000000
