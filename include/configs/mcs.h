770fbaafd15ba5cd3cb2b4e37d25c3a41b62391f
Troy Kisky <troy.kisky@boundarydevices.com>
2020-11-18 13:51:44 -0800

    mcs: move CONFIG_PREBOOT to defconfig and add CONFIG_USE_PREBOOT
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- include/configs/mcs.h
+++ include/configs/mcs.h
@@ -12,12 +12,6 @@
 #include "mx6_common.h"
 
 #define CONFIG_MACH_TYPE	3769
-
-#define CONFIG_PREBOOT	"if itest.s  \"\" != \"$bmpsize\" ; then " \
-				"sf probe && " \
-				"sf read ${splashimage} ${splashflash} $bmpsize" \
-				" && bmp d 10008000;" \
-			"fi"
 #define CONFIG_SYS_FSL_USDHC_NUM	3
 #define CONFIG_USB_MAX_CONTROLLER_COUNT 2
 #define BD_I2C_MASK	7
