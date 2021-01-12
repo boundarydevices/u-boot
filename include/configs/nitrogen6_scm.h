77fefced2d83253568a677afab318990bd3d31eb
Gary Bisson <gary.bisson@boundarydevices.com>
2021-01-12 15:48:45 +0100

    nitrogen6_scm: remove obsolete M4 macros
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- include/configs/nitrogen6_scm.h
+++ include/configs/nitrogen6_scm.h
@@ -15,9 +15,6 @@
 #define CONFIG_ETHPRIME			"usb_ether"
 
 /* M4 specific */
-#define SYS_AUXCORE_BOOTDATA_DDR	0x9ff00000
-#define SYS_AUXCORE_BOOTDATA_OCRAM	0x00910000
-#define SYS_AUXCORE_BOOTDATA_TCM	0x007F8000
 #define EXTRA_ENV_M4 \
 	"m4image=m4_fw.bin\0" \
 	"m4offset=0x1e0000\0" \
@@ -33,7 +30,7 @@
 			"fi; " \
 		"done; " \
 		"done\0" \
-	"m4loadaddr="__stringify(CONFIG_SYS_AUXCORE_BOOTDATA_TCM)"\0" \
+	"m4loadaddr="__stringify(CONFIG_CONFIG_IMX_MCORE_TCM_ADDR)"\0" \
 	"m4boot=run m4boot_nor\0" \
 	"m4boot_ext=load ${dtype} ${disk}:1 ${m4loadaddr} ${m4image}; " \
 		"dcache flush; bootaux ${m4loadaddr}\0" \
@@ -41,7 +38,6 @@
 		"dcache flush; bootaux ${m4loadaddr}\0"
 
 #define CONFIG_PCIE_IMX_PERST_GPIO	IMX_GPIO_NR(4, 10)
-#define CONFIG_SYS_AUXCORE_BOOTDATA	0x900000	/* M4 specific */
 #define CONFIG_SYS_FSL_ESDHC_GPIO_WP
 #define CONFIG_SYS_SPD_BUS_NUM	1
 
