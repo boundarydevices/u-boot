45c9d08276a247df2c78ac83a540a9e7a4a453b4
Troy Kisky <troy.kisky@boundarydevices.com>
2021-02-04 17:55:42 -0800

    cmd: sata: make sure device is active before using
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- cmd/sata.c
+++ cmd/sata.c
@@ -57,8 +57,12 @@ int sata_probe(int devnum)
 	int rc;
 
 	rc = uclass_get_device(UCLASS_AHCI, devnum, &dev);
-	if (rc)
+	if (rc) {
 		rc = uclass_find_first_device(UCLASS_AHCI, &dev);
+		if (!rc && !(dev->flags & DM_FLAG_ACTIVATED))
+			rc = -ENODEV;
+
+	}
 	if (rc) {
 		printf("Cannot probe SATA device %d (err=%d)\n", devnum, rc);
 		return CMD_RET_FAILURE;
