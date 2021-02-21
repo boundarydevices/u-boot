28129ce1e93d67e2b8227431dd62dfc02535ae38
Troy Kisky <troy.kisky@boundarydevices.com>
2021-02-21 10:08:27 -0800

    Revert "mach-imx: cpu: call imx8m_fb_disable in arch_preboot_os"
    This reverts commit 0e6dd4e922f69348f239077159421e23b373727a.

--- arch/arm/mach-imx/cpu.c
+++ arch/arm/mach-imx/cpu.c
@@ -19,9 +19,6 @@
 #include <asm/arch/clock.h>
 #include <asm/arch/sys_proto.h>
 #include <asm/arch/crm_regs.h>
-#if defined(CONFIG_VIDEO_IMXDCSS)
-#include <asm/arch/video_common.h>
-#endif
 #include <asm/mach-imx/boot_mode.h>
 #include <imx_thermal.h>
 #include <ipu_pixfmt.h>
@@ -309,9 +306,6 @@ void arch_preboot_os(void)
 #endif
 #if defined(CONFIG_VIDEO_MXS) && !defined(CONFIG_DM_VIDEO)
 	lcdif_power_down();
-#endif
-#if defined(CONFIG_VIDEO_IMXDCSS)
-	imx8m_fb_disable();
 #endif
 }
 
