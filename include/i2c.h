f0a0d7204148983ae6ff0b5586f19844d9fa56c4
Troy Kisky <troy.kisky@boundarydevices.com>
2021-02-24 12:36:54 -0800

    i2c: i2c-uclass: remove static from i2c_probe_chip
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- include/i2c.h
+++ include/i2c.h
@@ -242,6 +242,9 @@ int dm_i2c_reg_read(struct udevice *dev, uint offset);
  */
 int dm_i2c_reg_write(struct udevice *dev, uint offset, unsigned int val);
 
+int i2c_probe_chip(struct udevice *bus, uint chip_addr,
+		   enum dm_i2c_chip_flags chip_flags);
+
 /**
  * dm_i2c_reg_clrset() - Apply bitmask to an I2C register
  *
