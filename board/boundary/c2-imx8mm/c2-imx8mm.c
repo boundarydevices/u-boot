d9867ec7a25fc6266acd1265973b8abb5a998925
Troy Kisky <troy.kisky@boundarydevices.com>
2021-03-30 17:31:36 -0700

    c2-imx8mm: add mipi display support
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- board/boundary/c2-imx8mm/c2-imx8mm.c
+++ board/boundary/c2-imx8mm/c2-imx8mm.c
@@ -46,17 +46,23 @@ static iomux_v3_cfg_t const init_pads[] = {
 #define GP_LTK08_MIPI_EN		IMX_GPIO_NR(1, 1)
 	IOMUX_PAD_CTRL(GPIO1_IO01__GPIO1_IO1, 0x146),
 
+#define GP_DLC0350_ENABLE		IMX_GPIO_NR(1, 3)
+	IOMUX_PAD_CTRL(GPIO1_IO03__GPIO1_IO3, 0x16),
+
+	/* Touch screen on J8 and J22, same pins used */
 #define GPIRQ_GT911 			IMX_GPIO_NR(5, 22)
 	IOMUX_PAD_CTRL(UART1_RXD__GPIO5_IO22, 0xd6),
 #define GP_GT911_RESET			IMX_GPIO_NR(5, 23)
-	IOMUX_PAD_CTRL(UART1_TXD__GPIO5_IO23, 0x49),
+	IOMUX_PAD_CTRL(UART1_TXD__GPIO5_IO23, 0x109),
 
 #define GP_SN65DSI83_EN		IMX_GPIO_NR(1, 9)
 #define GP_MIPI_RESET		IMX_GPIO_NR(1, 9)
+/* enable for TPS65132 Single Inductor - Dual Output Power Supply */
+#define GP_LCD133_070_ENABLE		IMX_GPIO_NR(1, 9)
 	IOMUX_PAD_CTRL(GPIO1_IO09__GPIO1_IO9, 0x06),
 	IOMUX_PAD_CTRL(SAI2_RXC__GPIO4_IO22, PAD_CTL_DSE1 | PAD_CTL_ODE),
 
-#define GP_BACKLIGHT_MIPI		IMX_GPIO_NR(5, 3)
+#define GP_BACKLIGHT_MIPI2		IMX_GPIO_NR(5, 3)
 	IOMUX_PAD_CTRL(SPDIF_TX__GPIO5_IO3, 0x116),
 
 #define GP_CSI1_OV5640_MIPI_POWER_DOWN	IMX_GPIO_NR(1, 8)
@@ -90,7 +96,7 @@ int board_early_init_f(void)
 	struct wdog_regs *wdog = (struct wdog_regs *)WDOG1_BASE_ADDR;
 
 	imx_iomux_v3_setup_multiple_pads(init_pads, ARRAY_SIZE(init_pads));
-	gpio_direction_output(GP_BACKLIGHT_MIPI, 0);
+	gpio_direction_output(GP_BACKLIGHT_MIPI2, 0);
 	gpio_request(GP_SN65DSI83_EN, "sn65en");
 	gpio_direction_output(GP_SN65DSI83_EN, 0);
 	gpio_direction_output(GP_EMMC_RESET, 1);
@@ -102,36 +108,22 @@ int board_early_init_f(void)
 #ifdef CONFIG_CMD_FBPANEL
 int board_detect_gt911(struct display_info_t const *di)
 {
-	int ret;
-	struct udevice *dev, *chip;
+	return board_detect_gt911_common(di, (di->bus_num >> 4) | 4, 0, GP_GT911_RESET, GPIRQ_GT911);
+}
 
-	if (di->bus_gp)
-		gpio_direction_output(di->bus_gp, 1);
-	gpio_set_value(GP_GT911_RESET, 0);
-	mdelay(20);
-	gpio_direction_output(GPIRQ_GT911, di->addr_num == 0x14 ? 1 : 0);
-	udelay(100);
-	gpio_set_value(GP_GT911_RESET, 1);
-	mdelay(6);
-	gpio_set_value(GPIRQ_GT911, 0);
-	mdelay(50);
-	gpio_direction_input(GPIRQ_GT911);
-	ret = uclass_get_device(UCLASS_I2C, di->bus_num, &dev);
-	if (ret)
-		return 0;
-
-	ret = dm_i2c_probe(dev, di->addr_num, 0x0, &chip);
-	if (ret && di->bus_gp)
-		gpio_direction_input(di->bus_gp);
-
-	return (ret == 0);
+void board_enable_mipi(const struct display_info_t *di, int enable)
+{
+	if (di->enable_alias[0] == FBP_BACKLIGHT_MIPI2) {
+		gpio_direction_output(GP_BACKLIGHT_MIPI2, enable);
+	}
 }
 
 static const struct display_info_t displays[] = {
-	VD_MIPI_M101NWWB(MIPI, fbp_detect_i2c, fbp_bus_gp(3, GP_SN65DSI83_EN, 0, 0), 0x2c, FBP_MIPI_TO_LVDS, FBTS_FT5X06),
-	VD_LTK080A60A004T(MIPI, board_detect_gt911, fbp_bus_gp(3, GP_LTK08_MIPI_EN, GP_LTK08_MIPI_EN, 0), 0x5d, FBTS_GOODIX),
-	VD_MIPI_1280_720M_60(MIPI, NULL, fbp_bus_gp((3 | (3 << 4)), 0, 0, 0), 0x68, FBP_PCA9546),
-	VD_MIPI_1920_1080M_60(MIPI, board_detect_pca9546, fbp_bus_gp((3 | (3 << 4)), 0, 0, 0), 0x68, FBP_PCA9546),
+	VD_MIPI_DLC0350GEV06(MIPI, board_detect_gt911, fbp_bus_gp(2, 0, GP_DLC0350_ENABLE, 0), fbp_addr_gp(0x5d, 0, 0, 0), FBTS_GOODIX),
+	VD_LTK080A60A004T(MIPI, NULL, fbp_bus_gp(2, GP_LTK08_MIPI_EN, GP_LTK08_MIPI_EN, 0), 0x5d, FBP_BACKLIGHT_MIPI2, FBTS_GOODIX),
+	VD_MIPI_M101NWWB(MIPI, fbp_detect_i2c, fbp_bus_gp(2, GP_SN65DSI83_EN, 0, 0), 0x2c, FBP_MIPI_TO_LVDS, FBTS_FT5X06),
+	VD_MIPI_1280_720M_60(MIPI, NULL, fbp_bus_gp((2 | (3 << 4)), 0, 0, 0), 0x68, FBP_PCA9546),
+	VD_MIPI_1920_1080M_60(MIPI, board_detect_pca9546, fbp_bus_gp((2 | (3 << 4)), 0, 0, 0), 0x68, FBP_PCA9546),
 };
 #define display_cnt	ARRAY_SIZE(displays)
 #else
@@ -141,14 +133,21 @@ static const struct display_info_t displays[] = {
 
 int board_init(void)
 {
+#ifndef CONFIG_DM_VIDEO
 	gpio_request(GP_SN65DSI83_EN, "sn65dsi83_enable");
 	gpio_request(GP_LTK08_MIPI_EN, "lkt08_mipi_en");
+#endif
+	gpio_request(GP_DLC0350_ENABLE, "dlc0350 reset");
+	gpio_request(GP_BACKLIGHT_MIPI2, "backlight_pwm2");
 	gpio_request(GP_GT911_RESET, "gt911_reset");
 	gpio_request(GPIRQ_GT911, "gt911_irq");
 	gpio_request(GP_CSI1_OV5640_MIPI_RESET, "csi1_mipi_reset");
+	gpio_direction_output(GP_DLC0350_ENABLE, 0);
 	gpio_direction_output(GP_CSI1_OV5640_MIPI_RESET, 0);
 	gpio_direction_output(GP_GT911_RESET, 0);
-#ifdef CONFIG_MXC_SPI
+	gpio_free(GP_DLC0350_ENABLE);
+
+#if defined(CONFIG_MXC_SPI) && !defined(CONFIG_DM_SPI)
 	setup_spi();
 #endif
 
