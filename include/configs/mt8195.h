f3339647402348f150f986a9b6809694aa14546a
Fabien Parent <fparent@baylibre.com>
2021-09-09 16:57:10 +0200

    board: mediatek: add mt8195 demo board
    Signed-off-by: Fabien Parent <fparent@baylibre.com>
    Signed-off-by: Macpaul Lin <macpaul.lin@mediatek.com>

---
+++ include/configs/mt8195.h
@@ -0,0 +1,32 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration for MT8195 based boards
+ *
+ * Copyright (C) 2021 BayLibre, SAS
+ * Author: Fabien Parent <fparent@baylibre.com
+ */
+
+#ifndef __MT8195_H
+#define __MT8195_H
+
+#include <linux/sizes.h>
+
+#define CONFIG_SYS_NS16550_SERIAL
+#define CONFIG_SYS_NS16550_REG_SIZE	-4
+#define CONFIG_SYS_NS16550_MEM32
+#define CONFIG_SYS_NS16550_COM1		0x11002000
+#define CONFIG_SYS_NS16550_CLK		26000000
+
+/* Environment settings */
+#include <config_distro_bootcmd.h>
+
+#define BOOT_TARGET_DEVICES(func) \
+	func(MMC, mmc, 0)
+
+#if !defined(CONFIG_EXTRA_ENV_SETTINGS)
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"scriptaddr=0x40000000\0" \
+	BOOTENV
+#endif
+
+#endif
