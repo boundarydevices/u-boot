b3d42a21792a545dbb616157c9f80e935d2f258d
Fabien Parent <fparent@baylibre.com>
2021-11-30 12:17:06 +0100

    cmd: nvedit: allow to modify SCSI stored env
    Modify the nvedit command to allow to modify the environment that is
    stored in SCSI backed storage.
    
    Signed-off-by: Fabien Parent <fparent@baylibre.com>

--- cmd/nvedit.c
+++ cmd/nvedit.c
@@ -52,6 +52,7 @@ DECLARE_GLOBAL_DATA_PTR;
 	defined(CONFIG_ENV_IS_IN_NVRAM)		|| \
 	defined(CONFIG_ENV_IS_IN_ONENAND)	|| \
 	defined(CONFIG_ENV_IS_IN_SATA)		|| \
+	defined(CONFIG_ENV_IS_IN_SCSI)		|| \
 	defined(CONFIG_ENV_IS_IN_SPI_FLASH)	|| \
 	defined(CONFIG_ENV_IS_IN_REMOTE)	|| \
 	defined(CONFIG_ENV_IS_IN_UBI)
