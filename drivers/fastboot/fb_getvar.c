86a35c2280207c4532489fc422417cfa69d3ade9
Gary Bisson <gary.bisson@boundarydevices.com>
2021-12-10 15:07:45 +0100

    fastboot: rename mmc_size to mmc-size
    - to be more consistent with the other options
    - also return the value in hexadecimal to match others
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- drivers/fastboot/fb_getvar.c
+++ drivers/fastboot/fb_getvar.c
@@ -77,7 +77,7 @@ static const struct {
 #endif
 #if CONFIG_IS_ENABLED(FASTBOOT_FLASH_MMC)
 	}, {
-		.variable = "mmc_size",
+		.variable = "mmc-size",
 		.dispatch = getvar_mmc_size
 	}, {
 		.variable = "partition-type",
@@ -234,7 +234,7 @@ static void getvar_mmc_size(char *var_parameter, char *response)
 	struct mmc *mmc = find_mmc_device(CONFIG_FASTBOOT_FLASH_MMC_DEV);
 
 	if (mmc)
-		fastboot_response("OKAY", response, "%llu", mmc->capacity_user);
+		fastboot_response("OKAY", response, "0x%" PRIx64, mmc->capacity_user);
 	else
 		fastboot_fail("mmc device not found", response);
 }
