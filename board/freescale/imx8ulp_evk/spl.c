d56620fb6be094b67ac276362c230a0a9834eaef
Ye Li <ye.li@nxp.com>
2021-12-20 20:55:38 +0800

    LFU-271-2 imx8ulp_evk: Skip init DDR for reboot in dual boot mode
    When M33 is LPAV owner in dual boot, DDR, PCC5, CGC2 won't be reset
    during APD reset. So no need to init DDR again after reboot, but need to
    reconfigure the PLL4 PFD/PFDDIV/LPAV NIC etc, because kernel may
    change or disable some of them.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    (cherry picked from commit 998cde6f8f15906f18c3c84cbf5745b40f5af5b7)

--- board/freescale/imx8ulp_evk/spl.c
+++ board/freescale/imx8ulp_evk/spl.c
@@ -21,13 +21,22 @@
 #include <asm/arch/ddr.h>
 #include <asm/arch/rdc.h>
 #include <asm/arch/upower.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/pcc.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
 void spl_dram_init(void)
 {
-	init_clk_ddr();
-	ddr_init(&dram_timing);
+	/* Reboot in dual boot setting no need to init ddr again */
+	bool ddr_enable = pcc_clock_is_enable(5, LPDDR4_PCC5_SLOT);
+	if (!ddr_enable) {
+		init_clk_ddr();
+		ddr_init(&dram_timing);
+	} else {
+		/* reinit pfd/pfddiv and lpavnic except pll4*/
+		cgc2_pll4_init(false);
+	}
 }
 
 u32 spl_boot_device(void)
