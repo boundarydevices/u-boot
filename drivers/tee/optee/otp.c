77adbe87e601f1ecfd2a128d0fb92e89798796f1
Alexandre Bailon <abailon@baylibre.com>
2022-01-21 09:31:24 +0100

    Add OP-TEE OTP driver
    This adds a driver to read data from an emulated OTP.
    The data are stored in the RPMB and OP-TEE makes sure that
    we can only write the data once and read it as many time we want.
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>

---
+++ drivers/tee/optee/otp.c
@@ -0,0 +1,177 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2020 NXP
+ * Copyright 2022 BayLibre
+ *
+ * Based on cmd/optee_rpmb.c
+ */
+
+#include <env.h>
+#include <errno.h>
+#include <mmc.h>
+#include <net.h>
+#include <tee.h>
+
+#include <tee/optee_ta_otp.h>
+
+#define MAC_ADDRESS_LEN 6
+#define MAX_SN_LEN 64
+
+static struct udevice *tee;
+static u32 session;
+
+static int optee_otp_ta_open_session(void)
+{
+	const struct tee_optee_ta_uuid uuid = TA_OTP_UUID;
+	struct tee_open_session_arg arg;
+	int rc;
+
+	tee = tee_find_device(tee, NULL, NULL, NULL);
+	if (!tee)
+		return -ENODEV;
+
+	memset(&arg, 0, sizeof(arg));
+	tee_optee_ta_uuid_to_octets(arg.uuid, &uuid);
+	rc = tee_open_session(tee, &arg, 0, NULL);
+	if (!rc)
+		session = arg.session;
+
+	return 0;
+}
+
+void optee_otp_ta_close_session(void)
+{
+	if (tee)
+		tee_close_session(tee, session);
+}
+
+static int invoke_func(u32 func, ulong num_param, struct tee_param *param)
+{
+	struct tee_invoke_arg arg;
+
+	if (!tee)
+		if (optee_otp_ta_open_session())
+			return -ENODEV;
+
+	memset(&arg, 0, sizeof(arg));
+	arg.func = func;
+	arg.session = session;
+
+	if (tee_invoke_func(tee, &arg, num_param, param))
+		return -EFAULT;
+	switch (arg.ret) {
+	case TEE_SUCCESS:
+		return 0;
+	case TEE_ERROR_OUT_OF_MEMORY:
+	case TEE_ERROR_STORAGE_NO_SPACE:
+		return -ENOSPC;
+	case TEE_ERROR_ITEM_NOT_FOUND:
+		return -EIO;
+	case TEE_ERROR_TARGET_DEAD:
+		/*
+		 * The TA has paniced, close the session to reload the TA
+		 * for the next request.
+		 */
+		tee_close_session(tee, session);
+		tee = NULL;
+		return -EIO;
+	default:
+		return -EIO;
+	}
+}
+
+int read_persistent_value(const char *name,
+			  size_t buffer_size,
+			  u8 *out_buffer,
+			  size_t *out_num_bytes_read)
+{
+	int rc = 0;
+	struct tee_shm *shm_name;
+	struct tee_shm *shm_buf;
+	struct tee_param param[2];
+	size_t name_size = strlen(name) + 1;
+
+	if (!tee)
+		if (optee_otp_ta_open_session())
+			return -ENODEV;
+
+	rc = tee_shm_alloc(tee, name_size,
+			   TEE_SHM_ALLOC, &shm_name);
+	if (rc)
+		return -ENOMEM;
+
+	rc = tee_shm_alloc(tee, buffer_size,
+			   TEE_SHM_ALLOC, &shm_buf);
+	if (rc) {
+		rc = -ENOMEM;
+		goto free_name;
+	}
+
+	memcpy(shm_name->addr, name, name_size);
+
+	memset(param, 0, sizeof(param));
+	param[0].attr = TEE_PARAM_ATTR_TYPE_MEMREF_INPUT;
+	param[0].u.memref.shm = shm_name;
+	param[0].u.memref.size = name_size;
+	param[1].attr = TEE_PARAM_ATTR_TYPE_MEMREF_OUTPUT;
+	param[1].u.memref.shm = shm_buf;
+	param[1].u.memref.size = buffer_size;
+
+	rc = invoke_func(TA_OTP_CMD_READ_RAW,
+			 2, param);
+	if (rc)
+		goto out;
+
+	if (param[1].u.memref.size > buffer_size) {
+		rc = -EINVAL;
+		goto out;
+	}
+
+	*out_num_bytes_read = param[1].u.memref.size;
+
+	memcpy(out_buffer, shm_buf->addr, *out_num_bytes_read);
+
+out:
+	tee_shm_free(shm_buf);
+free_name:
+	tee_shm_free(shm_name);
+
+	return rc;
+}
+
+int optee_otp_read_serial(void)
+{
+	u8 serial_data[MAX_SN_LEN];
+	size_t len;
+	int ret;
+
+	if (env_get("serial#")) {
+		printf("Using serial from u-boot env\n");
+		return 0;
+	}
+
+	memset(serial_data, 0, MAX_SN_LEN);
+	ret = read_persistent_value("serial", MAX_SN_LEN, serial_data, &len);
+	if (!ret)
+		env_set("serial#", serial_data);
+
+	return ret;
+}
+
+int optee_otp_read_mac(const char *name)
+{
+	u8 ethaddr[MAC_ADDRESS_LEN];
+	size_t len;
+	int ret;
+
+	if (env_get(name)) {
+		printf("%s: Using mac address from u-boot env\n", name);
+		return 0;
+	}
+
+	ret = read_persistent_value("mac", MAC_ADDRESS_LEN, ethaddr, &len);
+	if (!ret && is_valid_ethaddr(ethaddr))
+		eth_env_set_enetaddr(name, ethaddr);
+
+	return ret;
+}
---
+++ include/tee/optee_ta_otp.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2017, Linaro Limited
+ * Copyright (c) 2022, BayLibre
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef __OTP_TA_H__
+#define __OTP_TA_H__
+
+/* UUID of the trusted application */
+#define TA_OTP_UUID \
+		{ 0x3712bdda, 0x569f, 0x4940, \
+			{ 0xb7, 0x49, 0xfb, 0x3b, 0x06, 0xa5, 0xfd, 0x86 } }
+/*
+ * TA_OTP_CMD_READ_RAW - Create and fill a secure storage file
+ * param[0] (memref) ID used the identify the persistent object
+ * param[1] (memref) Raw data dumped from the persistent object
+ * param[2] unused
+ * param[3] unused
+ */
+#define TA_OTP_CMD_READ_RAW		0
+
+/*
+ * TA_OTP_CMD_WRITE_RAW - Create and fill a secure storage file
+ * param[0] (memref) ID used the identify the persistent object
+ * param[1] (memref) Raw data to be writen in the persistent object
+ * param[2] unused
+ * param[3] unused
+ */
+#define TA_OTP_CMD_WRITE_RAW		1
+
+#ifdef CONFIG_OPTEE_TA_OTP
+int read_persistent_value(const char *name,
+                          size_t buffer_size,
+                          u8 *out_buffer,
+                          size_t *out_num_bytes_read);
+int optee_otp_read_serial(void);
+int optee_otp_read_mac(const char *name);
+void optee_otp_ta_close_session(void);
+#else /* CONFIG_OPTEE_TA_OTP */
+static inline int read_persistent_value(const char *name, size_t buffer_size,
+                                        u8 *out_buffer,
+                                        size_t *out_num_bytes_read)
+{
+        return -ENODEV;
+}
+
+static inline int optee_otp_read_serial(void)
+{
+        return -ENODEV;
+}
+static inline int optee_otp_read_mac(const char *name)
+{
+        return -ENODEV;
+}
+static inline void optee_otp_ta_close_session(void) {}
+#endif /* CONFIG_OPTEE_TA_OTP */
+
+#endif /* __OTP_TA_H__ */
