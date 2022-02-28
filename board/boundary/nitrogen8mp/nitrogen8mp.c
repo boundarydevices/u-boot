555ace0dd0bb34653068ec9158e771bf886e5cdc
Troy Kisky <troy.kisky@boundarydevices.com>
2022-02-28 13:38:25 -0800

    dts: imx8mp-nitrogen8mp: prevent white flash when lcm-jm430 display is enabled
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- board/boundary/nitrogen8mp/nitrogen8mp.c
+++ board/boundary/nitrogen8mp/nitrogen8mp.c
@@ -43,7 +43,6 @@ static iomux_v3_cfg_t const init_pads[] = {
 	IOMUX_PAD_CTRL(SAI1_RXD2__ENET1_MDC, PAD_CTRL_ENET_MDC),
 
 #define GPIRQ_SN65DSI83		IMX_GPIO_NR(4, 0)
-#define GP_LCM_JM430_BKL_EN	IMX_GPIO_NR(4, 0)
 #define GP_LTK08_MIPI_EN	IMX_GPIO_NR(4, 0)
 	IOMUX_PAD_CTRL(SAI1_RXFS__GPIO4_IO00, 0x04),
 
@@ -155,7 +154,7 @@ static const struct display_info_t displays[] = {
 	VD_MIPI_MTD0900DCP27KF(MIPI, board_detect_pca9546, fbp_bus_gp((1 | (3 << 4)), 0, 0, 0), 0x41, FBP_MIPI_TO_LVDS, FBTS_ILI251X),
 	VD_DMT050WVNXCMI(MIPI, board_detect_pca9546, fbp_bus_gp((1 | (3 << 4)), GP_SC18IS602B_RESET, 0, 30), fbp_addr_gp(0x2f, 0, 6, 0), FBP_SPI_LCD, FBTS_GOODIX),
 	VD_LTK080A60A004T(MIPI, board_detect_gt911, fbp_bus_gp((1 | (3 << 4)), GP_LTK08_MIPI_EN, GP_LTK08_MIPI_EN, 0), 0x5d, FBTS_GOODIX),	/* Goodix touchscreen */
-	VD_LCM_JM430_MINI(MIPI, board_detect_pca9546, fbp_bus_gp((1 | (3 << 4)), GP_ST1633_RESET, GP_TC358762_EN, 30), fbp_addr_gp(0x55, GP_LCM_JM430_BKL_EN, 0, 0), FBTS_ST1633I),	/* Sitronix touch */
+	VD_LCM_JM430_MINI(MIPI, board_detect_pca9546, fbp_bus_gp((1 | (3 << 4)), GP_ST1633_RESET, GP_TC358762_EN, 30), fbp_addr_gp(0x55, 0, 0, 0), FBTS_ST1633I),	/* Sitronix touch */
 	VD_LTK0680YTMDB_2(MIPI, NULL, fbp_bus_gp((1 | (3 << 4)), GP_MIPI_RESET, GP_MIPI_RESET, 0), 0x5d, FBTS_GOODIX),
 	VD_MIPI_COM50H5N03ULC(MIPI, NULL, fbp_bus_gp((1 | (3 << 4)), GP_MIPI_RESET, GP_MIPI_RESET, 0), 0x00),
 	/* 0x3e is the TPS65132 power chip on our adapter board */
