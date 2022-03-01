c7a8015dd0fc5c6740f559a63685418e19207245
Troy Kisky <troy.kisky@boundarydevices.com>
2022-03-01 10:47:43 -0800

    video: imx: sec_dsim_imx: call panel_enable
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/video/imx/sec_dsim_imx.c
+++ drivers/video/imx/sec_dsim_imx.c
@@ -141,6 +141,18 @@ static int imx_sec_dsim_attach(struct udevice *dev)
 			priv->panel->name, ret);
 		return ret;
 	}
+
+	ret = dsi_host_enable(priv->dsi_host);
+	if (ret) {
+		dev_err(dev, "failed to enable mipi dsi host\n");
+		return ret;
+	}
+
+	ret = panel_enable(priv->panel);
+	if (ret && (ret != ENOSYS)) {
+		dev_err(dev, "panel_enable failed\n");
+		return ret;
+	}
 	return 0;
 }
 
@@ -148,12 +160,6 @@ static int imx_sec_dsim_set_backlight(struct udevice *dev, int percent)
 {
 	struct imx_sec_dsim_priv *priv = dev_get_priv(dev);
 	int ret;
-
-	ret = dsi_host_enable(priv->dsi_host);
-	if (ret) {
-		dev_err(dev, "failed to enable mipi dsi host\n");
-		return ret;
-	}
 
 	ret = panel_enable_backlight(priv->panel);
 	if (ret) {
