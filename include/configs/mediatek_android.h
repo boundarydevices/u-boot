2f5447b421ce5489ee710b3d3b40ffce2ced2835
Julien Masson <jmasson@baylibre.com>
2022-05-19 15:50:49 +0200

    mediatek_android: do not set loadaddr
    The environment variable loadaddr is already set with
    CONFIG_SYS_LOAD_ADDR defconfig:
    59f75f55d4 u-boot: configs: add CONFIG_SYS_LOAD_ADDR to defconfig
    
    Signed-off-by: Julien Masson <jmasson@baylibre.com>

--- include/configs/mediatek_android.h
+++ include/configs/mediatek_android.h
@@ -260,7 +260,6 @@
 	"dtbo_index=0\0" \
 	"serial#=" SERIAL_ANDROID "\0" \
 	"hardware=" CONFIG_SYS_BOARD "\0" \
-	"loadaddr=0x50000000\0" \
 	"dtbaddr=0x54000000\0" \
 	"dtboaddr=0x54C00000\0" \
 	"fdt_addr_r=0x56000000\0" \
