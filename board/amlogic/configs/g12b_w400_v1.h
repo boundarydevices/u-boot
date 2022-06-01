d62bb9748f80cb9e34dd8d1e468eeb49de834b18
Gary Bisson <gary.bisson@boundarydevices.com>
2022-06-01 15:59:50 +0200

    g12b_w400: fix device product name
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- board/amlogic/configs/g12b_w400_v1.h
+++ board/amlogic/configs/g12b_w400_v1.h
@@ -536,7 +536,7 @@
 #define CONFIG_USBDOWNLOAD_GADGET 1
 #define CONFIG_SYS_CACHELINE_SIZE 64
 #define CONFIG_FASTBOOT_MAX_DOWN_SIZE	0x8000000
-#define CONFIG_DEVICE_PRODUCT	"galilei"
+#define CONFIG_DEVICE_PRODUCT	"w400"
 
 //UBOOT Facotry usb/sdcard burning config
 //#define CONFIG_AML_V2_FACTORY_BURN              1       //support facotry usb burning
