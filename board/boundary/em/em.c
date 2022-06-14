24da6d57a776b709d80c6159be23a455ab7078b6
Gary Bisson <gary.bisson@boundarydevices.com>
2022-06-14 22:47:06 +0200

    em: initial addition, Boundary Devices board
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>
    
    em: fixup Ethernet PHY
    em: remove deprecated LOADADDR variable
    em: add  CONFIG_SPL_DRIVERS_MISC: to defconfigs
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>
    
    em: minor dts changes
    em: pull-up 5V_BOOST_EN
    em: add MAX77975/MAX17260 battery manangement support
    em: add panel-m101nwwb support
    em: add alias mipi_to_lvds
    em: touchscreens are on bus 2, (i2c3 from dts pov)
    em: finish adding panel-ltk080a60a004t
    em: defconfig changes for 2022 branch
    em: dts: change coex1/2 to pull-down
    em: sort panels
    em: pull-down B_MODEM_COEX1/2
    em: drive B_MODEM_COEX1/2 low
    em: move CONFIG_VIDEO_MXS/CONFIG_VIDEO_LOGO/CONFIG_VIDEO_BMP_GZIP to defconfig
    em: CONFIG_VIDEO_BMP_RLE8 moved to defconfig, selected by VIDEO_LOGO
    em: add m101nwwb/tm070jdhg30 burst/sync/pulse choices
    em: select SYS_MMCSD_RAW_MODE_U_BOOT_USE_OFFSET
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

---
+++ board/boundary/em/em.c
@@ -0,0 +1,213 @@
+/*
+ * Copyright 2022 Boundary Devices LLC
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <command.h>
+#include <errno.h>
+#include <asm/io.h>
+#include <miiphy.h>
+#include <netdev.h>
+#include <asm-generic/gpio.h>
+#include <fsl_esdhc_imx.h>
+#include <mmc.h>
+#include <asm/arch/clock.h>
+#if defined(CONFIG_IMX8MM)
+#include <asm/arch/imx8mm_pins.h>
+#elif defined(CONFIG_IMX8MN)
+#include <asm/arch/imx8mn_pins.h>
+#endif
+#include <asm/arch/sys_proto.h>
+#include <asm/mach-imx/dma.h>
+#include <asm/mach-imx/fbpanel.h>
+#include <asm/mach-imx/gpio.h>
+#include <asm/mach-imx/iomux-v3.h>
+#include <asm/mach-imx/mxc_i2c.h>
+#include <asm/mach-imx/video.h>
+#include <dm.h>
+#include <env.h>
+#include <i2c.h>
+#include <linux/delay.h>
+#include <spl.h>
+#include "../common/padctrl.h"
+#include "../common/bd_common.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+#define UART_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_FSEL1)
+
+static iomux_v3_cfg_t const init_pads[] = {
+	IOMUX_PAD_CTRL(GPIO1_IO02__WDOG1_WDOG_B, WDOG_PAD_CTRL),
+	IOMUX_PAD_CTRL(UART2_RXD__UART2_DCE_RX, UART_PAD_CTRL),
+	IOMUX_PAD_CTRL(UART2_TXD__UART2_DCE_TX, UART_PAD_CTRL),
+
+#define GP_B_MODEM_COEX1		IMX_GPIO_NR(5, 28)
+	IOMUX_PAD_CTRL(UART4_RXD__GPIO5_IO28, 0x100),	/* B_MODEM_COEX1 pull-down */
+#define GP_B_MODEM_COEX2		IMX_GPIO_NR(5, 29)
+	IOMUX_PAD_CTRL(UART4_TXD__GPIO5_IO29, 0x100),	/* B_MODEM_COEX2 pull-down */
+
+#define GPIRQ_SN65DSI83			IMX_GPIO_NR(1, 1)
+#define GP_LTK08_MIPI_EN		IMX_GPIO_NR(1, 1)
+	IOMUX_PAD_CTRL(GPIO1_IO01__GPIO1_IO1, 0x146),
+#define GPIRQ_TS_GT911 			IMX_GPIO_NR(1, 6)
+#define GPIRQ_TS_FT5X06			IMX_GPIO_NR(1, 6)
+	IOMUX_PAD_CTRL(GPIO1_IO06__GPIO1_IO6, 0x180),
+#define GP_TS_GT911_RESET		IMX_GPIO_NR(1, 7)
+#define GP_ST1633_RESET			IMX_GPIO_NR(1, 7)
+#define GP_TS_FT5X06_RESET		IMX_GPIO_NR(1, 7)
+#define GP_TS_FT7250_RESET		IMX_GPIO_NR(1, 7)
+	IOMUX_PAD_CTRL(GPIO1_IO07__GPIO1_IO7, 0x100),
+#define GP_TC358762_EN		IMX_GPIO_NR(1, 9)
+#define GP_SC18IS602B_RESET	IMX_GPIO_NR(1, 9)
+#define GP_SN65DSI83_EN		IMX_GPIO_NR(1, 9)
+#define GP_MIPI_RESET		IMX_GPIO_NR(1, 9)
+	IOMUX_PAD_CTRL(GPIO1_IO09__GPIO1_IO9, 0x100),
+#define GP_BACKLIGHT_MIPI		IMX_GPIO_NR(5, 3)
+	IOMUX_PAD_CTRL(SPDIF_TX__GPIO5_IO3, 0x116),
+
+#define GP_PCIE0_RESET		IMX_GPIO_NR(4, 31)
+	IOMUX_PAD_CTRL(SAI3_TXFS__GPIO4_IO31, 0x100),
+#define GP_PCIE0_DISABLE	IMX_GPIO_NR(3, 21)
+	IOMUX_PAD_CTRL(SAI5_RXD0__GPIO3_IO21, 0x100),
+
+#ifdef CONFIG_FEC_MXC
+#ifdef CONFIG_FEC_PHY_BITBANG
+#define GP_MII_MDIO	IMX_GPIO_NR(1, 17)
+	IOMUX_PAD_CTRL(ENET_MDIO__GPIO1_IO17, 0),
+#define GP_MII_MDC	IMX_GPIO_NR(1, 16)
+	IOMUX_PAD_CTRL(ENET_MDC__GPIO1_IO16, 0),
+#else
+	IOMUX_PAD_CTRL(ENET_MDIO__ENET1_MDIO, PAD_CTRL_ENET_MDIO),
+	IOMUX_PAD_CTRL(ENET_MDC__ENET1_MDC, PAD_CTRL_ENET_MDC),
+#endif
+	IOMUX_PAD_CTRL(ENET_TX_CTL__ENET1_RGMII_TX_CTL, PAD_CTRL_ENET_TX),
+	IOMUX_PAD_CTRL(ENET_TD0__ENET1_RGMII_TD0, PAD_CTRL_ENET_TX),
+	IOMUX_PAD_CTRL(ENET_TD1__ENET1_RGMII_TD1, PAD_CTRL_ENET_TX),
+	IOMUX_PAD_CTRL(ENET_TD2__ENET1_RGMII_TD2, PAD_CTRL_ENET_TX),
+	IOMUX_PAD_CTRL(ENET_TD3__ENET1_RGMII_TD3, PAD_CTRL_ENET_TX),
+	IOMUX_PAD_CTRL(ENET_TXC__ENET1_RGMII_TXC, PAD_CTRL_ENET_TX),
+#endif
+
+#ifdef CONFIG_IMX8MM
+#define GP_USB_RESET	IMX_GPIO_NR(5, 0)
+	IOMUX_PAD_CTRL(SAI3_TXC__GPIO5_IO0, WEAK_PULLUP),
+#endif
+
+#define GP_EMMC_RESET	IMX_GPIO_NR(2, 10)
+	IOMUX_PAD_CTRL(SD1_RESET_B__GPIO2_IO10, 0x41),
+};
+
+int board_early_init_f(void)
+{
+	struct wdog_regs *wdog = (struct wdog_regs *)WDOG1_BASE_ADDR;
+
+	gpio_direction_output(GP_BACKLIGHT_MIPI, 0);
+	gpio_request(GP_SN65DSI83_EN, "sn65en");
+	gpio_request(GP_B_MODEM_COEX1, "modem_coex1");
+	gpio_request(GP_B_MODEM_COEX2, "modem_coex2");
+	gpio_direction_output(GP_SN65DSI83_EN, 0);
+	gpio_direction_output(GP_B_MODEM_COEX1, 0);
+	gpio_direction_output(GP_B_MODEM_COEX2, 0);
+	imx_iomux_v3_setup_multiple_pads(init_pads, ARRAY_SIZE(init_pads));
+
+	gpio_direction_output(GP_USB_RESET, 1);
+	gpio_direction_output(GP_EMMC_RESET, 1);
+	set_wdog_reset(wdog);
+	gpio_direction_output(GP_SN65DSI83_EN, 0);
+
+	return 0;
+}
+
+#ifdef CONFIG_CMD_FBPANEL
+int board_detect_gt911(struct display_info_t const *di)
+{
+	return board_detect_gt911_common(di, 0, 0, GP_TS_GT911_RESET, GPIRQ_TS_GT911);
+}
+
+int board_detect_gt911_sn65(struct display_info_t const *di)
+{
+	return board_detect_gt911_sn65_common(di, 0, 0, GP_TS_GT911_RESET, GPIRQ_TS_GT911);
+}
+
+static const struct display_info_t displays[] = {
+	VD_MIPI_TM070JDHG30_x("tm070jdhg30-1",	B, MIPI, board_detect_gt911_sn65, fbp_bus_gp(2, GP_SN65DSI83_EN, 0, 0), 0x5d, FBP_MIPI_TO_LVDS, FBTS_GOODIX),
+	VD_MIPI_TM070JDHG30_x("tm070jdhg30-2",	U, MIPI, NULL, fbp_bus_gp(2, GP_SN65DSI83_EN, 0, 0), 0x5d, FBP_MIPI_TO_LVDS, FBTS_GOODIX),
+	VD_MIPI_TM070JDHG30_x("tm070jdhg30-3",	E, MIPI, NULL, fbp_bus_gp(2, GP_SN65DSI83_EN, 0, 0), 0x5d, FBP_MIPI_TO_LVDS, FBTS_GOODIX),
+	VD_MIPI_M101NWWB_x("m101nwwb-1",	B, MIPI, NULL, fbp_bus_gp(2, GP_SN65DSI83_EN, 0, 0), 0x5d, FBP_MIPI_TO_LVDS, FBTS_GOODIX),
+	VD_MIPI_M101NWWB_x("m101nwwb-2",	U, MIPI, NULL, fbp_bus_gp(2, GP_SN65DSI83_EN, 0, 0), 0x5d, FBP_MIPI_TO_LVDS, FBTS_GOODIX),
+	VD_MIPI_M101NWWB_x("m101nwwb-3",	E, MIPI, NULL, fbp_bus_gp(2, GP_SN65DSI83_EN, 0, 0), 0x5d, FBP_MIPI_TO_LVDS, FBTS_GOODIX),
+
+	VD_MIPI_TM070JDHG30_x("tm070jdhg30-4",	B, MIPI, board_detect_sn65_and_ts, fbp_bus_gp(2, GP_SN65DSI83_EN, GP_TS_FT5X06_RESET, 0), 0x38, FBP_MIPI_TO_LVDS, FBTS_FT5X06),
+	VD_MIPI_TM070JDHG30_x("tm070jdhg30-5",	U, MIPI, NULL, fbp_bus_gp(2, GP_SN65DSI83_EN, GP_TS_FT5X06_RESET, 0), 0x38, FBP_MIPI_TO_LVDS, FBTS_FT5X06),
+	VD_MIPI_TM070JDHG30_x("tm070jdhg30-6",	E, MIPI, NULL, fbp_bus_gp(2, GP_SN65DSI83_EN, GP_TS_FT5X06_RESET, 0), 0x38, FBP_MIPI_TO_LVDS, FBTS_FT5X06),
+	VD_MIPI_M101NWWB_x("m101nwwb-4",	B, MIPI, NULL, fbp_bus_gp(2, GP_SN65DSI83_EN, GP_TS_FT5X06_RESET, 0), 0x38, FBP_MIPI_TO_LVDS, FBTS_FT5X06),
+	VD_MIPI_M101NWWB_x("m101nwwb-5",	U, MIPI, NULL, fbp_bus_gp(2, GP_SN65DSI83_EN, GP_TS_FT5X06_RESET, 0), 0x38, FBP_MIPI_TO_LVDS, FBTS_FT5X06),
+	VD_MIPI_M101NWWB_x("m101nwwb-6",	E, MIPI, NULL, fbp_bus_gp(2, GP_SN65DSI83_EN, GP_TS_FT5X06_RESET, 0), 0x38, FBP_MIPI_TO_LVDS, FBTS_FT5X06),
+
+	VD_LTK080A60A004T(MIPI, board_detect_gt911, fbp_bus_gp(2, GP_LTK08_MIPI_EN, GP_LTK08_MIPI_EN, 0), 0x5d, FBTS_GOODIX),	/* Goodix touchscreen */
+};
+#define display_cnt	ARRAY_SIZE(displays)
+#else
+#define displays	NULL
+#define display_cnt	0
+#endif
+
+int board_init(void)
+{
+#ifdef CONFIG_FEC_PHY_BITBANG
+	gpio_request(GP_MII_MDC, "mii_mdc");
+	gpio_request(GP_MII_MDIO, "mii_mdio");
+#endif
+	gpio_request(GP_TS_GT911_RESET, "gt911_reset");
+	gpio_request(GPIRQ_TS_GT911, "gt911_irq");
+#ifndef CONFIG_DM_VIDEO
+	gpio_request(GP_SN65DSI83_EN, "sn65dsi83_enable");
+	gpio_request(GP_LTK08_MIPI_EN, "lkt08_mipi_en");
+#endif
+	gpio_direction_output(GP_TS_GT911_RESET, 0);
+#if defined(CONFIG_MXC_SPI) && !defined(CONFIG_DM_SPI)
+	setup_spi();
+#endif
+#ifdef CONFIG_MAX77975
+	max77975_init();
+	max77975_set_chrgin_limit(8000);	/* 8 amps */
+#endif
+#ifdef CONFIG_DM_ETH
+	board_eth_init(gd->bd);
+#endif
+#ifdef CONFIG_CMD_FBPANEL
+	fbp_setup_display(displays, display_cnt);
+#endif
+	return 0;
+}
+
+void board_env_init(void)
+{
+	/*
+	 * If touchscreen reset is low, display will not initialize, but runs fine
+	 * after init independent of gpio level
+	 */
+	gpio_direction_output(GP_TS_FT7250_RESET, 1);
+}
+
+void board_poweroff(void)
+{
+	struct snvs_regs *snvs = (struct snvs_regs *)(SNVS_BASE_ADDR);
+
+	writel(0x60, &snvs->lpcr);
+	mdelay(500);
+}
+
+static int _do_poweroff(struct cmd_tbl *cmdtp, int flag, int argc, char * const argv[])
+{
+	board_poweroff();
+	return 0;
+}
+
+U_BOOT_CMD(
+	poweroff, 70, 0, _do_poweroff,
+	"power down board",
+	""
+);
