db42ba126f1a16cf89b2efffb083e74a7137f7d2
Troy Kisky <troy.kisky@boundarydevices.com>
2022-07-25 12:22:36 -0700

    ash2: add micrel support
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- include/configs/ash2.h
+++ include/configs/ash2.h
@@ -19,6 +19,7 @@
 #define CONFIG_IMX_HDMI
 #define CONFIG_SYS_FSL_USDHC_NUM	1
 #define CONFIG_USB_MAX_CONTROLLER_COUNT 2
+#define CONFIG_FEC_MXC_KSZ_PHYADDR	7
 #define BD_I2C_MASK	7
 
 #include "boundary.h"
