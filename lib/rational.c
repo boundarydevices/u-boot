f2fe00bb37d6de0f629c90a639e488e3b5b418e9
Troy Kisky <troy.kisky@boundarydevices.com>
2022-08-22 09:22:30 -0700

    lib: rational: add rational_best_ratio_bigger
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- lib/rational.c
+++ lib/rational.c
@@ -13,6 +13,87 @@
 #include <linux/kernel.h>
 
 /*
+ * continued fraction
+ *      2  1  2  1  2
+ * 0 1  2  3  8 11 30
+ * 1 0  1  1  3  4 11
+ */
+void rational_best_ratio_bigger(unsigned long *pnum, unsigned long *pdenom, unsigned max_n, unsigned max_d)
+{
+	unsigned long a = *pnum;
+	unsigned long b = *pdenom;
+	unsigned long c;
+	unsigned n0 = 0;
+	unsigned n1 = 1;
+	unsigned d0 = 1;
+	unsigned d1 = 0;
+	unsigned _n = 0;
+	unsigned _d = 1;
+	unsigned whole;
+
+	while (b) {
+		whole = a / b;
+		/* n0/d0 is the earlier term */
+		n0 = n0 + (n1 * whole);
+		d0 = d0 + (d1 * whole);
+
+		c = a - (b * whole);
+		a = b;
+		b = c;
+
+		if (b) {
+			/* n1/d1 is the earlier term */
+			whole = a / b;
+			_n = n1 + (n0 * whole);
+			_d = d1 + (d0 * whole);
+		} else {
+			_n = n0;
+			_d = d0;
+		}
+		debug("%s: cf=%i %d/%d, %d/%d\n", __func__, whole, n0, d0, _n, _d);
+		if ((_n > max_n) || (_d > max_d)) {
+			unsigned h;
+
+			h = n0;
+			if (h) {
+				_n = max_n - n1;
+				_n /= h;
+				if (whole > _n)
+					whole = _n;
+			}
+			h = d0;
+			if (h) {
+				_d = max_d - d1;
+				_d /= h;
+				if (whole > _d)
+					whole = _d;
+			}
+			_n = n1 + (n0 * whole);
+			_d = d1 + (d0 * whole);
+			debug("%s: b=%ld, n=%d of %d, d=%d of %d\n", __func__, b, _n, max_n, _d, max_d);
+			if (!_d) {
+				/* Don't choose infinite for a bigger ratio */
+				_n = n0 + 1;
+				_d = d0;
+				pr_err("%s: %d/%d is too big\n", __func__, _n, _d);
+			}
+			break;
+		}
+
+		if (!b)
+			break;
+		n1 = _n;
+		d1 = _d;
+		c = a - (b * whole);
+		a = b;
+		b = c;
+	}
+
+	*pnum = _n;
+	*pdenom = _d;
+}
+
+/*
  * calculate best rational approximation for a given fraction
  * taking into account restricted register size, e.g. to find
  * appropriate values for a pll with 5 bit denominator and
@@ -77,13 +158,18 @@ void rational_best_approximation(
 		 * found below as 't'.
 		 */
 		if ((n2 > max_numerator) || (d2 > max_denominator)) {
-			unsigned long t = min((max_numerator - n0) / n1,
-					      (max_denominator - d0) / d1);
+			unsigned long t = ULONG_MAX;
 
-			/* This tests if the semi-convergent is closer
-			 * than the previous convergent.
+			if (d1)
+				t = (max_denominator - d0) / d1;
+			if (n1)
+				t = min(t, (max_numerator - n0) / n1);
+
+			/* This tests if the semi-convergent is closer than the previous
+			 * convergent.  If d1 is zero there is no previous convergent as this
+			 * is the 1st iteration, so always choose the semi-convergent.
 			 */
-			if (2u * t > a || (2u * t == a && d0 * dp > d1 * d)) {
+			if (!d1 || 2u * t > a || (2u * t == a && d0 * dp > d1 * d)) {
 				n1 = n0 + t * n1;
 				d1 = d0 + t * d1;
 			}
