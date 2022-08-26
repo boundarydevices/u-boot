23fcfbb1e77469eef0e36ed2c80550401e4e2981
Gary Bisson <gary.bisson@boundarydevices.com>
2022-08-26 12:27:43 +0200

    sign_hab_imx8m.sh: adapt to binman
    - binman is now used instead of mkimage to create the final image
    - Main difference is that it now doesn't create the IVT header for the
    FIT image any more. So we have to create it ourselves.
    - Also the order of the binaries in the FIT image might differ
      -> rendering print_fit_hab.sh useless
    - Now using itb.map and flash.log exclusively
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- sign_hab_imx8m.sh
+++ sign_hab_imx8m.sh
@@ -34,12 +34,6 @@ SCRIPT_PATH=$(dirname $0)
 # source config file for convenience
 source .config
 
-# get FIT HAB signing offsets
-for i in $CONFIG_OF_LIST; do
-	DTBS="arch/arm/dts/$i.dtb $DTBS"
-done
-$SCRIPT_PATH/print_fit_hab.sh $DTBS > fit.log
-
 # copy templates and update values
 cp $SCRIPT_PATH/doc/imx/habv4/csf_examples/mx8m_mx8mm/template_csf_spl.txt csf_spl.txt
 cp $SCRIPT_PATH/doc/imx/habv4/csf_examples/mx8m_mx8mm/template_csf_fit.txt csf_fit.txt
@@ -56,30 +50,59 @@ sed -i "s|_SPL_OFFSET_|$SPL_OFFSET|g" csf_spl.txt
 sed -i "s|_SPL_LENGTH_|$SPL_LENGTH|g" csf_spl.txt
 
 # update FIT values
-FIT_START_ADDR=`awk '/sld hab block/{print $4}' flash.log`
-FIT_OFFSET=`awk '/sld hab block/{print $5}' flash.log`
-FIT_LENGTH=`awk '/sld hab block/{print $6}' flash.log`
+if grep -Eq "^CONFIG_IMX8MQ=y$" .config; then
+TEE_LOAD_ADDR=0xfe000000
+ATF_LOAD_ADDR=0x00910000
+elif grep -Eq "^CONFIG_IMX8MM=y$" .config; then
+TEE_LOAD_ADDR=0xbe000000
+ATF_LOAD_ADDR=0x00920000
+elif grep -Eq "^CONFIG_IMX8MN=y$" .config; then
+TEE_LOAD_ADDR=0xbe000000
+ATF_LOAD_ADDR=0x00960000
+elif grep -Eq "^CONFIG_IMX8MP=y$" .config; then
+TEE_LOAD_ADDR=0x56000000
+ATF_LOAD_ADDR=0x00970000
+fi
+BLOCK_LEN="0x200"
+IVTOFFSET="0x1000"
+IVTSIZE="0x20"
+FIT_OFFSET=`awk '/File.*uboot.*image-pos/{print $9}' flash.log | tail -n 1`
+FIT_SIZE="0x`awk '/fit/{print $3}' itb.map | head -n 1`"
+FIT_UBOOT_OFFSET="0x`awk '/uboot/{print $2}' itb.map | head -n 1`"
+FIT_UBOOT_SIZE="0x`awk '/uboot/{print $3}' itb.map | head -n 1`"
+FIT_ATF_OFFSET="0x`awk '/atf/{print $2}' itb.map | head -n 1`"
+FIT_ATF_SIZE="0x`awk '/atf/{print $3}' itb.map | head -n 1`"
+FIT_DTB_OFFSET="0x`awk '/fip/{print $2}' itb.map | head -n 1`"
+printf -v FIT_START_ADDR '%#x' "$((CONFIG_SYS_TEXT_BASE - BLOCK_LEN - CONFIG_CSF_SIZE - IVTOFFSET - CONFIG_SYS_CACHELINE_SIZE))"
+printf -v FIT_LENGTH '%#x' "$((IVTOFFSET + IVTSIZE))"
 sed -i "s|_FIT_START_ADDR_|$FIT_START_ADDR|g" csf_fit.txt
 sed -i "s|_FIT_OFFSET_|$FIT_OFFSET|g" csf_fit.txt
 sed -i "s|_FIT_LENGTH_|$FIT_LENGTH|g" csf_fit.txt
-UBOOT_START_ADDR=`awk 'NR==1{print $1}' fit.log`
-UBOOT_OFFSET=`awk 'NR==1{print $2}' fit.log`
-UBOOT_LENGTH=`awk 'NR==1{print $3}' fit.log`
-sed -i "s|_UBOOT_START_ADDR_|$UBOOT_START_ADDR|g" csf_fit.txt
+printf -v UBOOT_OFFSET '%#x' "$((FIT_OFFSET + FIT_UBOOT_OFFSET))"
+sed -i "s|_UBOOT_START_ADDR_|$CONFIG_SYS_TEXT_BASE|g" csf_fit.txt
 sed -i "s|_UBOOT_OFFSET_|$UBOOT_OFFSET|g" csf_fit.txt
-sed -i "s|_UBOOT_LENGTH_|$UBOOT_LENGTH|g" csf_fit.txt
-DTB_START_ADDR=`awk 'NR==2{print $1}' fit.log`
-DTB_OFFSET=`awk 'NR==2{print $2}' fit.log`
-DTB_LENGTH=`awk 'NR==2{print $3}' fit.log`
+sed -i "s|_UBOOT_LENGTH_|$FIT_UBOOT_SIZE|g" csf_fit.txt
+printf -v DTB_START_ADDR '%#x' "$((CONFIG_SYS_TEXT_BASE + FIT_UBOOT_SIZE))"
+printf -v DTB_OFFSET '%#x' "$((FIT_OFFSET + FIT_DTB_OFFSET))"
+printf -v DTB_LENGTH '%#x' "$((FIT_SIZE - FIT_DTB_OFFSET))"
 sed -i "s|_DTB_START_ADDR_|$DTB_START_ADDR|g" csf_fit.txt
 sed -i "s|_DTB_OFFSET_|$DTB_OFFSET|g" csf_fit.txt
 sed -i "s|_DTB_LENGTH_|$DTB_LENGTH|g" csf_fit.txt
-ATF_START_ADDR=`awk 'NR==3{print $1}' fit.log`
-ATF_OFFSET=`awk 'NR==3{print $2}' fit.log`
-ATF_LENGTH=`awk 'NR==3{print $3}' fit.log`
-sed -i "s|_ATF_START_ADDR_|$ATF_START_ADDR|g" csf_fit.txt
+printf -v ATF_OFFSET '%#x' "$((FIT_OFFSET + FIT_ATF_OFFSET))"
+sed -i "s|_ATF_START_ADDR_|$ATF_LOAD_ADDR|g" csf_fit.txt
 sed -i "s|_ATF_OFFSET_|$ATF_OFFSET|g" csf_fit.txt
-sed -i "s|_ATF_LENGTH_|$ATF_LENGTH|g" csf_fit.txt
+sed -i "s|_ATF_LENGTH_|$FIT_ATF_SIZE|g" csf_fit.txt
+
+echo Generating IVT header for FIT...
+printf -v IVTADDRESS '%#x' "$((FIT_START_ADDR + IVTOFFSET))"
+printf -v CSFADDRESS '%#x' "$((IVTADDRESS + IVTSIZE))"
+cp doc/imx/habv4/csf_examples/mx8m_mx8mm/template_genIVT.txt genIVT.pl
+sed -i "s|_LOADADDR_|$FIT_START_ADDR|g" genIVT.pl
+sed -i "s|_IVTADDRESS_|$IVTADDRESS|g" genIVT.pl
+sed -i "s|_CSFADDRESS_|$CSFADDRESS|g" genIVT.pl
+chmod +x genIVT.pl
+./genIVT.pl
+dd if=ivt.bin of=flash.bin seek=$((FIT_OFFSET + IVTOFFSET)) bs=1 conv=notrunc
 
 # generate signatures
 $CST_BIN -i csf_spl.txt -o csf_spl.bin
@@ -87,7 +110,7 @@ $CST_BIN -i csf_fit.txt -o csf_fit.bin
 
 # copy signatures into binary
 CSF_SPL_OFFSET=`awk '/csf_off/{print $2}' flash.log | head -n 1`
-CSF_FIT_OFFSET=`awk '/csf_off/{print $2}' flash.log | tail -n 1`
+CSF_FIT_OFFSET=$((FIT_OFFSET + IVTOFFSET + IVTSIZE))
 cp flash.bin signed_flash.bin
 dd if=csf_spl.bin of=signed_flash.bin seek=$(($CSF_SPL_OFFSET)) bs=1 conv=notrunc
 dd if=csf_fit.bin of=signed_flash.bin seek=$(($CSF_FIT_OFFSET)) bs=1 conv=notrunc
