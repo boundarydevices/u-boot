nxp-imx/uboot-imx

MLK-16365 mmc: add sd/mmc legacy capability
Add SD/MMC legacy capability. Otherwise the legacy cards supports will
be broken.

Signed-off-by: Peng Fan <peng.fan@nxp.com>

drivers/mmc/mmc.c
@@ -1555,6 +1555,7 @@ static int sd_select_mode_and_width(struct mmc *mmc, uint card_caps)
 
 	/* Restrict card's capabilities by what the host can do */
 	caps = card_caps & (mmc->cfg->host_caps | MMC_MODE_1BIT);
+	caps |= MMC_CAP(SD_LEGACY);
 
 	if (!uhs_en)
 		caps &= ~UHS_CAPS;
@@ -1693,6 +1694,7 @@ static int mmc_select_mode_and_width(struct mmc *mmc, uint card_caps)
 
 	/* Restrict card's capabilities by what the host can do */
 	card_caps &= (mmc->cfg->host_caps | MMC_MODE_1BIT);
+	card_caps |= MMC_CAP(MMC_LEGACY);
 
 	/* Only version 4 of MMC supports wider bus widths */
 	if (mmc->version < MMC_VERSION_4)

boundarydevices/u-boot

Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

drivers/mmc/mmc.c
@@ -1788,6 +1788,7 @@ static int sd_select_mode_and_width(struct mmc *mmc, uint card_caps)
 
 	/* Restrict card's capabilities by what the host can do */
 	caps = card_caps & mmc->host_caps;
+	caps |= MMC_CAP(MMC_LEGACY);
 
 	if (!uhs_en)
 		caps &= ~UHS_CAPS;
@@ -2111,6 +2112,7 @@ static int mmc_select_mode_and_width(struct mmc *mmc, uint card_caps)
 
 	/* Restrict card's capabilities by what the host can do */
 	card_caps &= mmc->host_caps;
+	card_caps |= MMC_CAP(MMC_LEGACY);
 
 	/* Only version 4 of MMC supports wider bus widths */
 	if (mmc->version < MMC_VERSION_4)