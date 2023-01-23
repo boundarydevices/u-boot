5cdcf8c051470d260e796fb2edd5843061465440
Troy Kisky <troy.kisky@boundarydevices.com>
2023-01-23 11:58:33 -0800

    fbpanel: add VD_LTK069WXICT10
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- arch/arm/include/asm/mach-imx/fbpanel.h
+++ arch/arm/include/asm/mach-imx/fbpanel.h
@@ -131,6 +131,8 @@ struct display_info_t {
 #define FBF_LTK0680YTMDB_LCD	(FBF_MODE_SKIP_EOT | FBF_MODE_VIDEO | FBF_MIPI_CMDS | FBF_DSI_LANES_4 | FBF_MODESTR)
 #define FBF_LTK069WXBCT02	(FBF_MODE_SKIP_EOT | FBF_MODE_VIDEO | FBF_MODE_VIDEO_BURST | FBF_MIPI_CMDS | FBF_DSI_LANES_4 | FBF_PINCTRL | FBF_ENABLE_GPIOS_DTB)
 #define FBF_LTK069WXBCT02_LCD	(FBF_MODE_SKIP_EOT | FBF_MODE_VIDEO | FBF_MIPI_CMDS | FBF_DSI_LANES_4 | FBF_MODESTR)
+#define FBF_LTK069WXICT10	(FBF_MODE_SKIP_EOT | FBF_MODE_VIDEO | FBF_MODE_VIDEO_BURST | FBF_MIPI_CMDS | FBF_DSI_LANES_4 | FBF_PINCTRL | FBF_ENABLE_GPIOS_DTB)
+#define FBF_LTK069WXICT10_LCD	(FBF_MODE_SKIP_EOT | FBF_MODE_VIDEO | FBF_MIPI_CMDS | FBF_DSI_LANES_4 | FBF_MODESTR)
 #define FBF_LXD_M8509A		(FBF_MODE_SKIP_EOT | FBF_MODE_VIDEO | FBF_MODE_VIDEO_BURST | FBF_MIPI_CMDS | FBF_DSI_LANES_2)
 #define FBF_MIPI_LT8912		(FBF_MODE_SKIP_EOT | FBF_MODE_VIDEO | FBF_MODE_VIDEO_BURST | FBF_DSI_LANES_4 |FBF_BKLIT_DTB)
 #define FBF_MIPI_LT8912_2	(FBF_MODE_SKIP_EOT | FBF_MODE_VIDEO | FBF_MODE_VIDEO_BURST | FBF_DSI_LANES_4)
@@ -291,6 +293,7 @@ void fbp_setup_env_cmds(void);
 #define VD_LTK0680YTMDB(_mode, args...)		VDF_LTK0680YTMDB(_mode, "ltk0680ytmdb", RGB24, FB_##_mode == FB_LCD ? FBF_LTK0680YTMDB_LCD : FBF_LTK0680YTMDB, args)
 #define VD_LTK0680YTMDB_2(_mode, args...)	VDF_LTK0680YTMDB_2(_mode, "ltk0680ytmdb-2", RGB24, FB_##_mode == FB_LCD ? FBF_LTK0680YTMDB_LCD : FBF_LTK0680YTMDB, args)
 #define VD_LTK069WXBCT02(_mode, args...)	VDF_LTK069WXBCT02(_mode, "ltk069wxbct02", RGB24, FB_##_mode == FB_LCD ? FBF_LTK069WXBCT02_LCD : FBF_LTK069WXBCT02, args)
+#define VD_LTK069WXICT10(_mode, args...)	VDF_LTK069WXICT10(_mode, "ltk069wxict10", RGB24, FB_##_mode == FB_LCD ? FBF_LTK069WXICT10_LCD : FBF_LTK069WXICT10, args)
 
 #define VD_MIPI_G156HCE_L01(_mode, args...)	VDF_MIPI_G156HCE_L01(_mode, "G156HCE-L01", RGB24, FBF_G156HCE_L01, args)
 #define VD_MIPI_COM50H5N03ULC(_mode, args...)	VDF_MIPI_COM50H5N03ULC(_mode, "com50h5n03ulc", RGB24, FBF_COM50H5N03ULC, args)
@@ -1719,6 +1722,26 @@ void fbp_setup_env_cmds(void);
 		.vsync_len      = 4,\
 		.sync           = FB_SYNC_EXT,\
 		.vmode          = FB_VMODE_NONINTERLACED\
+	}\
+}
+
+#define VDF_LTK069WXICT10(_mode, _name, _fmt, _flags, args...) \
+{\
+	VD_HEADER(_mode, _fmt, _flags, args),\
+	.mode	= {\
+		.name		= _name,\
+		.refresh	= 60,\
+		.xres		= 480,\
+		.yres		= 1280,\
+		.pixclock	= 1000000000000ULL/((480+50+50+4)*(1280+16+16+4)*60), \
+		.left_margin	= 50,\
+		.right_margin	= 50,\
+		.upper_margin	= 16,\
+		.lower_margin	= 16,\
+		.hsync_len	= 4,\
+		.vsync_len	= 4,\
+		.sync		= FB_SYNC_EXT,\
+		.vmode		= FB_VMODE_NONINTERLACED\
 	}\
 }
 
