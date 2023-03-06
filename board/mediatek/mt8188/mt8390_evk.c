0aae0a9b01d185d088ad5fcdb76a31be9c03332f
Macpaul Lin <macpaul.lin@mediatek.com>
2023-03-06 15:47:17 +0800

    GENIO: mt8390: board: Add firmware image information for capsule updates
    Add information that will be needed for enabling the UEFI capsule
    update feature on the mt8188/mt8390 boards. With the feature enabled, it
    would be possible to update the fitImage and fip.bin (u-boot/TF-A/OPTEE)
    images on the mt8188/mt8390 variants.
    
    Change-Id: Ib5c5df27e7b9812a537fa4cd52b6ce9b02369579
    Signed-off-by: Macpaul Lin <macpaul.lin@mediatek.com>

--- board/mediatek/mt8188/mt8390_evk.c
+++ board/mediatek/mt8188/mt8390_evk.c
@@ -6,8 +6,46 @@
 
 #include <common.h>
 #include <dm.h>
+#include <efi_loader.h>
 #include <net.h>
 #include <asm/io.h>
+#include <linux/kernel.h>
+
+#define MT8390_UPDATABLE_IMAGES	2
+
+#if CONFIG_IS_ENABLED(EFI_HAVE_CAPSULE_SUPPORT)
+static struct efi_fw_image fw_images[MT8390_UPDATABLE_IMAGES] = {0};
+
+struct efi_capsule_update_info update_info = {
+	.images = fw_images,
+};
+
+u8 num_image_type_guids = MT8390_UPDATABLE_IMAGES;
+#endif
+
+#if defined(CONFIG_EFI_HAVE_CAPSULE_SUPPORT) && defined(CONFIG_EFI_PARTITION)
+static bool board_is_genio_700_evk(void)
+{
+	return CONFIG_IS_ENABLED(TARGET_MT8390) &&
+		of_machine_is_compatible("mediatek,genio-700-evk");
+}
+
+void mediatek_capsule_update_board_setup(void)
+{
+	if (board_is_genio_700_evk()) {
+		efi_guid_t image_type_guid =
+			GENIO_700_EVK_FIT_IMAGE_GUID;
+		efi_guid_t uboot_image_type_guid = GENIO_700_EVK_FIP_IMAGE_GUID;
+
+		guidcpy(&fw_images[0].image_type_id, &image_type_guid);
+		guidcpy(&fw_images[1].image_type_id, &uboot_image_type_guid);
+
+		fw_images[0].fw_name = u"GENIO-700-EVK-FIT";
+		fw_images[1].fw_name = u"GENIO-700-EVK-FIP";
+	}
+}
+#endif /* CONFIG_EFI_HAVE_CAPSULE_SUPPORT && CONFIG_EFI_PARTITION */
+
 int board_init(void)
 {
 	struct udevice *dev;
--- include/configs/mt8188.h
+++ include/configs/mt8188.h
@@ -17,6 +17,15 @@
 #define CONFIG_SYS_NS16550_COM1		0x11002000
 #define CONFIG_SYS_NS16550_CLK		26000000
 
+
+#define GENIO_700_EVK_FIT_IMAGE_GUID \
+	EFI_GUID(0x40ecb7c9, 0x416e, 0x4524, 0xb4, 0x95, \
+		 0xa9, 0x67, 0xe0, 0x69, 0x9c, 0xb1)
+
+#define GENIO_700_EVK_FIP_IMAGE_GUID \
+	EFI_GUID(0x551cc92d, 0x9a22, 0x4dc8, 0x88, 0x72, \
+		 0xdb, 0x1b, 0xb8, 0xdc, 0xaf, 0xa0)
+
 /* Environment settings */
 #include <config_distro_bootcmd.h>
 
@@ -24,6 +33,12 @@
 #define BOOT_TARGET_MMC(func) func(MMC, mmc, 0)
 #else
 #define BOOT_TARGET_MMC(func)
+#endif
+
+#ifdef CONFIG_CMD_USB
+#define BOOT_TARGET_USB(func) func(USB, usb, 0)
+#else
+#define BOOT_TARGET_USB(func)
 #endif
 
 #ifdef CONFIG_CMD_SCSI
@@ -34,6 +49,7 @@
 
 #define BOOT_TARGET_DEVICES(func) \
 	BOOT_TARGET_MMC(func) \
+	BOOT_TARGET_USB(func) \
 	BOOT_TARGET_SCSI(func)
 
 #if !defined(CONFIG_EXTRA_ENV_SETTINGS)
