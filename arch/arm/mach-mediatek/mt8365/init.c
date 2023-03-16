73ce1e22c774f870bcb7706c8b4ceeba8f616afe
Macpaul Lin <macpaul.lin@mediatek.com>
2023-03-16 16:55:11 +0800

    ARM: mediatek: mt8365: update DRAM size up to 3GB
    Update supported DRAM size up to 3GB.
    
    Change-Id: Ib1c6908e82baf6d5e675f9c9c47d9376fef61dec
    Signed-off-by: Macpaul Lin <macpaul.lin@mediatek.com>

--- arch/arm/mach-mediatek/mt8365/init.c
+++ arch/arm/mach-mediatek/mt8365/init.c
@@ -58,12 +58,12 @@ static struct mm_region mt8365_mem_map[] = {
 		/* DDR */
 		.virt = 0x40000000UL,
 		.phys = 0x40000000UL,
-		.size = 0x40000000UL,
+		.size = 0xC0000000UL,
 		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) | PTE_BLOCK_OUTER_SHARE,
 	}, {
 		.virt = 0x00000000UL,
 		.phys = 0x00000000UL,
-		.size = 0x20000000UL,
+		.size = 0xC0000000UL,
 		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
 			 PTE_BLOCK_NON_SHARE |
 			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
