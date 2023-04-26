ab17b46f861b673ec7ae565eec16e7b8d453851f
Ye Li <ye.li@nxp.com>
2023-04-26 11:11:56 +0800

    LFU-488 imx8ulp: Support 8ULP variant part numbers
    8ULP has below variant parts. Add support for these part numbers.
    Due to EPDC/PXP and GPU2/3D will be fused, so by default not enable
    power switches and memory partitions of them.
    
    D7:  full feature
    D5:  ePXP+EPDC disabled
    D3:  ePXP+EPDC + GPU disabled
    S5/S3:  Solo core base on D5/D3
    SC:  900Mhz A35 base on D5
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- arch/arm/mach-imx/imx8ulp/upower/upower_hal.c
+++ arch/arm/mach-imx/imx8ulp/upower/upower_hal.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0+
+// SPDX-License-Identifier: BSD-3-Clause
 /*
  * Copyright 2021 NXP
  */
@@ -43,12 +43,12 @@ void upower_apd_inst_isr(upwr_isr_callb txrx_isr, upwr_isr_callb excp_isr)
 
 void upower_wait_resp(void)
 {
-    while(muptr->RSR.B.RF0 == 0) {
-        debug("%s: poll the mu:%x\n", __func__, muptr->RSR.R);
-        udelay(100);
-    }
+	while (muptr->RSR.B.RF0 == 0) {
+		debug("%s: poll the mu:%x\n", __func__, muptr->RSR.R);
+		udelay(100);
+	}
 
-    upwr_txrx_isr();
+	upwr_txrx_isr();
 }
 
 void usr_upwr_callb(upwr_sg_t sg, uint32_t func, upwr_resp_t errcode, int ret)
@@ -58,26 +58,27 @@ void usr_upwr_callb(upwr_sg_t sg, uint32_t func, upwr_resp_t errcode, int ret)
 
 u32 upower_status(int status)
 {
-    u32 ret = -1;
-    switch(status) {
-        case 0:
-            debug("%s: finished successfully!\n", __func__);
-            ret = 0;
-            break;
-        case -1:
-            printf("%s: memory allocation or resource failed!\n", __func__);
-            break;
-        case -2:
-            printf("%s: invalid argument!\n", __func__);
-            break;
-        case -3:
-            printf("%s: called in an invalid API state!\n", __func__);
-            break;
-        default:
-            printf("%s: invalid return status\n", __func__);
-            break;
-    }
-    return ret;
+	u32 ret = -1;
+
+	switch (status) {
+	case 0:
+		debug("%s: finished successfully!\n", __func__);
+		ret = 0;
+		break;
+	case -1:
+		printf("%s: memory allocation or resource failed!\n", __func__);
+		break;
+	case -2:
+		printf("%s: invalid argument!\n", __func__);
+		break;
+	case -3:
+		printf("%s: called in an invalid API state!\n", __func__);
+		break;
+	default:
+		printf("%s: invalid return status\n", __func__);
+		break;
+	}
+	return ret;
 }
 
 void user_upwr_rdy_callb(uint32_t soc, uint32_t vmajor, uint32_t vminor)
@@ -93,14 +94,14 @@ int upower_pmic_i2c_write(u32 reg_addr, u32 reg_val)
 
 	ret = upwr_xcp_i2c_access(0x32, 1, 1, reg_addr, reg_val, NULL);
 	if (ret) {
-		printf("pmic i2c read failed ret %d\n", ret);
+		printf("pmic i2c write failed ret %d\n", ret);
 		return ret;
 	}
 
 	upower_wait_resp();
 	ret = upwr_poll_req_status(UPWR_SG_EXCEPT, NULL, &err_code, &ret_val, 1000);
 	if (ret != UPWR_REQ_OK) {
-		printk("i2c poll Faliure %d, err_code %d, ret_val 0x%x\n", ret, err_code, ret_val);
+		printf("i2c poll Failure %d, err_code %d, ret_val 0x%x\n", ret, err_code, ret_val);
 		return ret;
 	}
 
@@ -126,7 +127,7 @@ int upower_pmic_i2c_read(u32 reg_addr, u32 *reg_val)
 	upower_wait_resp();
 	ret = upwr_poll_req_status(UPWR_SG_EXCEPT, NULL, &err_code, &ret_val, 1000);
 	if (ret != UPWR_REQ_OK) {
-		printk("i2c poll Faliure %d, err_code %d, ret_val 0x%x\n", ret, err_code, ret_val);
+		printf("i2c poll Failure %d, err_code %d, ret_val 0x%x\n", ret, err_code, ret_val);
 		return ret;
 	}
 
@@ -158,13 +159,13 @@ int upower_init(void)
 		}
 
 		soc_id = upwr_rom_version(&fw_major, &fw_minor, &fw_vfixes);
-		if (soc_id == 0) {
+		if (!soc_id) {
 			printf("%s:, soc_id not initialized\n", __func__);
 			break;
-		} else {
-			printf("%s: soc_id=%d\n", __func__, soc_id);
-			printf("%s: version:%d.%d.%d\n", __func__, fw_major, fw_minor, fw_vfixes);
 		}
+
+		printf("%s: soc_id=%d\n", __func__, soc_id);
+		printf("%s: version:%d.%d.%d\n", __func__, fw_major, fw_minor, fw_vfixes);
 
 		printf("%s: start uPower RAM service\n", __func__);
 		status = upwr_start(1, user_upwr_rdy_callb);
@@ -173,11 +174,11 @@ int upower_init(void)
 			printf("%s: upower init failure\n", __func__);
 			break;
 		}
-	} while(0);
+	} while (0);
 
-	swton = PS_UPOWER | PS_FUSE | PS_FUSION_AO | PS_NIC_LPAV | PS_PXP_EPDC | PS_DDR |
-		PS_HIFI4 | PS_GPU3D | PS_MIPI_DSI;
-	ret = upwr_pwm_power_on(&swton, NULL /* no memories */, NULL /* no callback */);
+	swton = PS_UPOWER | PS_FUSE | PS_FUSION_AO | PS_NIC_LPAV | PS_DDR |
+		PS_HIFI4 | PS_MIPI_DSI;
+	ret = upwr_pwm_power_on(&swton, NULL, NULL);
 	if (ret)
 		printf("Turn on switches fail %d\n", ret);
 	else
@@ -248,8 +249,8 @@ int upower_init(void)
 	 * Sentinel Data RAM0, Inst RAM2
 	 */
 	/* MIPI-CSI FIFO BIT28 not set */
-	memon = 0x3FFFFFEFFFFFFCUL;
-	ret = upwr_pwm_power_on(NULL, (const uint32_t *)&memon /* no memories */, NULL /* no callback */);
+	memon = 0x3FFFFFAC27FFFCUL;
+	ret = upwr_pwm_power_on(NULL, (const uint32_t *)&memon, NULL);
 	if (ret)
 		printf("Turn on memories fail %d\n", ret);
 	else
