boundarydevices/u-boot

commit b961c7a22bc7c4f79ad98659a2b2563694994654

GENIO: mt8188: bootlogo: add configs for bootlogo
Details

mt8188.h
a. set splashimage as destination to read logo.bmp
b. set splashsource as mmc_fs to read from a file system
c. set splashfile as logo.bmp to assign the logo file name
d. set splashdevpart as bootassets partition
e. set splashpos=m,m to put logo in the middle of the panel
f. set CONFIG_SYS_MALLOC_LEN to SZ_64M.
   the framebuffer for bootlogo will be mallocated

genio_700_evk_defconfig
a. enable several must configs for bootlogo
b. default background is set to white
   to set to black, set CONFIG_SYS_WHITE_ON_BLACK = y

Change-Id: I437e121bd7fa6452ebf198516ae287b0e07460d7
Signed-off-by: Tommy Chen <tommyyl.chen@mediatek.com>

include/configs/mt8188.h
@@ -72,11 +72,22 @@
 	"kernel_addr_r=0x45000000\0" \
 	"ramdisk_addr_r=0x46000000\0" \
 	"fdtfile=" CONFIG_DEFAULT_DEVICE_TREE ".dtb\0" \
+	"splashimage=0x60000000\0" \
+	"splashsource=mmc_fs\0" \
+	"splashfile=logo.bmp\0" \
+	"splashdevpart=0#bootassets\0" \
+	"splashpos=m,m\0" \
 	BOOTENV
 #endif
 
 #ifdef CONFIG_ARM64
 #define MTK_SIP_PARTNAME_ID		0xC2000529
 #endif
+
+#ifdef CONFIG_SYS_MALLOC_LEN
+#undef CONFIG_SYS_MALLOC_LEN
+#endif
+
+#define CONFIG_SYS_MALLOC_LEN		SZ_64M
 
 #endif

configs/genio_700_evk_defconfig
@@ -44,6 +44,7 @@ CONFIG_CMD_I2C=y
 CONFIG_CMD_MBR=y
 CONFIG_CMD_MMC=y
 # CONFIG_CMD_ITEST is not set
+CONFIG_CMD_BMP=y
 # CONFIG_CMD_BLOCK_CACHE is not set
 CONFIG_CMD_EFIDEBUG=y
 CONFIG_PARTITION_TYPE_GUID=y
@@ -51,6 +52,9 @@ CONFIG_ENV_IS_IN_MMC=y
 CONFIG_SYS_MMC_ENV_PART=2
 CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG=y
 CONFIG_ENV_IMPORT_FDT=y
+CONFIG_DM_ALLOW_MULTIPLE_DRIVERS=y
+CONFIG_REGMAP=y
+CONFIG_SYSCON=y
 CONFIG_DEVRES=y
 CONFIG_CLK=y
 CONFIG_DFU_MMC=y
@@ -59,6 +63,7 @@ CONFIG_SYS_DFU_MAX_FILE_SIZE=0x200000
 CONFIG_DM_I2C=y
 CONFIG_SYS_I2C_MTK=y
 # CONFIG_INPUT is not set
+CONFIG_MISC=y
 # CONFIG_MMC_QUIRKS is not set
 CONFIG_MMC_HS400_SUPPORT=y
 CONFIG_MMC_MTK=y
@@ -67,6 +72,12 @@ CONFIG_PHY_MTK_TPHY=y
 CONFIG_PINCTRL=y
 CONFIG_PINCONF=y
 CONFIG_PINCTRL_MT8188=y
+CONFIG_POWER_DOMAIN=y
+CONFIG_MTK_POWER_DOMAIN=y
+CONFIG_DM_PMIC=y
+CONFIG_MTK_PMIC_WRAP=y
+CONFIG_DM_REGULATOR=y
+CONFIG_DM_REGULATOR_MT6357=y
 CONFIG_DM_RTC=y
 CONFIG_RTC_EMULATION=y
 CONFIG_BAUDRATE=921600
@@ -75,7 +86,14 @@ CONFIG_DEBUG_UART_ANNOUNCE=y
 CONFIG_MTK_SERIAL=y
 CONFIG_SYSRESET=y
 CONFIG_SYSRESET_PSCI=y
+CONFIG_DM_VIDEO=y
+CONFIG_VIDEO_MEDIATEK=y
+CONFIG_SPLASH_SCREEN=y
+CONFIG_SPLASH_SCREEN_ALIGN=y
+CONFIG_SPLASH_SOURCE=y
+CONFIG_BMP_24BPP=y
 CONFIG_WDT=y
+CONFIG_ERRNO_STR=y
 CONFIG_OF_LIBFDT_OVERLAY=y
 CONFIG_EFI_SET_TIME=y
 CONFIG_EFI_RUNTIME_UPDATE_CAPSULE=y

commit 819d0485c8900d326ecbf3cd0a4fa69995aae2eb

GENIO: mt8188: bootlogo: fix splashimage address
Enable splashimage causes USB and UFS boot failed.
The address of splashimage should be configured as
"splashimage=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0"
instead of other fixed address for avoiding memory
management issues.

this fixes: b961c7a22bc7c4f79ad98659a2b2563694994654

Change-Id: I98a9dc0cd4963711d33516a11a9d9574e051cd79
Signed-off-by: Macpaul Lin <macpaul.lin@mediatek.com>

include/configs/mt8188.h
@@ -72,7 +72,7 @@
 	"kernel_addr_r=0x45000000\0" \
 	"ramdisk_addr_r=0x46000000\0" \
 	"fdtfile=" CONFIG_DEFAULT_DEVICE_TREE ".dtb\0" \
-	"splashimage=0x60000000\0" \
+	"splashimage=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
 	"splashsource=mmc_fs\0" \
 	"splashfile=logo.bmp\0" \
 	"splashdevpart=0#bootassets\0" \