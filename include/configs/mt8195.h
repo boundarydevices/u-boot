boundarydevices/u-boot

commit 2b26f45d8062d03a55c93a4e381cc6aebf709140

GENIO: mt8195: bootlogo: add configs for bootlogo
Details

mt8195.h
a. set splashimage as destination to read logo.bmp
b. set splashsource as mmc_fs to read from a file system
c. set splashfile as logo.bmp to assign the logo file name
d. set splashdevpart as bootassets partition
e. set splashpos=m,m to put logo in the middle of the panel
f. set CONFIG_SYS_MALLOC_LEN to SZ_64M.
   the framebuffer for bootlogo will be mallocated

genio_1200_evk_defconfig
a. enable several must configs for bootlogo
b. default background is set to white
   to set to black, set CONFIG_SYS_WHITE_ON_BLACK = y

Change-Id: Ia677b13f4dc46c90edc44bc2433ca9dded08c8bd
Signed-off-by: Tommy Chen <tommyyl.chen@mediatek.com>

include/configs/mt8195.h
@@ -111,11 +111,22 @@
 	"kernel_addr_r=0x45000000\0" \
 	"ramdisk_addr_r=0x46000000\0" \
 	"fdtfile=" CONFIG_DEFAULT_DEVICE_TREE ".dtb\0" \
+	"splashimage=0x60000000\0" \
+	"splashsource=mmc_fs\0" \
+	"splashfile=logo.bmp\0" \
+	"splashdevpart=0#bootassets\0" \
+	"splashpos=m,m\0" \
 	BOOTENV
 #endif
 
 #ifdef CONFIG_ARM64
 #define MTK_SIP_PARTNAME_ID		0xC2000529
 #endif
+
+#ifdef CONFIG_SYS_MALLOC_LEN
+#undef CONFIG_SYS_MALLOC_LEN
+#endif
+
+#define CONFIG_SYS_MALLOC_LEN		SZ_64M
 
 #endif

configs/genio_1200_evk_defconfig
@@ -45,6 +45,7 @@ CONFIG_CMD_MMC=y
 CONFIG_CMD_USB=y
 CONFIG_CMD_USB_MASS_STORAGE=y
 # CONFIG_CMD_ITEST is not set
+CONFIG_CMD_BMP=y
 # CONFIG_CMD_BLOCK_CACHE is not set
 CONFIG_CMD_EFIDEBUG=y
 CONFIG_PARTITION_TYPE_GUID=y
@@ -52,6 +53,7 @@ CONFIG_ENV_IS_IN_MMC=y
 CONFIG_SYS_MMC_ENV_PART=2
 CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG=y
 CONFIG_ENV_IMPORT_FDT=y
+CONFIG_DM_ALLOW_MULTIPLE_DRIVERS=y
 CONFIG_REGMAP=y
 CONFIG_SYSCON=y
 CONFIG_DEVRES=y
@@ -68,6 +70,7 @@ CONFIG_FASTBOOT_MMC_BOOT_SUPPORT=y
 CONFIG_DM_I2C=y
 CONFIG_SYS_I2C_MTK=y
 # CONFIG_INPUT is not set
+CONFIG_MISC=y
 # CONFIG_MMC_QUIRKS is not set
 CONFIG_MMC_HS400_SUPPORT=y
 CONFIG_MMC_MTK=y
@@ -80,6 +83,12 @@ CONFIG_PHY_MTK_TPHY=y
 CONFIG_PINCTRL=y
 CONFIG_PINCONF=y
 CONFIG_PINCTRL_MT8195=y
+CONFIG_POWER_DOMAIN=y
+CONFIG_MTK_POWER_DOMAIN=y
+CONFIG_DM_PMIC=y
+CONFIG_MTK_PMIC_WRAP=y
+CONFIG_DM_REGULATOR=y
+CONFIG_DM_REGULATOR_MT6357=y
 CONFIG_DM_RTC=y
 CONFIG_RTC_EMULATION=y
 CONFIG_BAUDRATE=921600
@@ -104,7 +113,14 @@ CONFIG_USB_GADGET=y
 CONFIG_USB_GADGET_VENDOR_NUM=0x0e8d
 CONFIG_USB_GADGET_PRODUCT_NUM=0x201c
 CONFIG_USB_ETHER=y
+CONFIG_DM_VIDEO=y
+CONFIG_VIDEO_MEDIATEK=y
+CONFIG_SPLASH_SCREEN=y
+CONFIG_SPLASH_SCREEN_ALIGN=y
+CONFIG_SPLASH_SOURCE=y
+CONFIG_BMP_24BPP=y
 CONFIG_WDT=y
+CONFIG_ERRNO_STR=y
 CONFIG_OF_LIBFDT_OVERLAY=y
 CONFIG_EFI_SET_TIME=y
 CONFIG_EFI_RUNTIME_UPDATE_CAPSULE=y

commit 4817c1e199ecd966314de94ce21bcf600a15c54e

GENIO: mt8195: bootlogo: fix splashimage address
Enable splashimage causes USB and UFS boot failed.
The address of splashimage should be configured as
"splashimage=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0"
instead of other fixed address for avoiding memory
management issues.

this fixes: 2b26f45d8062d03a55c93a4e381cc6aebf709140

Change-Id: Ia698e4fa84e1227eee2368014bd1dd8789fdf30f
Signed-off-by: Macpaul Lin <macpaul.lin@mediatek.com>

include/configs/mt8195.h
@@ -111,7 +111,7 @@
 	"kernel_addr_r=0x45000000\0" \
 	"ramdisk_addr_r=0x46000000\0" \
 	"fdtfile=" CONFIG_DEFAULT_DEVICE_TREE ".dtb\0" \
-	"splashimage=0x60000000\0" \
+	"splashimage=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
 	"splashsource=mmc_fs\0" \
 	"splashfile=logo.bmp\0" \
 	"splashdevpart=0#bootassets\0" \