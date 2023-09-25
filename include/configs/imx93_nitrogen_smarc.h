0f1b1cc350791ee45ecba01e0ef6150c54063b1e
Simon Gaynor <simon.gaynor@lairdconnect.com>
2023-09-25 15:26:12 -0700

    imx93_nitrogen_smarc: initial addition
    imx93_nitrogen_smarc: fix  undefined reference to mmc_map_to_kernel_blk
    arm: dts: imx93-nitrogen-smarc: fix incorrect ethphy2 interrupt pin
    imx93_nitrogen_smarc: use bl31-iMX93.bin instead of bl31.bin
    
    Signed-off-by: Simon Gaynor <simon.gaynor@lairdconnect.com>
    
    imx93_nitrogen_smarc: rename folder
    imx93_nitrogen_smarc: align with latest evk code
    imx93_nitrogen_smarc: run savedefconfig
    imx93_nitrogen_smarc: fix build issues
    imx93_nitrogen_smarc: align dts with evk
    arm: dts: imx93-nitrogen-smarc: fix ethernet phy resets
    imx93_nitrogen_smarc_defconfig: fix USB IDs
    imx93_nitrogen_smarc: add serial# variable
    imx93_nitrogen_smarc: fix indentation
    imx93_nitrogen_smarc: remove unnecessary bl31 binary
    imx93_nitrogen_smarc: fix build issue
    imx93_nitrogen_smarc: sync default env with other nitrogen platforms
    imx93_nitrogen_smarc: fix indentation issues
    
    Signed-off-by: Gary Bisson <gary.bisson@ezurio.com>

---
+++ include/configs/imx93_nitrogen_smarc.h
@@ -0,0 +1,104 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2022 NXP
+ */
+
+#ifndef __IMX93_NITROGEN_SMARC_H
+#define __IMX93_NITROGEN_SMARC_H
+
+#include <linux/sizes.h>
+#include <linux/stringify.h>
+#include <asm/arch/imx-regs.h>
+#include "imx_env.h"
+
+#define CFG_SYS_UBOOT_BASE	\
+	(QSPI0_AMBA_BASE + CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR * 512)
+
+#ifdef CONFIG_AHAB_BOOT
+#define AHAB_ENV "sec_boot=yes\0"
+#else
+#define AHAB_ENV "sec_boot=no\0"
+#endif
+
+#ifdef CONFIG_DISTRO_DEFAULTS
+#define BOOT_TARGET_DEVICES(func) \
+	func(MMC, mmc, 0) \
+	func(MMC, mmc, 1) \
+	func(USB, usb, 0)
+
+#include <config_distro_bootcmd.h>
+#else
+#define BOOTENV
+#endif
+
+#define CFG_MFG_ENV_SETTINGS \
+	CFG_MFG_ENV_SETTINGS_DEFAULT \
+	"initrd_addr=0x83800000\0" \
+	"initrd_high=0xffffffffffffffff\0" \
+	"emmc_dev=0\0"\
+	"sd_dev=1\0" \
+
+#define NITROGEN_SMARC_DEFCONFIG	"imx93_nitrogen_smarc"
+
+/* Initial environment variables */
+#define CFG_EXTRA_ENV_SETTINGS		\
+	CFG_MFG_ENV_SETTINGS \
+	BOOTENV \
+	AHAB_ENV \
+	"board=nitrogen-smarc\0" \
+	"soc_type=imx93\0" \
+	"env_dev=" __stringify(CONFIG_SYS_MMC_ENV_DEV) "\0" \
+	"env_part=" __stringify(CONFIG_SYS_MMC_ENV_PART) "\0" \
+	"fastboot_raw_partition_bootloader=0x0 0x1ff0 mmcpart 1\0" \
+	"fastboot_raw_partition_bootloader-env=0x1ff0 0x10 mmcpart 1\0" \
+	"mcoreboot=load ${devtype} ${devnum}:1 ${mcore_loadaddr} ${mcore_image}; " \
+		"dcache flush; bootaux " __stringify(TCML_BASE_MCORE_SEC_ADDR)"\0" \
+	"mcore_image=m33_fw.bin\0" \
+	"mcore_loadaddr=" __stringify(CONFIG_IMX_MCORE_TCM_ADDR) "\0" \
+	"mcore_bootargs=clk-imx93.mcore_booted\0" \
+	"scriptaddr=0x83500000\0" \
+	"console=ttyLP0\0" \
+	"fdt_addr=0x83000000\0"			\
+	"fdt_high=0xffffffffffffffff\0"		\
+	"fdt_file=" CONFIG_DEFAULT_FDT_FILE "\0" \
+	"netargs=setenv bootargs console=${console},115200 root=/dev/nfs rw " \
+		"ip=dhcp nfsroot=${tftpserverip}:${nfsroot},v3,tcp\0" \
+	"netboot=echo Booting from net ...; " \
+		"run netargs;  " \
+		"if test ${ip_dyn} = yes; then " \
+			"setenv get_cmd dhcp; " \
+		"else " \
+			"setenv get_cmd tftp; " \
+		"fi; " \
+		"${get_cmd} ${loadaddr} ${tftpserverip}:Image; " \
+		"if ${get_cmd} ${fdt_addr} ${tftpserverip}:${fdt_file}; then " \
+			"booti ${loadaddr} - ${fdt_addr}; " \
+		"else " \
+			"echo WARN: Cannot load the DT; " \
+		"fi;\0" \
+	"upgradeu=setenv boot_scripts upgrade.scr; boot;" \
+                "echo Upgrade failed!; setenv boot_scripts boot.scr;\0" \
+	"uboot_defconfig=" NITROGEN_SMARC_DEFCONFIG "\0"
+/* Link Definitions */
+
+#define CFG_SYS_INIT_RAM_ADDR        0x80000000
+#define CFG_SYS_INIT_RAM_SIZE        0x200000
+
+#define CFG_SYS_SDRAM_BASE           0x80000000
+#define PHYS_SDRAM                      0x80000000
+#define PHYS_SDRAM_SIZE			0x80000000 /* 2GB DDR */
+
+#define CFG_SYS_FSL_USDHC_NUM	2
+
+/* Using ULP WDOG for reset */
+#define WDOG_BASE_ADDR          WDG3_BASE_ADDR
+
+#if defined(CONFIG_CMD_NET)
+#define PHY_ANEG_TIMEOUT 20000
+#endif
+
+#ifdef CONFIG_IMX_MATTER_TRUSTY
+#define NS_ARCH_ARM64 1
+#endif
+
+#endif
