1734d7287b41e77c5b41796dab83a2af5e18bd24
Gary Bisson <gary.bisson@boundarydevices.com>
2023-10-17 11:11:05 +0200

    imx8ulp_nitrogen_som: initial addition
    - renaming tag-test into nitrogen-som
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

---
+++ board/boundary/imx8ulp_nitrogen_som/spl.c
@@ -0,0 +1,177 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2023 Boundary Devices
+ */
+
+#include <common.h>
+#include <init.h>
+#include <spl.h>
+#include <asm/io.h>
+#include <errno.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/arch/imx8ulp-pins.h>
+#include <asm/mach-imx/s400_api.h>
+#include <fsl_sec.h>
+#include <dm/uclass.h>
+#include <dm/device.h>
+#include <dm/uclass-internal.h>
+#include <dm/device-internal.h>
+#include <dm/lists.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/ddr.h>
+#include <asm/arch/pcc.h>
+#include <asm/arch/rdc.h>
+#include <asm/arch/upower.h>
+#include "../common/bd_common.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+u32 spl_boot_device(void)
+{
+	return BOOT_DEVICE_BOOTROM;
+}
+
+int power_init_board(void)
+{
+	u32 buck3val;
+
+	if (IS_ENABLED(CONFIG_IMX8ULP_LD_MODE)) {
+		/* Set buck3 to 0.9v LD */
+		buck3val = 0x18;
+	} else if (IS_ENABLED(CONFIG_IMX8ULP_ND_MODE)) {
+		/* Set buck3 to 1.0v ND */
+		buck3val = 0x20;
+	} else {
+		/* Set buck3 to 1.1v OD */
+		buck3val = 0x28;
+	}
+
+	upower_pmic_i2c_write(0x4e, 0x5C); /* unlock */
+	upower_pmic_i2c_write(0x22, buck3val);	/* PMIC_STBY_REQ = L */
+	upower_pmic_i2c_write(0x2a, buck3val);	/* PMIC_STBY_REQ = H */
+	upower_pmic_i2c_write(0x1D, 0x20); 	/* BUCK2 DVS: 1.0V (0.6+(0x20*12.5)) = 1.000 */
+	upower_pmic_i2c_write(0x4e, 0); /* lock */
+
+	return 0;
+}
+
+void display_ele_fw_version(void)
+{
+	u32 fw_version, sha1, res;
+	int ret;
+
+	ret = ahab_get_fw_version(&fw_version, &sha1, &res);
+	if (ret) {
+		printf("ahab get firmware version failed %d, 0x%x\n", ret, res);
+	} else {
+		printf("ELE firmware version %u.%u.%u-%x",
+		       (fw_version & (0x00ff0000)) >> 16,
+		       (fw_version & (0x0000ff00)) >> 8,
+		       (fw_version & (0x000000ff)), sha1);
+		((fw_version & (0x80000000)) >> 31) == 1 ? puts("-dirty\n") : puts("\n");
+	}
+}
+
+void spl_board_init(void)
+{
+	struct udevice *dev;
+	u32 res;
+	int ret;
+	u32 reg, value;
+
+	ret = arch_cpu_init_dm();
+	if (ret)
+		return;
+
+	board_early_init_f();
+
+	preloader_console_init();
+
+	puts("Normal Boot\n");
+
+	display_ele_fw_version();
+
+	/* Load the lposc fuse to work around ROM issue,
+	 *  The fuse depends on S400 to read.
+	 */
+	if (is_soc_rev(CHIP_REV_1_0))
+		load_lposc_fuse();
+
+	upower_init();
+
+	power_init_board();
+
+	clock_init_late();
+
+	/* This must place after upower init, so access to MDA and MRC are valid */
+	/* Init XRDC MDA  */
+	xrdc_init_mda();
+
+	/* Init XRDC MRC for VIDEO, DSP domains */
+	xrdc_init_mrc();
+
+	xrdc_init_pdac_msc();
+
+	/* DDR initialization */
+	spl_dram_init();
+
+	/* Call it after PS16 power up */
+	set_lpav_qos();
+
+	/* Asks S400 to release CAAM for A35 core */
+	ret = ahab_release_caam(7, &res);
+	if (!ret) {
+
+		/* Only two UCLASS_MISC devicese are present on the platform. There
+		 * are MU and CAAM. Here we initialize CAAM once it's released by
+		 * S400 firmware..
+		 */
+		if (IS_ENABLED(CONFIG_FSL_CAAM)) {
+			ret = uclass_get_device_by_driver(UCLASS_MISC, DM_DRIVER_GET(caam_jr), &dev);
+			if (ret)
+				printf("Failed to initialize caam_jr: %d\n", ret);
+		}
+	}
+
+	/*
+	 * RNG start only available on the A1 soc revision.
+	 * Check some JTAG register for the SoC revision.
+	 */
+	if (!is_soc_rev(CHIP_REV_1_0)) {
+		ret = ahab_start_rng();
+		if (ret)
+			printf("Fail to start RNG: %d\n", ret);
+	}
+
+	printf("\n"
+	       "                    PMIC registers:\n"
+	       "    00 01 02 03 04 05 06 07   08 09 0a 0b 0c 0d 0e 0f\n"
+	       "    -- -- -- -- -- -- -- --   -- -- -- -- -- -- -- --\n");
+	for (reg=0; reg <= 0x4e; reg++) {
+		if (0 == (reg & 0x0f))
+			printf("%02x: ", reg);
+		ret = upower_pmic_i2c_read(reg, &value);
+		if (ret)
+			printf("XX ");
+		else
+			printf("%02x ", value);
+		if (7 == (reg & 0x0f)) {
+			printf("  ");
+		} else if (0x0f == (reg & 0x0f)) {
+			putc('\n');
+		}
+	}
+	printf("\n\n");
+}
+
+void board_init_f(ulong dummy)
+{
+	/* Clear the BSS. */
+	memset(__bss_start, 0, __bss_end - __bss_start);
+
+	timer_init();
+
+	arch_cpu_init();
+
+	board_init_r(NULL, 0);
+}
---
+++ include/configs/imx8ulp_nitrogen_som.h
@@ -0,0 +1,162 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2023 Boundary Devices
+ */
+
+#ifndef __IMX8ULP_NIROGEN_SOM_H
+#define __IMX8ULP_NIROGEN_SOM_H
+
+#include <linux/sizes.h>
+#include <asm/arch/imx-regs.h>
+
+#ifdef CONFIG_BOARD_TYPE_SET
+#undef CONFIG_SYS_BOARD
+#define CONFIG_SYS_BOARD CONFIG_BOARD_TYPE
+#endif
+#define CONFIG_SYS_BOOTM_LEN		(SZ_64M)
+#define CONFIG_SPL_MAX_SIZE		(148 * 1024)
+#define CONFIG_SYS_MONITOR_LEN		(512 * 1024)
+#define CONFIG_SYS_UBOOT_BASE	(QSPI0_AMBA_BASE + CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR * 512)
+
+#ifdef CONFIG_SPL_BUILD
+#define CONFIG_SPL_STACK		0x22050000
+#define CONFIG_SPL_BSS_START_ADDR	0x22048000
+#define CONFIG_SPL_BSS_MAX_SIZE		0x2000	/* 8 KB */
+#define CONFIG_SYS_SPL_MALLOC_START	0x22040000
+#define CONFIG_SYS_SPL_MALLOC_SIZE	0x8000	/* 32 KB */
+
+#define CONFIG_MALLOC_F_ADDR		0x22040000
+
+#define CONFIG_SPL_ABORT_ON_RAW_IMAGE /* For RAW image gives a error info not panic */
+
+#endif
+
+/* ENET Config */
+#if defined(CONFIG_FEC_MXC)
+#define CONFIG_ETHPRIME                 "FEC"
+#define PHY_ANEG_TIMEOUT		20000
+
+#define CONFIG_FEC_XCV_TYPE		RMII
+#define CONFIG_FEC_MXC_PHYADDR		7
+
+#define IMX_FEC_BASE			0x29950000
+#endif
+
+#ifdef CONFIG_CMD_MMC
+#define DISTRO_BOOT_DEV_MMC(func) func(MMC, mmc, 0)
+#else
+#define DISTRO_BOOT_DEV_MMC(func)
+#endif
+
+#ifdef CONFIG_USB_STORAGE
+#define DISTRO_BOOT_DEV_USB(func) func(USB, usb, 0)
+#else
+#define DISTRO_BOOT_DEV_USB(func)
+#endif
+
+#ifndef BOOT_TARGET_DEVICES
+#define BOOT_TARGET_DEVICES(func) \
+	DISTRO_BOOT_DEV_USB(func) \
+	DISTRO_BOOT_DEV_MMC(func)
+#endif
+
+#ifdef CONFIG_DISTRO_DEFAULTS
+#include <config_distro_bootcmd.h>
+#else
+#define BOOTENV
+#endif
+#define CONFIG_CMD_FBPANEL
+/* Framebuffer */
+#if defined(CONFIG_VIDEO) || defined(CONFIG_DM_VIDEO)
+#define CONFIG_SYS_VIDEO_LOGO_MAX_SIZE (6 * 1024 * 1024)
+#define CONFIG_BMP_16BPP
+#define CONFIG_BMP_24BPP
+#define CONFIG_BMP_32BPP
+#define CONFIG_VIDEO_BMP_LOGO
+#endif
+
+#define BD_CONSOLE	"ttyLP3"
+#define BD_RAM_SCRIPT	"80020000"
+#define BD_RAM_KERNEL	"80800000"
+#define BD_RAM_RAMDISK	"82800000"
+#define BD_RAM_FDT	"83000000"
+
+/* Initial environment variables */
+#define CONFIG_EXTRA_ENV_SETTINGS		\
+	"console=" BD_CONSOLE "\0" \
+	"env_dev=" __stringify(CONFIG_SYS_MMC_ENV_DEV) "\0" \
+	"env_part=" __stringify(CONFIG_SYS_MMC_ENV_PART) "\0" \
+	"scriptaddr=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
+	"fastboot_raw_partition_bootloader=0x0 0x1ff0 mmcpart 1\0" \
+	"fastboot_raw_partition_bootloader-env=0x1ff0 0x10 mmcpart 1\0" \
+	"fdt_addr=0x83000000\0" \
+	"fdt_high=0xffffffffffffffff\0" \
+	"initrd_high=0xffffffffffffffff\0" \
+	"kernel_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
+	"image=Image\0" \
+	"boot_fit=no\0" \
+	"fdtfile=imx8ulp-nitrogen-som.dtb\0" \
+	"initrd_addr=0x83800000\0"		\
+	"bootm_size=0x10000000\0" \
+	"mmcdev="__stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
+	"mmcpart=1\0" \
+	"mmcroot=" CONFIG_MMCROOT " rootwait rw\0" \
+	"netargs=setenv bootargs console=${console},115200 root=/dev/nfs rw " \
+		"ip=dhcp nfsroot=${tftpserverip}:${nfsroot},v3,tcp\0" \
+	"netboot=run netargs; " \
+		"if test -z \"${fdt_file}\" -a -n \"${soc}\"; then " \
+			"setenv fdt_file ${soc}-${board}${boardver}.dtb; " \
+		"fi; " \
+		"if test ${ip_dyn} = yes; then " \
+			"setenv get_cmd dhcp; " \
+		"else " \
+			"setenv get_cmd tftp; " \
+		"fi; " \
+		"${get_cmd} ${loadaddr} ${tftpserverip}:Image; " \
+		"if ${get_cmd} ${fdt_addr} ${tftpserverip}:${fdt_file}; then " \
+			"booti ${loadaddr} - ${fdt_addr}; " \
+		"else " \
+			"echo WARN: Cannot load the DT; " \
+		"fi;\0" \
+	"net_upgradeu=dhcp " BD_RAM_SCRIPT " net_upgradeu.scr && source " BD_RAM_SCRIPT "\0" \
+	"otg_upgradeu=run usbnetwork; tftp " BD_RAM_SCRIPT " net_upgradeu.scr && source " BD_RAM_SCRIPT "\0" \
+	"upgradeu=setenv boot_scripts upgrade.scr; boot;" \
+		"echo Upgrade failed!; setenv boot_scripts boot.scr\0" \
+	"usbnet_devaddr=00:19:b8:00:00:02\0" \
+	"usbnet_hostaddr=00:19:b8:00:00:01\0" \
+	"usbnetwork=setenv ethact usb_ether; " \
+		"setenv ipaddr 10.0.0.2; " \
+		"setenv netmask 255.255.255.0; " \
+		"setenv serverip 10.0.0.1;\0" \
+	BOOTENV
+
+/* Link Definitions */
+
+#define CONFIG_SYS_INIT_RAM_ADDR	0x80000000
+#define CONFIG_SYS_INIT_RAM_SIZE	0x80000
+#define CONFIG_SYS_INIT_SP_OFFSET	(CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE)
+#define CONFIG_SYS_INIT_SP_ADDR		(CONFIG_SYS_INIT_RAM_ADDR + CONFIG_SYS_INIT_SP_OFFSET)
+
+#define CONFIG_MMCROOT			"/dev/mmcblk0p2"
+
+#define CONFIG_SYS_SDRAM_BASE		0x80000000
+#define PHYS_SDRAM			0x80000000
+#define PHYS_SDRAM_SIZE			0x80000000 /* 2GB DDR */
+
+/* Monitor Command Prompt */
+#define CONFIG_SYS_CBSIZE		2048
+#define CONFIG_SYS_MAXARGS		64
+#define CONFIG_SYS_BARGSIZE		CONFIG_SYS_CBSIZE
+#define CONFIG_SYS_PBSIZE		(CONFIG_SYS_CBSIZE + sizeof(CONFIG_SYS_PROMPT) + 16)
+
+/* Using ULP WDOG for reset */
+#define WDOG_BASE_ADDR			WDG3_RBASE
+
+/* USB configs */
+#ifndef CONFIG_SPL_BUILD
+#define CONFIG_USBD_HS
+#endif
+#define CONFIG_USB_GADGET_VBUS_DRAW 2
+#define CONFIG_USB_MAX_CONTROLLER_COUNT         2
+
+#endif
