dc98888a6fe364785b4da24d297c7a5f15f829e0
Simon Gaynor <simon.gaynor@ezurio.com>
2024-04-02 09:16:31 -0700

    imx8mp_mmr: switch on lvds power supplies and backlight
    Signed-off-by: Simon Gaynor <simon.gaynor@ezurio.com>

--- board/boundary/imx8mp_mmr/imx8mp_mmr.c
+++ board/boundary/imx8mp_mmr/imx8mp_mmr.c
@@ -39,11 +39,14 @@ static iomux_v3_cfg_t const init_pads[] = {
 	IOMUX_PAD_CTRL(GPIO1_IO02__WDOG1_WDOG_B, WDOG_PAD_CTRL),
 	IOMUX_PAD_CTRL(UART2_RXD__UART2_DCE_RX, UART_PAD_CTRL),
 	IOMUX_PAD_CTRL(UART2_TXD__UART2_DCE_TX, UART_PAD_CTRL),
-#define GP_MIPI_ENABLE		IMX_GPIO_NR(4, 3)
+#define GP_DISP0_GPIO		IMX_GPIO_NR(4, 3)
 	IOMUX_PAD_CTRL(SAI1_RXD1__GPIO4_IO03, 0x100),
-
+#define GP_24V_EN		IMX_GPIO_NR(4, 4)
+	IOMUX_PAD_CTRL(SAI1_RXD2__GPIO4_IO04, 0),
+#define GP_12V_EN		IMX_GPIO_NR(4, 6)
+	IOMUX_PAD_CTRL(SAI1_RXD4__GPIO4_IO06, 0),
 #define GP_PWM1_MIPI		IMX_GPIO_NR(5, 4)
-	IOMUX_PAD_CTRL(GPIO1_IO08__PWM1_OUT, 0x100),		/* PWM1 */
+	IOMUX_PAD_CTRL(GPIO1_IO08__GPIO1_IO08, 0),		/* PWM1 */
 	/* eqos */
 #define GP_EQOS_MII_MDC		IMX_GPIO_NR(1, 16)
 	IOMUX_PAD_CTRL(ENET_MDC__ENET_QOS_MDC, 0x3),
@@ -62,11 +65,27 @@ int board_early_init_f(void)
 {
 	struct wdog_regs *wdog = (struct wdog_regs *)WDOG1_BASE_ADDR;
 
+	imx_iomux_v3_setup_multiple_pads(init_pads, ARRAY_SIZE(init_pads));
+
+	/* LVDS Power and backlight */
+	gpio_request(GP_24V_EN, "24v_en");
+	gpio_direction_output(GP_24V_EN, 1);
+	gpio_free(GP_24V_EN);
+
 	gpio_request(GP_PWM1_MIPI, "pwm1");
-	gpio_direction_output(GP_PWM1_MIPI, 0);
+	gpio_direction_output(GP_PWM1_MIPI, 1);
 	gpio_free(GP_PWM1_MIPI);
 
-	imx_iomux_v3_setup_multiple_pads(init_pads, ARRAY_SIZE(init_pads));
+	gpio_request(GP_DISP0_GPIO, "disp0_gpio");
+	gpio_direction_output(GP_DISP0_GPIO, 1);
+	gpio_free(GP_DISP0_GPIO);
+
+	gpio_request(GP_12V_EN, "12v_en");
+	gpio_direction_output(GP_12V_EN, 1);
+	gpio_free(GP_12V_EN);
+
+	/*	*/
+
 	set_wdog_reset(wdog);
 	init_uart_clk(1);
 
@@ -90,6 +109,12 @@ int board_detect_hdmi(struct display_info_t const *di)
 }
 #endif
 static const struct display_info_t displays[] = {
+	/*
+	 * lvds
+	 * uses both lvds connectors
+	 */
+	VD_AUO_P280IVN01_J(LVDS, NULL, fbp_bus_gp(0, 0, GP_DISP0_GPIO, 0), 0x00),
+
 	/* hdmi */
 #ifdef CONFIG_VIDEO_IMX8MP_HDMI
 	VD_1920_1080M_60(HDMI, board_detect_hdmi, 0, 0x50),
@@ -100,13 +125,6 @@ static const struct display_info_t displays[] = {
 	VD_1280_720M_60(HDMI, NULL, 0, 0x50),
 	VD_1024_768M_60(HDMI, NULL, 0, 0x50),
 	VD_640_480M_60(HDMI, NULL, 0, 0x50),
-
-	/* mipi TODO If i remove this HDMI stops working in Linux ?? */
-	VD_MIPI_640_480M_60(MIPI, NULL, fbp_bus_gp((2 | (2 << 4)), 0, 0, 0), 0x68, FBP_PCA9546),
-
-	/* lvds */
-	/* TODO */
-	VD_M101NWWB(    LVDS, NULL, fbp_bus_gp(3, 0, 0, 0), 0x38, FBTS_FT5X06_2, FBTS_LVDS_FT5X06),
 };
 #define display_cnt	ARRAY_SIZE(displays)
 #else
