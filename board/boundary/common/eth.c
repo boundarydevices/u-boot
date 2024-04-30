boundarydevices/u-boot

commit 6866cccb9a571efda2c080fab6c45871180c2b0d

boundary: common: eth: skip this code for Atheros phy on Nit8MP
On the Nitrogen 8MP platform Atheros PHY support is provided
by the DM_ETH (Device-tree) mechanism.
So we skip the common/eth code for Atheros and this platform.

Signed-off-by: Simon Gaynor <simon.gaynor@ezurio.com>

board/boundary/common/eth.c
@@ -3,6 +3,14 @@
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
+
+/* If TARGET_NITROGEN8MP AND PHY_ATHEROS then
+ * use the CONFIG_DM_ETH method of loading it and not code in this file.
+ */
+#if defined(CONFIG_TARGET_NITROGEN8MP) && defined(CONFIG_PHY_ATHEROS)
+#undef CONFIG_PHY_ATHEROS
+#endif
+
 #include <common.h>
 #ifndef CONFIG_ARCH_MEDIATEK
 #include <asm/arch/clock.h>

commit b0e908b1ecbd5762e6cbab30c4c43debd273886e

boundary: eth: fix board_rv variable for nitrogen8mp
Fixes: 6866cccb9a5 boundary: common: eth: skip this code for Atheros phy
on Nit8MP

Since the Atheros macro got disabled, then board_rv was set as r20.

Signed-off-by: Gary Bisson <gary.bisson@ezurio.com>

board/boundary/common/eth.c
@@ -1027,17 +1027,16 @@ int board_phy_config(struct phy_device *phydev)
 	} else if (((phydev->drv->uid ^ PHY_ID_AR8035) & 0xffffffef) == 0) {
 		phy_ar8035_config(phydev);
 		board_eth_type(PHY_INDEX, 0);
-	} else if (is_micrel_part(phydev)) {
+	}
+#endif
+#if defined(CONFIG_PHY_MICREL) && !defined(CONFIG_PHY_MICREL_KSZ8XXX)
+	if (is_micrel_part(phydev)) {
 		/* found KSZ, reinit phy for KSZ */
 		setup_iomux_enet(1, (PHY_INDEX ? 2 : 1));
-#else
-	{
-#endif
-#ifndef CONFIG_PHY_MICREL_KSZ8XXX
 		phy_micrel_config(phydev);
 		board_eth_type(PHY_INDEX, 1);
-#endif
 	}
+#endif
 	if (phydev->drv->config)
 		phydev->drv->config(phydev);
 	return 0;