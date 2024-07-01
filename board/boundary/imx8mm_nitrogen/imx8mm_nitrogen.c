501d8e6a6fbefe723521da365e3c0e5d478f2595
Simon Gaynor <simon.gaynor@ezurio.com>
2024-07-01 15:21:53 -0700

    imx8mm_nitrogen_som: Initial addition
    imx8mm_nitrogen_som: Initial addition nitrogen8mm_som_2gr0_defconfig
    imx8mm_nitrogen_som: Initial addition nitrogen8mm_som_4g_defconfig
    imx8mm_nitrogen_som: board_late_mmc_env_init not needed
    imx8mm_nitrogen_som: Initial addition nitrogen8mm_som_1gr0_defconfig
    imx8mm_nitrogen_som: add eth phy reset timing
    imx8mm_nitrogen_som: dts: use reg_usdhc2_vqmmc for sdcard
    nitrogen8mm: som and sbc use different pins for usb2 oc
    nitrogen8mm_som : use CONFIG_USB_GADGET_SPL_SPD_PRODUCT_NUM
    nitrogen8mm_som: fix indentation
    nitrogen8mm_som: Remove unnecessary GPIO macros
    nitrogen8mm_som: revert to upstream fastboot commands
    
    - allowing to flash the bootloader with simple commands
    - also exposes the entire emmc user space as "emmc"
    
    nitrogen8mm: remove imx8mmn
    nitrogen8mm_rev2_2gr0: Initial import from u-boot 2022.04
    nitrogen8mm_rev2_2gr0: Align with other boundary boards
    nitrogen8mm_rev2 : file name correction
    nitrogen8mm: now for som and sbc
    nitrogen8mm: add serial# variable
    
    Signed-off-by: Simon Gaynor <simon.gaynor@ezurio.com>
    
    nitrogen8mm_som: cleanup board file
    
    Remove unnecessary code
    
    arm: dts: imx8mm-nitrogen8mm: fix ethernet pad settings
    
    - add pull-ups on all RXD/RXC signals to set all the address bits
    - use same pad settings as NXP EVK for TX
    - add pull-up on PHY reset signal (instead of pull-down)
    
    nitrogen8mm_som_2gr0_defconfig: run savedefconfig
    imx8mm_nitrogen: update mcore related variables
    
    Signed-off-by: Gary Bisson <gary.bisson@ezurio.com>

---
+++ board/boundary/imx8mm_nitrogen/imx8mm_nitrogen.c
@@ -0,0 +1,163 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2024 Ezurio
+ */
+
+#include <errno.h>
+#include <asm/io.h>
+#include <init.h>
+#include <miiphy.h>
+#include <netdev.h>
+#include <asm/global_data.h>
+#include <asm/mach-imx/iomux-v3.h>
+#include <asm-generic/gpio.h>
+#include <asm/arch/imx8mm_pins.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/mach-imx/dma.h>
+#include <asm/mach-imx/gpio.h>
+#include <asm/mach-imx/mxc_i2c.h>
+#include <env.h>
+#include <dm.h>
+#include <i2c.h>
+#include <linux/delay.h>
+#include <spl.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+#define UART_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_FSEL1)
+#define WDOG_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_ODE | PAD_CTL_PUE | PAD_CTL_PE)
+
+static iomux_v3_cfg_t const uart_pads[] = {
+	IMX8MM_PAD_UART2_RXD_UART2_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
+	IMX8MM_PAD_UART2_TXD_UART2_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
+};
+
+static iomux_v3_cfg_t const wdog_pads[] = {
+	IMX8MM_PAD_GPIO1_IO02_WDOG1_WDOG_B | MUX_PAD_CTRL(WDOG_PAD_CTRL),
+};
+
+static iomux_v3_cfg_t const init_pads[] = {
+	IMX8MM_PAD_GPIO1_IO01_GPIO1_IO1 | MUX_PAD_CTRL(PAD_CTL_PE |
+						       PAD_CTL_DSE6),
+	IMX8MM_PAD_GPIO1_IO06_GPIO1_IO6 | MUX_PAD_CTRL(PAD_CTL_PE |
+						       PAD_CTL_HYS),
+	IMX8MM_PAD_GPIO1_IO07_GPIO1_IO7 | MUX_PAD_CTRL(PAD_CTL_PE),
+	IMX8MM_PAD_SAI3_TXC_GPIO5_IO0 | MUX_PAD_CTRL(PAD_CTL_DSE6),
+	IMX8MM_PAD_GPIO1_IO03_GPIO1_IO3 | MUX_PAD_CTRL(PAD_CTL_PE | PAD_CTL_HYS
+						       | PAD_CTL_PUE),
+
+	IMX8MM_PAD_GPIO1_IO11_GPIO1_IO11 | MUX_PAD_CTRL(PAD_CTL_PE |
+							PAD_CTL_PUE),
+	IMX8MM_PAD_GPIO1_IO09_GPIO1_IO9 | MUX_PAD_CTRL(PAD_CTL_PE),
+
+	IMX8MM_PAD_SAI3_TXFS_GPIO4_IO31 | MUX_PAD_CTRL(PAD_CTL_PE),
+	IMX8MM_PAD_GPIO1_IO04_GPIO1_IO4 | MUX_PAD_CTRL(PAD_CTL_PE),
+
+	IMX8MM_PAD_GPIO1_IO10_GPIO1_IO10 | MUX_PAD_CTRL(PAD_CTL_HYS),
+	IMX8MM_PAD_SAI3_RXFS_GPIO4_IO28 | MUX_PAD_CTRL(PAD_CTL_HYS),
+	IMX8MM_PAD_SD1_RESET_B_GPIO2_IO10 | MUX_PAD_CTRL(PAD_CTL_PUE |
+							 PAD_CTL_DSE1),
+
+	IMX8MM_PAD_GPIO1_IO14_USB2_OTG_PWR | MUX_PAD_CTRL(PAD_CTL_FSEL1 |
+							  PAD_CTL_DSE6),
+#if (IS_ENABLED(CONFIG_TARGET_NITROGEN8MM_SOM))
+	/* GPIO15 is used for CCM_CLKO2, GPIO1_IO08 is overcurrent */
+	IMX8MM_PAD_GPIO1_IO08_GPIO1_IO8 | MUX_PAD_CTRL(PAD_CTL_PUE |
+						       PAD_CTL_PE),
+#else
+	/* SBC */
+	IMX8MM_PAD_GPIO1_IO15_USB2_OTG_OC | MUX_PAD_CTRL(PAD_CTL_PUE |
+							 PAD_CTL_PE),
+#endif
+
+};
+
+#if (IS_ENABLED(CONFIG_FEC_MXC))
+static int setup_fec(void)
+{
+	struct iomuxc_gpr_base_regs *gpr =
+	    (struct iomuxc_gpr_base_regs *)IOMUXC_GPR_BASE_ADDR;
+
+	/* Use 125M anatop REF_CLK1 for ENET1, not from external */
+	clrsetbits_le32(&gpr->gpr[1], 0x2000, 0);
+
+	return 0;
+}
+#endif
+
+int board_early_init_f(void)
+{
+	struct wdog_regs *wdog = (struct wdog_regs *)WDOG1_BASE_ADDR;
+
+	imx_iomux_v3_setup_multiple_pads(wdog_pads, ARRAY_SIZE(wdog_pads));
+
+	set_wdog_reset(wdog);
+
+	imx_iomux_v3_setup_multiple_pads(uart_pads, ARRAY_SIZE(uart_pads));
+
+	init_uart_clk(1);
+
+	gpio_request(IMX_GPIO_NR(5, 0), "sn65en");
+	gpio_direction_output(IMX_GPIO_NR(5, 0), 0);
+	imx_iomux_v3_setup_multiple_pads(init_pads, ARRAY_SIZE(init_pads));
+
+	gpio_direction_output(IMX_GPIO_NR(2, 10), 1);
+	set_wdog_reset(wdog);
+	return 0;
+}
+
+int board_phys_sdram_size(phys_size_t *size)
+{
+	if (!size)
+		return -EINVAL;
+
+	*size = get_ram_size((void *)CFG_SYS_SDRAM_BASE, SZ_4G);	/* Maximum 4G */
+
+	return 0;
+}
+
+int board_init(void)
+{
+	gpio_request(IMX_GPIO_NR(1, 7), "gt911_reset");
+	gpio_request(IMX_GPIO_NR(1, 6), "gt911_irq");
+#ifndef CONFIG_DM_VIDEO
+	gpio_request(IMX_GPIO_NR(5, 0), "sn65dsi83_enable");
+	gpio_request(IMX_GPIO_NR(1, 1), "lkt08_mipi_en");
+#endif
+	gpio_request(IMX_GPIO_NR(1, 9), "csi1_mipi_reset");
+	gpio_direction_output(IMX_GPIO_NR(1, 7), 0);
+#if defined(CONFIG_MXC_SPI) && !defined(CONFIG_DM_SPI)
+	setup_spi();
+#endif
+	if (IS_ENABLED(CONFIG_FEC_MXC))
+		setup_fec();
+#ifdef CONFIG_FSL_FSPI
+	board_qspi_init();
+#endif
+
+	return 0;
+}
+
+int mmc_map_to_kernel_blk(int dev_no)
+{
+	return dev_no;
+}
+
+int board_late_init(void)
+{
+	if (!env_get("serial#")) {
+		unsigned char mac_address[8];
+		char serialbuf[20];
+
+		imx_get_mac_from_fuse(0, mac_address);
+		snprintf(serialbuf, sizeof(serialbuf),
+			 "%02x%02x%02x%02x%02x%02x", mac_address[0],
+			 mac_address[1], mac_address[2], mac_address[3],
+			 mac_address[4], mac_address[5]);
+		printf("serial: %s\n", serialbuf);
+		env_set("serial#", serialbuf);
+	}
+
+	return 0;
+}
---
+++ board/boundary/imx8mm_nitrogen/spl.c
@@ -0,0 +1,304 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2019, 2021 NXP
+ */
+
+#include <common.h>
+#include <command.h>
+#include <cpu_func.h>
+#include <display_options.h>
+#include <hang.h>
+#include <image.h>
+#include <init.h>
+#include <log.h>
+#include <spl.h>
+#include <asm/global_data.h>
+#include <asm/io.h>
+#include <asm/mach-imx/iomux-v3.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/imx8mm_pins.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/mach-imx/boot_mode.h>
+#include <asm/arch/ddr.h>
+#include <asm/sections.h>
+
+#include <power/pmic.h>
+#include <asm/mach-imx/gpio.h>
+#include <asm/mach-imx/mxc_i2c.h>
+#include <fsl_esdhc_imx.h>
+#include <mmc.h>
+#include <linux/delay.h>
+#include <fsl_sec.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+void spl_dram_init(void)
+{
+	long dram_size;
+
+	ddr_init(&dram_timing);
+
+	dram_size = get_ram_size((long int *)CFG_SYS_SDRAM_BASE, SZ_4G);
+
+	printf("DDRINFO: Size: ");
+	print_size(dram_size, "\n");
+}
+
+#define I2C_PAD_CTRL	(PAD_CTL_DSE1 | PAD_CTL_HYS | PAD_CTL_PUE | PAD_CTL_PE)
+
+struct i2c_pads_info i2c_pad_info1[] = {
+	{
+	 .scl = {
+		 .i2c_mode =
+		 IMX8MM_PAD_I2C1_SCL_I2C1_SCL | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		 .gpio_mode =
+		 IMX8MM_PAD_I2C1_SCL_GPIO5_IO14 | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		 .gp = IMX_GPIO_NR(5, 14),
+		 },
+	 .sda = {
+		 .i2c_mode =
+		 IMX8MM_PAD_I2C1_SDA_I2C1_SDA | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		 .gpio_mode =
+		 IMX8MM_PAD_I2C1_SDA_GPIO5_IO15 | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		 .gp = IMX_GPIO_NR(5, 15),
+		 },
+	  },
+	{
+	 .scl = {
+		 .i2c_mode =
+		 IMX8MM_PAD_I2C2_SCL_I2C2_SCL | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		 .gpio_mode =
+		 IMX8MM_PAD_I2C2_SCL_GPIO5_IO16 | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		 .gp = IMX_GPIO_NR(5, 16),
+		 },
+	 .sda = {
+		 .i2c_mode =
+		 IMX8MM_PAD_I2C2_SDA_I2C2_SDA | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		 .gpio_mode =
+		 IMX8MM_PAD_I2C2_SDA_GPIO5_IO17 | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		 .gp = IMX_GPIO_NR(5, 17),
+		 },
+	  },
+	{
+	 .scl = {
+		 .i2c_mode =
+		 IMX8MM_PAD_I2C3_SCL_I2C3_SCL | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		 .gpio_mode =
+		 IMX8MM_PAD_I2C3_SCL_GPIO5_IO18 | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		 .gp = IMX_GPIO_NR(5, 18),
+		 },
+	 .sda = {
+		 .i2c_mode =
+		 IMX8MM_PAD_I2C3_SDA_I2C3_SDA | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		 .gpio_mode =
+		 IMX8MM_PAD_I2C3_SDA_GPIO5_IO19 | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		 .gp = IMX_GPIO_NR(5, 19),
+		 },
+	  },
+	{
+	 .scl = {
+		 .i2c_mode =
+		 IMX8MM_PAD_I2C4_SDA_I2C4_SDA | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		 .gpio_mode =
+		 IMX8MM_PAD_I2C4_SCL_GPIO5_IO20 | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		 .gp = IMX_GPIO_NR(5, 20),
+		 },
+	 .sda = {
+		 .i2c_mode =
+		 IMX8MM_PAD_I2C4_SDA_I2C4_SDA | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		 .gpio_mode =
+		 IMX8MM_PAD_I2C4_SDA_GPIO5_IO21 | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		 .gp = IMX_GPIO_NR(5, 21),
+		 },
+	  },
+};
+
+#define USDHC_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_HYS | PAD_CTL_PUE |PAD_CTL_PE | \
+			 PAD_CTL_FSEL2)
+
+static iomux_v3_cfg_t const usdhc1_pads[] = {
+	IMX8MM_PAD_SD1_CLK_USDHC1_CLK | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MM_PAD_SD1_CMD_USDHC1_CMD | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MM_PAD_SD1_DATA0_USDHC1_DATA0 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MM_PAD_SD1_DATA1_USDHC1_DATA1 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MM_PAD_SD1_DATA2_USDHC1_DATA2 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MM_PAD_SD1_DATA3_USDHC1_DATA3 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MM_PAD_SD1_DATA4_USDHC1_DATA4 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MM_PAD_SD1_DATA5_USDHC1_DATA5 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MM_PAD_SD1_DATA6_USDHC1_DATA6 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MM_PAD_SD1_DATA7_USDHC1_DATA7 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MM_PAD_SD1_RESET_B_GPIO2_IO10 | MUX_PAD_CTRL(PAD_CTL_PUE |
+							 PAD_CTL_DSE1),
+};
+
+static iomux_v3_cfg_t const usdhc2_pads[] = {
+	IMX8MM_PAD_SD2_CLK_USDHC2_CLK | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MM_PAD_SD2_CMD_USDHC2_CMD | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MM_PAD_SD2_DATA0_USDHC2_DATA0 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MM_PAD_SD2_DATA1_USDHC2_DATA1 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MM_PAD_SD2_DATA2_USDHC2_DATA2 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MM_PAD_SD2_DATA3_USDHC2_DATA3 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MM_PAD_SD2_RESET_B_GPIO2_IO19 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MM_PAD_NAND_CE1_B_GPIO3_IO2 | MUX_PAD_CTRL(PAD_CTL_FSEL2 |
+						       PAD_CTL_DSE4 |
+						       PAD_CTL_DSE2),
+	IMX8MM_PAD_SD2_CD_B_USDHC2_CD_B | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+};
+
+static struct fsl_esdhc_cfg usdhc_cfg[2] = {
+	{ USDHC1_BASE_ADDR, 0, 8 },
+	{ USDHC2_BASE_ADDR, 0, 4 },
+};
+
+int board_mmc_getcd(struct mmc *mmc)
+{
+	struct fsl_esdhc_cfg *cfg = (struct fsl_esdhc_cfg *)mmc->priv;
+	int ret = 0;
+
+	switch (cfg->esdhc_base) {
+	case USDHC1_BASE_ADDR:
+		ret = 1;
+		break;
+	case USDHC2_BASE_ADDR:
+		// TODO Detect USDHC2_CD_B
+		ret = 1;
+		break;
+	}
+
+	return ret;
+}
+
+int board_mmc_init(struct bd_info *bis)
+{
+	int i, ret;
+	/*
+	 * According to the board_mmc_init() the following map is done:
+	 * (U-Boot device node)    (Physical Port)
+	 * mmc0                    USDHC1
+	 * mmc1                    USDHC2
+	 */
+	for (i = 0; i < CFG_SYS_FSL_USDHC_NUM; i++) {
+		switch (i) {
+		case 0:
+			init_clk_usdhc(1);
+			usdhc_cfg[0].sdhc_clk = mxc_get_clock(MXC_ESDHC_CLK);
+			imx_iomux_v3_setup_multiple_pads(usdhc1_pads,
+							 ARRAY_SIZE
+							 (usdhc1_pads));
+			gpio_request(IMX_GPIO_NR(2, 10), "usdhc1_reset");
+			gpio_direction_output(IMX_GPIO_NR(2, 10), 0);
+			udelay(500);
+			gpio_direction_output(IMX_GPIO_NR(2, 10), 1);
+			break;
+		case 1:
+			init_clk_usdhc(2);
+			usdhc_cfg[1].sdhc_clk = mxc_get_clock(MXC_ESDHC2_CLK);
+			imx_iomux_v3_setup_multiple_pads(usdhc2_pads,
+							 ARRAY_SIZE
+							 (usdhc2_pads));
+			break;
+		default:
+			printf("Warning: you configured more USDHC controllers"
+			       "(%d) than supported by the board\n", i + 1);
+			return -EINVAL;
+		}
+
+		ret = fsl_esdhc_initialize(bis, &usdhc_cfg[i]);
+		if (ret)
+			return ret;
+	}
+
+	return 0;
+}
+
+int power_init_boundary(void)
+{
+	int ret;
+	unsigned char buf[4];
+
+	i2c_set_bus_num(0);
+#define PF8100	0x08
+#define SW2_VOLT	0x59
+#define SW3_CONFIG2	0x5e
+#define SW3_VOLT	0x61
+#define SW4_CONFIG2	0x66
+#define SW4_VOLT	0x69
+#define SW5_VOLT	0x71
+
+	buf[0] = 0x50;		/* (.90-.4)*160=.50*160=80=0x50  80/160+.4=.90 gpu/dram/arm */
+	ret = i2c_write(PF8100, SW2_VOLT, 1, buf, 1);
+	if (ret)
+		return ret;
+	ret = i2c_write(PF8100, SW4_VOLT, 1, buf, 1);
+	if (ret)
+		return ret;
+	ret = i2c_write(PF8100, SW3_VOLT, 1, buf, 1);
+	if (ret)
+		return ret;
+	/*
+	 * Make sw3 a 180 phase shift from sw4,
+	 * in case pmic not programmed for dual mode
+	 */
+	ret = i2c_read(PF8100, SW4_CONFIG2, 1, buf, 1);
+	if (!ret) {
+		buf[0] ^= 4;	/* 180 degree phase */
+		ret = i2c_write(PF8100, SW3_CONFIG2, 1, buf, 1);
+	}
+
+	buf[0] = 0x40;		/* (.80-.4)*160=.40*160=64=0x40  64/160+.4=.80 vpu */
+	ret = i2c_write(PF8100, SW5_VOLT, 1, buf, 1);
+	return ret;
+}
+
+void spl_board_init(void)
+{
+	if (IS_ENABLED(CONFIG_FSL_CAAM)) {
+		if (sec_init())
+			printf("\nsec_init failed!\n");
+	}
+	puts("Normal Boot\n");
+}
+
+#ifdef CONFIG_SPL_LOAD_FIT
+int board_fit_config_name_match(const char *name)
+{
+	/* Just empty function now - can't decide what to choose */
+	debug("%s: %s\n", __func__, name);
+
+	return 0;
+}
+#endif
+
+void board_init_f(ulong dummy)
+{
+	int ret;
+	int i;
+
+	/* Clear the BSS. */
+	memset(__bss_start, 0, __bss_end - __bss_start);
+
+	arch_cpu_init();
+
+	board_early_init_f();
+
+	timer_init();
+
+	preloader_console_init();
+
+	ret = spl_init();
+	if (ret) {
+		debug("spl_init() failed: %d\n", ret);
+		hang();
+	}
+
+	enable_tzc380();
+
+	for (i = 0; i < ARRAY_SIZE(i2c_pad_info1); i++)
+		setup_i2c(i, CONFIG_SYS_I2C_SPEED, 0x7f, &i2c_pad_info1[i]);
+
+	power_init_boundary();
+	/* DDR initialization */
+	spl_dram_init();
+
+	board_init_r(NULL, 0);
+}
