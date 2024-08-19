e91fdae928ec699e56b73dbe47aaa4d63be0c1c6
Gary Bisson <gary.bisson@ezurio.com>
2024-08-19 14:35:15 +0200

    imx93_nitrogen_smarc: sync default env with other nitrogen platforms
    Signed-off-by: Gary Bisson <gary.bisson@ezurio.com>

--- include/configs/imx93_nitrogen_smarc.h
+++ include/configs/imx93_nitrogen_smarc.h
@@ -51,55 +51,18 @@
 	"env_part=" __stringify(CONFIG_SYS_MMC_ENV_PART) "\0" \
 	"fastboot_raw_partition_bootloader=0x0 0x1ff0 mmcpart 1\0" \
 	"fastboot_raw_partition_bootloader-env=0x1ff0 0x10 mmcpart 1\0" \
-	"prepare_mcore=setenv mcore_clk clk-imx93.mcore_booted;\0" \
+	"mcoreboot=load ${devtype} ${devnum}:1 ${mcore_loadaddr} ${mcore_image}; " \
+		"dcache flush; bootaux " __stringify(TCML_BASE_MCORE_SEC_ADDR)"\0" \
+	"mcore_image=m33_fw.bin\0" \
+	"mcore_loadaddr=" __stringify(CONFIG_IMX_MCORE_TCM_ADDR) "\0" \
+	"mcore_bootargs=clk-imx93.mcore_booted\0" \
 	"scriptaddr=0x83500000\0" \
-	"kernel_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
-	"image=Image\0" \
-	"splashimage=0x90000000\0" \
 	"console=ttyLP0\0" \
-	"fdt_addr_r=0x83000000\0"			\
 	"fdt_addr=0x83000000\0"			\
 	"fdt_high=0xffffffffffffffff\0"		\
-	"cntr_addr=0x98000000\0"			\
-	"cntr_file=os_cntr_signed.bin\0" \
-	"boot_fit=no\0" \
-	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
-	"bootm_size=0x10000000\0" \
-	"mmcdev=" __stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
-	"mmcpart=1\0" \
-	"mmcroot=/dev/mmcblk1p2 rootwait rw\0" \
-	"mmcautodetect=yes\0" \
-	"mmcargs=setenv bootargs ${jh_clk} ${mcore_clk} console=${console} root=${mmcroot}\0 " \
-	"loadbootscript=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
-	"bootscript=echo Running bootscript from mmc ...; " \
-		"source\0" \
-	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
-	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr_r} ${fdtfile}\0" \
-	"loadcntr=fatload mmc ${mmcdev}:${mmcpart} ${cntr_addr} ${cntr_file}\0" \
-	"auth_os=auth_cntr ${cntr_addr}\0" \
-	"boot_os=booti ${loadaddr} - ${fdt_addr_r};\0" \
-	"mmcboot=echo Booting from mmc ...; " \
-		"run mmcargs; " \
-		"if test ${sec_boot} = yes; then " \
-			"if run auth_os; then " \
-				"run boot_os; " \
-			"else " \
-				"echo ERR: failed to authenticate; " \
-			"fi; " \
-		"else " \
-			"if test ${boot_fit} = yes || test ${boot_fit} = try; then " \
-				"bootm ${loadaddr}; " \
-			"else " \
-				"if run loadfdt; then " \
-					"run boot_os; " \
-				"else " \
-					"echo WARN: Cannot load the DT; " \
-				"fi; " \
-			"fi;" \
-		"fi;\0" \
-	"netargs=setenv bootargs ${jh_clk} ${mcore_clk} console=${console} " \
-		"root=/dev/nfs " \
-		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
+	"fdt_file=" CONFIG_DEFAULT_FDT_FILE "\0" \
+	"netargs=setenv bootargs console=${console},115200 root=/dev/nfs rw " \
+		"ip=dhcp nfsroot=${tftpserverip}:${nfsroot},v3,tcp\0" \
 	"netboot=echo Booting from net ...; " \
 		"run netargs;  " \
 		"if test ${ip_dyn} = yes; then " \
@@ -107,43 +70,12 @@
 		"else " \
 			"setenv get_cmd tftp; " \
 		"fi; " \
-		"if test ${sec_boot} = yes; then " \
-			"${get_cmd} ${cntr_addr} ${cntr_file}; " \
-			"if run auth_os; then " \
-				"run boot_os; " \
-			"else " \
-				"echo ERR: failed to authenticate; " \
-			"fi; " \
+		"${get_cmd} ${loadaddr} ${tftpserverip}:Image; " \
+		"if ${get_cmd} ${fdt_addr} ${tftpserverip}:${fdt_file}; then " \
+			"booti ${loadaddr} - ${fdt_addr}; " \
 		"else " \
-			"${get_cmd} ${loadaddr} ${image}; " \
-			"if test ${boot_fit} = yes || test ${boot_fit} = try; then " \
-				"bootm ${loadaddr}; " \
-			"else " \
-				"if ${get_cmd} ${fdt_addr_r} ${fdtfile}; then " \
-					"run boot_os; " \
-				"else " \
-					"echo WARN: Cannot load the DT; " \
-				"fi; " \
-			"fi;" \
+			"echo WARN: Cannot load the DT; " \
 		"fi;\0" \
-	"bsp_bootcmd=echo Running BSP bootcmd ...; " \
-		"mmc dev ${mmcdev}; if mmc rescan; then " \
-		   "if run loadbootscript; then " \
-			   "run bootscript; " \
-		   "else " \
-			   "if test ${sec_boot} = yes; then " \
-				   "if run loadcntr; then " \
-					   "run mmcboot; " \
-				   "else run netboot; " \
-				   "fi; " \
-			    "else " \
-				   "if run loadimage; then " \
-					   "run mmcboot; " \
-				   "else run netboot; " \
-				   "fi; " \
-				"fi; " \
-		   "fi; " \
-	   "fi;\0" \
 	"upgradeu=setenv boot_scripts upgrade.scr; boot;" \
                 "echo Upgrade failed!; setenv boot_scripts boot.scr;\0" \
 	"uboot_defconfig=" NITROGEN_SMARC_DEFCONFIG "\0"
