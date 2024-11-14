27a1ee7fc75932d743d2ab7934956e425f67fd79
Simon Gaynor <simon.gaynor@ezurio.com>
2024-11-14 11:04:50 -0800

    WIP : imx8mm nitrogen smarc: add display panel

--- board/boundary/imx8mm_nitrogen_smarc/imx8mm_nitrogen_smarc.c
+++ board/boundary/imx8mm_nitrogen_smarc/imx8mm_nitrogen_smarc.c
@@ -15,11 +15,13 @@
 #include <asm/arch/imx8mm_pins.h>
 #include <asm/arch/clock.h>
 #include <asm/arch/sys_proto.h>
+#include <asm/mach-imx/fbpanel.h>
 #include <asm/mach-imx/gpio.h>
 #include <asm/mach-imx/mxc_i2c.h>
 #include <i2c.h>
 #include <asm/io.h>
 #include <usb.h>
+#include "../common/bd_common.h"
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -38,11 +40,16 @@ static iomux_v3_cfg_t const wdog_pads[] = {
 static iomux_v3_cfg_t const init_pads[] = {
 	IMX8MM_PAD_SAI5_RXD3_GPIO3_IO24 | MUX_PAD_CTRL(PAD_CTL_PE),	/* MIPI_IRQ - SM_CARRIER_STANDBY */
 	IMX8MM_PAD_GPIO1_IO01_GPIO1_IO1 | MUX_PAD_CTRL(PAD_CTL_PE | PAD_CTL_HYS),	/* MIPI_TS_IRQ  - SM_LCD0_BKLT_EN */
+#define GP_SM_LCD0_BKLT_EN IMX_GPIO_NR(1, 1)
 	IMX8MM_PAD_SAI1_RXD3_GPIO4_IO5 | MUX_PAD_CTRL(PAD_CTL_PE),	/* MIPI_TS_RESET - SM_GPIO7 */
 	IMX8MM_PAD_GPIO1_IO03_GPIO1_IO3 | MUX_PAD_CTRL(PAD_CTL_DSE2 | PAD_CTL_DSE4),	/* MIPI_ENABLE - SM_LCD0_VDD_EN */
 	IMX8MM_PAD_GPIO1_IO10_GPIO1_IO10 | MUX_PAD_CTRL(PAD_CTL_DSE2 | PAD_CTL_DSE4),	/* SM_ESPI_ALERT0 */
+#define GP_TS_LVDS_GT911_RESET  IMX_GPIO_NR(1, 10)
+#define GP_TS_LVDS_FT5X06_RESET IMX_GPIO_NR(1, 10)
 	IMX8MM_PAD_SAI5_RXD1_GPIO3_IO22 | MUX_PAD_CTRL(PAD_CTL_DSE2 | PAD_CTL_DSE4),	/* SM_SMB_ALERT */
+#define GPIRQ_TS_LVDS_GT911     IMX_GPIO_NR(3, 22)
 	IMX8MM_PAD_SAI5_MCLK_GPIO3_IO25 | MUX_PAD_CTRL(PAD_CTL_DSE2 | PAD_CTL_DSE4),	/* SN65DSI83 on som  */
+#define GP_SN65DSI83_LVDS_EN    IMX_GPIO_NR(3, 25)
 	IMX8MM_PAD_SAI5_RXD2_GPIO3_IO23 | MUX_PAD_CTRL(PAD_CTL_DSE2 | PAD_CTL_DSE4),	/* RV3028 - SM_GPIO12 */
 	IMX8MM_PAD_NAND_CE1_B_GPIO3_IO2 | MUX_PAD_CTRL(PAD_CTL_PE),	/* SM_PCIE_A_RST */
 	IMX8MM_PAD_SD1_RESET_B_GPIO2_IO10 | MUX_PAD_CTRL(PAD_CTL_PUE | PAD_CTL_DSE1),	/* EMMC_RESET */
@@ -69,6 +76,28 @@ int board_phy_config(struct phy_device *phydev)
 }
 #endif
 
+#ifdef CONFIG_CMD_FBPANEL
+int board_detect_lvds_gt911(struct display_info_t const *di)
+{
+	/*						TS RESET	TS IRQ	*/
+	return board_detect_gt911_common(di, 0, 0, GP_TS_LVDS_GT911_RESET, GPIRQ_TS_LVDS_GT911);
+}
+
+static const struct display_info_t displays[] = {
+        /* on som mipi-to-lvds coverter */
+        VD_MIPI_TM070JDHG30_x("tm070jdhg30-5", UT, MIPI, board_detect_lvds_gt911, fbp_bus_gp(3, 0, GP_SN65DSI83_LVDS_EN, 0), 0x14, FBP_BACKLIGHT_MIPI_ALT, FBTS_LVDS_GOODIX),
+        VD_MIPI_TM070JDHG30_x("tm070jdhg30-6", BT, MIPI, NULL, fbp_bus_gp(3, 0, GP_SN65DSI83_LVDS_EN, 0), 0x14, FBP_BACKLIGHT_MIPI_ALT, FBTS_LVDS_GOODIX),
+        VD_MIPI_TM070JDHG30_x("tm070jdhg30-7", ET, MIPI, NULL, fbp_bus_gp(3, 0, GP_SN65DSI83_LVDS_EN, 0), 0x14, FBP_BACKLIGHT_MIPI_ALT, FBTS_LVDS_GOODIX),
+        VD_MIPI_TM070JDHG30_x("tm070jdhg30-8", UT, MIPI, board_detect_lvds_gt911, fbp_bus_gp(3, 0, GP_SN65DSI83_LVDS_EN, 0), 0x5d, FBP_BACKLIGHT_MIPI_ALT, FBTS_LVDS_GOODIX2),
+        VD_MIPI_TM070JDHG30_x("tm070jdhg30-9", BT, MIPI, fbp_detect_i2c, fbp_bus_gp(3, GP_TS_LVDS_FT5X06_RESET, GP_SN65DSI83_LVDS_EN, 0), 0x38, FBP_BACKLIGHT_MIPI_ALT, FBTS_LVDS_FT5X06),
+};
+#define display_cnt     ARRAY_SIZE(displays)
+#else
+#define displays        NULL
+#define display_cnt     0
+
+#endif
+
 int board_early_init_f(void)
 {
 	struct wdog_regs *wdog = (struct wdog_regs *)WDOG1_BASE_ADDR;
@@ -81,10 +110,15 @@ int board_early_init_f(void)
 
 	init_uart_clk(1);
 
-	gpio_request(IMX_GPIO_NR(1, 3), "sn65en");
-	gpio_direction_output(IMX_GPIO_NR(1, 3), 0);
 	imx_iomux_v3_setup_multiple_pads(init_pads, ARRAY_SIZE(init_pads));
 
+	gpio_request(GP_SN65DSI83_LVDS_EN, "sn65en");
+	gpio_direction_output(GP_SN65DSI83_LVDS_EN, 1);
+
+	gpio_request(GP_SM_LCD0_BKLT_EN, "lcd0_bklt_en");
+	gpio_direction_output(GP_SM_LCD0_BKLT_EN, 1);
+
+	gpio_request(IMX_GPIO_NR(2, 10), "emmc reset");
 	gpio_direction_output(IMX_GPIO_NR(2, 10), 1);
 	return 0;
 }
@@ -103,9 +137,9 @@ int board_init(void)
 {
 	gpio_request(IMX_GPIO_NR(3, 23), "rv3028_irq");
 	gpio_request(IMX_GPIO_NR(4, 5), "gt911_reset");
-	gpio_request(IMX_GPIO_NR(1, 10), "lvds_gt911_reset");
-	gpio_request(IMX_GPIO_NR(3, 22), "lvds_gt911_irq");
-#ifndef CONFIG_DM_VIDEO
+	gpio_request(GP_TS_LVDS_GT911_RESET, "lvds_gt911_reset");
+	gpio_request(GPIRQ_TS_LVDS_GT911, "lvds_gt911_irq");
+#ifndef CONFIG_VIDEO
 	gpio_request(IMX_GPIO_NR(1, 3), "sn65dsi83_enable");
 	/* This enables 5V power on LTK080A60A004T mipi display */
 	gpio_request(IMX_GPIO_NR(3, 24), "lkt08_mipi_en");
@@ -115,7 +149,9 @@ int board_init(void)
 
 	if (IS_ENABLED(CONFIG_FEC_MXC))
 		setup_fec();
-
+#ifdef CONFIG_CMD_FBPANEL
+        fbp_setup_display(displays, display_cnt);
+#endif
 	return 0;
 }
 
