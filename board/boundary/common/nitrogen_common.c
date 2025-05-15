ffbfb791ef3095fd4b8630fc72c4655c84b8c982
Simon Gaynor <simon.gaynor@ezurio.com>
2025-05-15 08:11:38 -0700

    boundary: common: fix formatting
    Signed-off-by: Simon Gaynor <simon.gaynor@ezurio.com>

--- board/boundary/common/nitrogen_common.c
+++ board/boundary/common/nitrogen_common.c
@@ -1,15 +1,12 @@
-/*
- * Copyright (C) 2025, Ezurio LLC.
- *
- * SPDX-License-Identifier:     GPL-2.0+
- *
- */
+// SPDX-License-Identifier:     GPL-2.0+
+// Copyright (C) 2025, Ezurio LLC.
 
 #include <env.h>
 #include <stdio.h>
 #include <vsprintf.h>
 
-void bd_setserialnumber(unsigned char mac[6]) {
+void bd_setserialnumber(unsigned char mac[6])
+{
 	char serialbuf[13];
 
 	snprintf(serialbuf, sizeof(serialbuf),
@@ -21,14 +18,15 @@ void bd_setserialnumber(unsigned char mac[6]) {
 }
 
 /* Function to increment a 6-byte MAC address */
-void bd_incrementmacaddress(unsigned char mac[6]) {
-    // Increment the address byte by byte
-    for (int i = 5; i >= 0; i--) {
-        mac[i]++;
-        if (mac[i] > 255) { // Check for overflow
-            mac[i] = 0;
-        } else {
-            break; // No need to increment further
-        }
-    }
+void bd_incrementmacaddress(unsigned char mac[6])
+{
+	// Increment the address byte by byte
+	for (int i = 5; i >= 0; i--) {
+		mac[i]++;
+		if (mac[i] > 255) { // Check for overflow
+			mac[i] = 0;
+		} else {
+			break; // No need to increment further
+		}
+	}
 }
