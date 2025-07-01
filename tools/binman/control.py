39d9ca02d3b83ba5280b077d00e9974f7961ae17
Yannic Moog <y.moog@phytec.de>
2025-07-01 07:45:37 +0200

    binman: migrate from pkg_resources to importlib
    pkg_resources is deprecated, use migration guide in [1] to migrate to
    importlib.resources.
    Keep the Python 3.6 backwards compatibility.
    This also fixes the binman version test which failed for setuptools
    versions that print the deprecation warning.
    Change from __name__ to __package__ as with the transition from
    pkg_resources to importlib_resources using __name__ results in
    TypeErrors [2].
    
    [1] https://importlib-resources.readthedocs.io/en/latest/migration.html
    [2] https://github.com/python/importlib_resources/issues/60
    
    Reviewed-by: Bryan Brattlof <bb@ti.com>
    Signed-off-by: Yannic Moog <y.moog@phytec.de>

--- tools/binman/control.py
+++ tools/binman/control.py
@@ -8,12 +8,11 @@
 from collections import OrderedDict
 import glob
 try:
-    import importlib.resources
-except ImportError:
+    import importlib.resources as importlib_resources
+except ImportError:  # pragma: no cover
     # for Python 3.6
     import importlib_resources
 import os
-import pkg_resources
 import re
 
 import sys
@@ -87,7 +86,7 @@ def _ReadMissingBlobHelp():
             msg = ''
         return tag, msg
 
-    my_data = pkg_resources.resource_string(__name__, 'missing-blob-help')
+    my_data = importlib_resources.files(__package__).joinpath('missing-blob-help').read_bytes()
     re_tag = re.compile('^([-a-z0-9]+):$')
     result = {}
     tag = None
@@ -134,8 +133,9 @@ def GetEntryModules(include_testing=True):
     Returns:
         Set of paths to entry class filenames
     """
-    glob_list = pkg_resources.resource_listdir(__name__, 'etype')
-    glob_list = [fname for fname in glob_list if fname.endswith('.py')]
+    entries = importlib_resources.files(__package__).joinpath('etype')
+    glob_list = [entry.name for entry in entries.iterdir()
+                 if entry.name.endswith('.py') and entry.is_file()]
     return set([os.path.splitext(os.path.basename(item))[0]
                 for item in glob_list
                 if include_testing or '_testing' not in item])
